623113e8e7b8f6e99810447352b22798c1fce5d3
Mona Mohamadinia <mona.mohamadinia@gmail.com>
2020-08-01 22:00:19 +0430

    SAM Conversions in Kotlin

---
+++ kotlin-lambda/src/main/kotlin/com/baeldung/sam/SamConversions.kt
@@ -0,0 +1,30 @@
+package com.baeldung.sam
+
+import java.util.concurrent.Callable
+import java.util.concurrent.Executors
+
+@Suppress("ObjectLiteralToLambda", "MoveLambdaOutsideParentheses")
+fun main() {
+    val objectExpressionThread = Thread(object : Runnable {
+        override fun run() { // the logic
+        }
+    })
+
+    val lambdaThread = Thread({
+        // the logic
+    })
+
+    val samThread = Thread {
+        // the logic
+    }
+
+    var answer = 42
+    val threadWithClosure = Thread { println(answer) }
+
+    val executor = Executors.newFixedThreadPool(2)
+    val submit = executor.submit(Callable { return@Callable 42 })
+}
+
+fun doSomething(): Runnable = Runnable {
+    // doing something
+}
