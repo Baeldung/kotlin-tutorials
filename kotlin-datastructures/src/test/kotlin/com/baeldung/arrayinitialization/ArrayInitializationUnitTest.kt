412e1d5423bee0ed8c03184b79ee24916bcd348a
Mona Mohamadinia <mona.mohamadinia@gmail.com>
2020-08-08 11:43:29 +0430

    Migrating to JUnit 5 and AssertJ

--- kotlin-datastructures/src/test/kotlin/com/baeldung/arrayinitialization/ArrayInitializationUnitTest.kt
+++ kotlin-datastructures/src/test/kotlin/com/baeldung/arrayinitialization/ArrayInitializationUnitTest.kt
@@ -1,12 +1,12 @@
 package com.baeldung.arrayinitialization
 
-import org.junit.Test
-import kotlin.test.assertEquals
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 
-class ArrayInitializationTest {
+class ArrayInitializationUnitTest {
 
     @Test
-    fun givenArrayOfStrings_thenValuesPopulated() {
+    fun `Given an array of strings, Then values are populated`() {
         val strings = arrayOf("January", "February", "March")
 
         assertEquals(3, strings.size)
@@ -14,7 +14,7 @@ class ArrayInitializationTest {
     }
 
     @Test
-    fun givenArrayOfIntegers_thenValuesPopulated() {
+    fun `Given an array of integers, Then values are populated`() {
         val integers = intArrayOf(1, 2, 3, 4)
 
         assertEquals(4, integers.size)
@@ -22,7 +22,7 @@ class ArrayInitializationTest {
     }
 
     @Test
-    fun givenArrayOfNulls_whenPopulated_thenValuesPresent() {
+    fun `Given an array of nulls, When populated, Then values are present`() {
         val array = arrayOfNulls<Number>(5)
 
         for (i in array.indices) {
@@ -33,14 +33,14 @@ class ArrayInitializationTest {
     }
 
     @Test
-    fun whenGeneratorUsed_thenValuesPresent() {
+    fun `When using generator, Then values are present`() {
         val generatedArray = IntArray(10) { i -> i * i }
 
         assertEquals(81, generatedArray[9])
     }
 
     @Test
-    fun whenStringGenerated_thenValuesPresent() {
+    fun `When generating strings, Then values are present`() {
         val generatedStringArray = Array(10) { i -> "Number of index: $i" }
 
         assertEquals("Number of index: 0", generatedStringArray[0])
