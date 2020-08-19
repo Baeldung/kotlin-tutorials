c9e62b369089192bebda4cf6d50b9e3d071a39de
Mona Mohamadinia <mona.mohamadinia@gmail.com>
2020-08-19 12:29:46 +0430

    Applying Requested Changes

--- kotlin-core/src/test/java/com/baeldung/unsigned/UnsignedUnitTest.kt
+++ kotlin-core/src/test/java/com/baeldung/unsigned/UnsignedUnitTest.kt
@@ -6,6 +6,27 @@ import org.junit.jupiter.api.Test
 @ExperimentalUnsignedTypes
 class UnsignedUnitTest {
 
+    val uByte: UByte = 42u
+    val uShort: UShort = 42u
+    val uInt: UInt = 42U
+    val uLong: ULong = 42u
+
+    val inferredUInt = 42U
+    val inferredULong = 0xFFFF_FFFF_FFFFu
+    val explicitULong = 42uL
+
+    val ba = UByteArray(42)
+    val ba2 = ubyteArrayOf(42u, 43u)
+
+    val anInt = 42
+    val converted = anInt.toUInt()
+
+    val toUIntArray = intArrayOf(-1, -2).toUIntArray()
+
+    init {
+        uintArrayOf(42u, 43u).map { it * it }.forEach { println(it) }
+    }
+
     @Test
     fun `Unsigned integers support the same set of operations`() {
         assertEquals(3u, 2u + 1u)
