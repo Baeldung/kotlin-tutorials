4d6b5373062345ddf834cd617f38367576652f3c
tpurdeva <ramprasad.devarakonda@waitrose.co.uk>
2020-11-05 20:38:37 +0000

    KTLN-38 | Examples For Kotlin Loops Article

---
+++ kotlin-core-2/src/main/kotlin/com/baeldung/loops/DoWhileLoop.kt
@@ -0,0 +1,12 @@
+package com.baeldung.loops
+
+fun main() {
+    var counter = 0
+    do {
+        println("do..while counter: " + counter++)
+    } while (counter < 5)
+
+do {
+    println("This will be printed exactly once")
+} while (false)
+}
