97e13a21b5b4bab772f5eee1e219fa15beab464a
tpurdeva <ramprasad.devarakonda@waitrose.co.uk>
2020-11-15 22:21:27 +0000

    KTLN-38 | Code Review Feedback Implementation

--- kotlin-core-2/src/main/kotlin/com/baeldung/loops/Repeat.kt
+++ kotlin-core-2/src/main/kotlin/com/baeldung/loops/Repeat.kt
@@ -2,7 +2,9 @@ package com.baeldung.loops
 
 fun main() {
 
-    repeat(2) { println("Hello World") }
+    repeat(2) {
+        println("Hello World")
+    }
 
     repeat(2) { index ->
         println("Iteration ${index + 1}: Hello World")
