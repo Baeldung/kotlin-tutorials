5776a2c3d756b6fe0d816cbc1b15550f79756597
Anirban Chatterjee <canirban99@gmail.com>
2020-11-21 01:04:40 +0100

    Added unit tests

--- kotlin-core/src/test/kotlin/com/baeldung/assertFailsWith/AssertFailsWithFunctionTest.kt
+++ kotlin-core/src/test/kotlin/com/baeldung/assertFailsWith/AssertFailsWithFunctionTest.kt
@@ -1,6 +1,33 @@
 package com.baeldung.assertFailsWith
 
+import org.junit.jupiter.api.Test
+import kotlin.test.assertFailsWith
+
 class AssertFailsWithFunctionTest {
 
     private val classUnderTest: AssertFailsWithFunction = AssertFailsWithFunction()
+
+    @Test
+    fun givenIllegalArgument_thenThrowsException() {
+        assertFailsWith<ArrayIndexOutOfBoundsException>(
+                message = "Array index is out of bound",
+                block = { classUnderTest.assertFailsWithMessage() }
+        )
+    }
+
+    @Test
+    fun givenInvalidFormat_thenThrowsException() {
+        assertFailsWith(
+                exceptionClass = NumberFormatException::class,
+                block = { classUnderTest.assertFailsWithExceptionClass() }
+        )
+    }
+
+    @Test
+    fun givenInvalidOperation_thenThrowsException() {
+        assertFailsWith(
+                exceptionClass = ArithmeticException::class,
+                message = "Invalid arithmetic operation",
+                block = { classUnderTest.assertFailsWithMessageAndExceptionClass() })
+    }
 }
