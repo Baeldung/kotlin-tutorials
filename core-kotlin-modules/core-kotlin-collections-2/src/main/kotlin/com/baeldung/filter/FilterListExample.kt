6b99ce85558ad144f9c44a2b174df337248be524
Anirban Chatterjee <canirban99@gmail.com>
2021-05-20 18:29:23 +0200

    Add list filter methods

--- core-kotlin-modules/core-kotlin-collections-2/src/main/kotlin/com/baeldung/filter/FilterListExample.kt
+++ core-kotlin-modules/core-kotlin-collections-2/src/main/kotlin/com/baeldung/filter/FilterListExample.kt
@@ -1,4 +1,55 @@
 package com.baeldung.filter
 
 class FilterListExample {
+
+    private val countries = listOf("Germany", "India", "Japan", "Brazil", "Australia")
+
+    fun filterList(): List<String> {
+        return countries.filter { it.length > 5 }
+    }
+
+    fun filterToList(): List<String> {
+        var list = mutableListOf("United States", "Canada")
+        return countries.filterTo(list, { it.length > 5 })
+    }
+
+    fun filterNotList(): List<String> {
+        return countries.filterNot { it.length > 5 }
+    }
+
+    fun filterNotToList(): List<String> {
+        var list = mutableListOf("United States", "Canada")
+        return countries.filterNotTo(list, { it.length > 5 })
+    }
+
+    fun filterIndexedList(): List<String> {
+        return countries.filterIndexed { index, it -> index != 3 && it.length > 5 }
+    }
+
+    fun filterIndexedToList(): List<String> {
+        var list = mutableListOf("United States", "Canada")
+        return countries.filterIndexedTo(list, { index, it -> index != 3 && it.length > 5 })
+    }
+
+    fun filterIsInstanceList(): List<Int> {
+        val countryCode = listOf("Germany", 49, null, "India", 91, "Japan", 81, "Brazil", null, "Australia", 61)
+        return countryCode.filterIsInstance<Int>()
+    }
+
+    fun filterIsInstanceToList(): List<Int> {
+        val countryCode = listOf("Germany", 49, null, "India", 91, "Japan", 81, "Brazil", null, "Australia", 61)
+        var list = mutableListOf(1, 24)
+        return countryCode.filterIsInstanceTo(list)
+    }
+
+    fun filterNotNullList(): List<String> {
+        val countries = listOf("Germany", "India", null, "Japan", "Brazil", null, "Australia")
+        return countries.filterNotNull()
+    }
+
+    fun filterNotNullToList(): List<String> {
+        val countries = listOf("Germany", "India", null, "Japan", "Brazil", null, "Australia")
+        var list = mutableListOf("United States", "Canada")
+        return countries.filterNotNullTo(list)
+    }
 }
--- core-kotlin-modules/core-kotlin-collections-2/src/test/kotlin/com/baeldung/filter/FilterListExampleUnitTest.kt
+++ core-kotlin-modules/core-kotlin-collections-2/src/test/kotlin/com/baeldung/filter/FilterListExampleUnitTest.kt
@@ -1,4 +1,94 @@
 package com.baeldung.filter
 
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
 class FilterListExampleUnitTest {
+
+    private val classUnderTest: FilterListExample = FilterListExample()
+
+    @Test
+    fun whenFilterList_thenContainsElements() {
+        val list = classUnderTest.filterList()
+        assertEquals(3, list.size)
+        assertTrue(list.contains("Brazil"))
+        assertTrue(list.contains("Germany"))
+    }
+
+    @Test
+    fun whenFilterToList_thenContainsElements() {
+        val list = classUnderTest.filterToList()
+        assertEquals(5, list.size)
+        assertTrue(list.contains("Canada"))
+        assertTrue(list.contains("Germany"))
+        assertTrue(list.contains("Brazil"))
+    }
+
+    @Test
+    fun whenFilterNotList_thenContainsElements() {
+        val list = classUnderTest.filterNotList()
+        assertEquals(2, list.size)
+        assertTrue(list.contains("India"))
+        assertTrue(list.contains("Japan"))
+    }
+
+    @Test
+    fun whenFilterNotToList_thenContainsElements() {
+        val list = classUnderTest.filterNotToList()
+        assertEquals(4, list.size)
+        assertTrue(list.contains("Canada"))
+        assertTrue(list.contains("India"))
+        assertTrue(list.contains("Japan"))
+    }
+
+    @Test
+    fun whenFilterIndexedList_thenContainsElements() {
+        val list = classUnderTest.filterIndexedList()
+        assertEquals(2, list.size)
+        assertTrue(list.contains("Germany"))
+        assertTrue(list.contains("Australia"))
+    }
+
+    @Test
+    fun whenFilterIndexedToList_thenContainsElements() {
+        val list = classUnderTest.filterIndexedToList()
+        assertEquals(4, list.size)
+        assertTrue(list.contains("Germany"))
+        assertTrue(list.contains("Australia"))
+        assertTrue(list.contains("Canada"))
+        assertTrue(list.contains("United States"))
+    }
+
+    @Test
+    fun whenFilterIsInstanceList_thenContainsElements() {
+        val list = classUnderTest.filterIsInstanceList()
+        assertEquals(4, list.size)
+        assertTrue(list.contains(49))
+        assertTrue(list.contains(91))
+    }
+
+    @Test
+    fun whenFilterIsInstanceToList_thenContainsElements() {
+        val list = classUnderTest.filterIsInstanceToList()
+        assertEquals(6, list.size)
+        assertTrue(list.contains(49))
+        assertTrue(list.contains(24))
+    }
+
+    @Test
+    fun whenFilterNotNullList_thenContainsElements() {
+        val list = classUnderTest.filterNotNullList()
+        assertEquals(5, list.size)
+        assertTrue(list.contains("Brazil"))
+        assertTrue(list.contains("Japan"))
+    }
+
+    @Test
+    fun whenFilterNotNullToList_thenContainsElements() {
+        val list = classUnderTest.filterNotNullToList()
+        assertEquals(7, list.size)
+        assertTrue(list.contains("Brazil"))
+        assertTrue(list.contains("Canada"))
+    }
 }
