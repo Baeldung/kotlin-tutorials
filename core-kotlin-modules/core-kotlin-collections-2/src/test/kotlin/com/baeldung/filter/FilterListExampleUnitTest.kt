891a6c7c924e9a3cb803a72e3e25c7b96a7e95d5
Anirban Chatterjee <canirban99@gmail.com>
2021-06-25 00:50:11 +0200

    Improve unit tests

--- core-kotlin-modules/core-kotlin-collections-2/src/test/kotlin/com/baeldung/filter/FilterListExampleUnitTest.kt
+++ core-kotlin-modules/core-kotlin-collections-2/src/test/kotlin/com/baeldung/filter/FilterListExampleUnitTest.kt
@@ -12,127 +12,90 @@ class FilterListExampleUnitTest {
     fun whenFilterList_thenContainsElements() {
         val list = classUnderTest.filterList()
         assertEquals(3, list.size)
-        assertTrue(list.contains("Germany"))
-        assertTrue(list.contains("Brazil"))
-        assertTrue(list.contains("Australia"))
+        assertTrue(list.containsAll(listOf("Germany","Brazil","Australia")))
     }
 
     @Test
     fun whenFilterToList_thenContainsElements() {
         val list = classUnderTest.filterToList()
         assertEquals(5, list.size)
-        assertTrue(list.contains("United States"))
-        assertTrue(list.contains("Canada"))
-        assertTrue(list.contains("Germany"))
-        assertTrue(list.contains("Brazil"))
-        assertTrue(list.contains("Australia"))
+        assertTrue(list.containsAll(listOf("United States","Canada","Germany","Brazil","Australia")))
     }
 
     @Test
     fun whenFilterNotList_thenContainsElements() {
         val list = classUnderTest.filterNotList()
         assertEquals(2, list.size)
-        assertTrue(list.contains("India"))
-        assertTrue(list.contains("Japan"))
+        assertTrue(list.containsAll(listOf("India","Japan")))
     }
 
     @Test
     fun whenFilterNotToList_thenContainsElements() {
         val list = classUnderTest.filterNotToList()
         assertEquals(4, list.size)
-        assertTrue(list.contains("United States"))
-        assertTrue(list.contains("Canada"))
-        assertTrue(list.contains("India"))
-        assertTrue(list.contains("Japan"))
+        assertTrue(list.containsAll(listOf("United States","Canada","India","Japan")))
     }
 
     @Test
     fun whenFilterIndexedList_thenContainsElements() {
         val list = classUnderTest.filterIndexedList()
         assertEquals(2, list.size)
-        assertTrue(list.contains("Germany"))
-        assertTrue(list.contains("Australia"))
+        assertTrue(list.containsAll(listOf("Germany","Australia")))
     }
 
     @Test
     fun whenFilterIndexedToList_thenContainsElements() {
         val list = classUnderTest.filterIndexedToList()
         assertEquals(4, list.size)
-        assertTrue(list.contains("United States"))
-        assertTrue(list.contains("Canada"))
-        assertTrue(list.contains("Germany"))
-        assertTrue(list.contains("Australia"))
+        assertTrue(list.containsAll(listOf("United States","Canada","Germany","Australia")))
     }
 
     @Test
     fun whenFilterIsInstanceList_thenContainsElements() {
         val list = classUnderTest.filterIsInstanceList()
         assertEquals(4, list.size)
-        assertTrue(list.contains(49))
-        assertTrue(list.contains(91))
-        assertTrue(list.contains(81))
-        assertTrue(list.contains(61))
+        assertTrue(list.containsAll(listOf(49,91,81,61)))
     }
 
     @Test
     fun whenFilterIsInstanceToList_thenContainsElements() {
         val list = classUnderTest.filterIsInstanceToList()
         assertEquals(6, list.size)
-        assertTrue(list.contains(1))
-        assertTrue(list.contains(24))
-        assertTrue(list.contains(49))
-        assertTrue(list.contains(91))
-        assertTrue(list.contains(81))
-        assertTrue(list.contains(61))
+        assertTrue(list.containsAll(listOf(1,24,49,91,81,61)))
     }
 
     @Test
     fun whenFilterNotNullList_thenContainsElements() {
         val list = classUnderTest.filterNotNullList()
         assertEquals(5, list.size)
-        assertTrue(list.contains("Germany"))
-        assertTrue(list.contains("India"))
-        assertTrue(list.contains("Japan"))
-        assertTrue(list.contains("Brazil"))
-        assertTrue(list.contains("Australia"))
-
+        assertTrue(list.containsAll(listOf("Germany","India","Japan","Brazil","Australia")))
     }
 
     @Test
     fun whenFilterNotNullToList_thenContainsElements() {
         val list = classUnderTest.filterNotNullToList()
         assertEquals(7, list.size)
-        assertTrue(list.contains("United States"))
-        assertTrue(list.contains("Canada"))
-        assertTrue(list.contains("Germany"))
-        assertTrue(list.contains("India"))
-        assertTrue(list.contains("Japan"))
-        assertTrue(list.contains("Brazil"))
-        assertTrue(list.contains("Australia"))
+        assertTrue(list.containsAll(listOf("United States","Canada","Germany","India","Japan","Brazil","Australia")))
     }
 
     @Test
     fun whenFilterUsingIterator_thenContainsElements() {
         val list = classUnderTest.filterUsingIterator()
         assertEquals(2, list.size)
-        assertTrue(list.contains("India"))
-        assertTrue(list.contains("Japan"))
+        assertTrue(list.containsAll(listOf("India","Japan")))
     }
 
     @Test
     fun whenFilterUsingRemoveAll_thenContainsElements() {
         val list = classUnderTest.filterUsingRemoveAll()
         assertEquals(2, list.size)
-        assertTrue(list.contains("India"))
-        assertTrue(list.contains("Japan"))
+        assertTrue(list.containsAll(listOf("India","Japan")))
     }
 
     @Test
     fun whenFilterUsingRetainAll_thenContainsElements() {
         val list = classUnderTest.filterUsingRetainAll()
         assertEquals(3, list.size)
-        assertTrue(list.contains("Germany"))
-        assertTrue(list.contains("Brazil"))
-        assertTrue(list.contains("Australia"))
+        assertTrue(list.containsAll(listOf("Germany","Brazil","Australia")))
     }
 }
