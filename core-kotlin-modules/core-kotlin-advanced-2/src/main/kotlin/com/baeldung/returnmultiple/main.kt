d81f41537b935756384083cdca062fc385067f7d
Ali Dehghani <ali.dehghani.g@gmail.com>
2021-08-12 10:40:57 +0430

    rename the four values function

--- core-kotlin-modules/core-kotlin-advanced-2/src/main/kotlin/com/baeldung/returnmultiple/main.kt
+++ core-kotlin-modules/core-kotlin-advanced-2/src/main/kotlin/com/baeldung/returnmultiple/main.kt
@@ -6,7 +6,7 @@ import java.security.KeyPairGenerator
 
 fun twoPair(): Pair<String, Int> = "Ali" to 33
 fun threeValues(): Triple<String, Int, String> = Triple("Ali", 33, "Neka")
-fun fourVales() = arrayOf("Berlin", "Munich", "Amsterdam", "Madrid", "Vienna")
+fun fiveValues() = arrayOf("Berlin", "Munich", "Amsterdam", "Madrid", "Vienna")
 
 data class Pod(val name: String, val ip: InetAddress, val assignedNode: String)
 fun getUniquePod() = Pod("postgres", InetAddress.getLocalHost(), "Node 1")
@@ -21,7 +21,7 @@ fun main() {
     println("$name is $age years old")
 
     val (name2, age2, bornOn) = threeValues()
-    val (v1, v2, v3, v4, v5) = fourVales()
+    val (v1, v2, v3, v4, v5) = fiveValues()
     val (podName, ip, assignedNode) = getUniquePod()
     val (publicKey, privateKey) = getRsaKeyPair()
 }
