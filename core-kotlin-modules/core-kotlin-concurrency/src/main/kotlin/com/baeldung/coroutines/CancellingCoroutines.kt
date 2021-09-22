6fd7642a5c39331fbece82d65998fe22e08d808a
manojmohandas <manoj.mohandas14@gmail.com>
2021-09-22 23:32:36 +0530

    Cancelling Coroutine KT file added

---
+++ core-kotlin-modules/core-kotlin-concurrency/src/main/kotlin/com/baeldung/coroutines/CancellingCoroutines.kt
@@ -0,0 +1,70 @@
+package com.baeldung.coroutines
+
+import kotlinx.coroutines.*
+
+fun main() = runBlocking {
+    printPagesPeriodicCheck(100)
+    printPagesEnsureActive(100)
+    printPagesIsActive(100)
+}
+
+private suspend fun CoroutineScope.printPagesPeriodicCheck(count: Int) {
+    println("This function uses yield to periodically check if Job is cancelled.")
+    val job = launch {
+        repeat(count) {
+            yield()
+            try {
+                println("Printing page number $it")
+                delay(1000)
+                println("Page $it printed")
+            } catch (e: CancellationException) {
+                println("Print job cancel triggered")
+            }
+        }
+    }
+    delay(2000)
+    job.cancelAndJoin()
+    println("Print job cancelled")
+}
+
+private suspend fun CoroutineScope.printPagesEnsureActive(count: Int) {
+    println("This function uses ensureActive function call to check if Job is active.")
+    val job = launch {
+        repeat(count) {
+            ensureActive()
+            try {
+                println("Printing page number $it")
+                delay(1000)
+                println("Page $it printed")
+            } catch (e: CancellationException) {
+                println("Print job cancel triggered")
+            }
+        }
+    }
+    job.start()
+    delay(2000)
+    job.cancelAndJoin()
+    println("Print job cancelled")
+}
+
+
+private suspend fun CoroutineScope.printPagesIsActive(count: Int) {
+    println("This function uses isActive field to check if Job is active.")
+    val job = launch {
+        repeat(count) {
+            if (isActive) {
+                try {
+                    println("Printing page number $it")
+                    delay(1000)
+                    println("Page $it printed")
+                } catch (e: CancellationException) {
+                    println("Print job cancel triggered")
+                }
+            }
+        }
+    }
+    job.start()
+    delay(2000)
+    job.cancelAndJoin()
+    println("Print job cancelled")
+}
