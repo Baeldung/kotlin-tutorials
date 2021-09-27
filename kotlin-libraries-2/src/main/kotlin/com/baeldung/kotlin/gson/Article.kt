ebebffdeaf34447d92a764b14c4b6e8d23ffad0f
Diego Torres <dtorres.py@gmail.com>
2021-09-27 17:13:00 -0400

    KTLN-274: Removed unused test cases
    Signed-off-by: Diego Torres <dtorres.py@gmail.com>

--- kotlin-libraries-2/src/main/kotlin/com/baeldung/kotlin/gson/Article.kt
+++ kotlin-libraries-2/src/main/kotlin/com/baeldung/kotlin/gson/Article.kt
@@ -1,13 +1,8 @@
 package com.baeldung.kotlin.gson
 
-import com.google.gson.annotations.SerializedName
-
 data class Article(
-    @SerializedName("article_title")
     var title: String,
-    @SerializedName("article_category")
     var category: String,
-    @SerializedName("article_views")
     var views: Int
 ) {
 }
--- kotlin-libraries-2/src/test/kotlin/com/baeldung/kotlin/gson/GsonUnitTest.kt
+++ kotlin-libraries-2/src/test/kotlin/com/baeldung/kotlin/gson/GsonUnitTest.kt
@@ -68,22 +68,7 @@ class GsonUnitTest {
         val serialized = Gson().toJson(authors)
 
         val json =
-            """[{"name":"John","type":"Technical Author","articles":[{"article_title":"Streams in Java","article_category":"Java","article_views":3},{"article_title":"Lambda Expressions","article_category":"Java","article_views":5}]},{"name":"Jane","type":"Technical Author","articles":[{"article_title":"Functional Interfaces","article_category":"Java","article_views":2}]},{"name":"William","type":"Technical Editor"}]"""
+            """[{"name":"John","type":"Technical Author","author_articles":[{"title":"Streams in Java","category":"Java","views":3},{"title":"Lambda Expressions","category":"Java","views":5}]},{"name":"Jane","type":"Technical Author","author_articles":[{"title":"Functional Interfaces","category":"Java","views":2}]},{"name":"William","type":"Technical Editor"}]"""
         assertEquals(serialized, json)
-    }
-
-    @Test
-    fun deserializeObjectListWithNonMatchingKeysTest() {
-
-        val json =
-            """[{"name":"John","type":"Technical Author","articles":[{"article_title":"Streams in Java","article_category":"Java","article_views":3},{"article_title":"Lambda Expressions","article_category":"Java","article_views":5}]},{"name":"Jane","type":"Technical Author","articles":[{"article_title":"Functional Interfaces","article_category":"Java","article_views":2}]},{"name":"William","type":"Technical Editor"}]"""
-        val typeToken = object : TypeToken<List<Author>>() {}.type
-        val authors = Gson().fromJson<List<Author>>(json, typeToken)
-
-
-        assertThat(authors).isNotEmpty
-        assertThat(authors).hasSize(3)
-        assertThat(authors).anyMatch { a -> a.name == "John" }
-        assertThat(authors).anyMatch { a -> a.type == "Technical Editor" }
     }
 }
