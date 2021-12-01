9f4d394f0917d8b672e648f6c045afe4a14beb7b
davisday227 <davisday227@gmail.com>
2021-12-01 23:38:14 +0800

    modify unit test for singleton class

--- kotlin-patterns/src/test/kotlin/com/baeldung/singleton/SingletonTest.kt
+++ kotlin-patterns/src/test/kotlin/com/baeldung/singleton/SingletonTest.kt
@@ -2,14 +2,15 @@ package com.baeldung.singleton
 
 import org.junit.Assert
 import org.junit.jupiter.api.Test
-import java.util.concurrent.Executors
 
 class SingletonTest {
     @Test
     fun `Get singleton object id and assert equals`() {
         val objectId1 = Singleton.toString()
         val objectId2 = Singleton.toString()
-
+        println("Test1:")
+        println(objectId1)
+        println(objectId2)
         Assert.assertEquals(objectId1, objectId2)
     }
 
@@ -17,7 +18,15 @@ class SingletonTest {
     fun `Get const in singleton and assert same`() {
         val constValue1 = Singleton.name
         val constValue2 = Singleton.name
-
+        println("Test2:")
+        println(constValue1)
+        println(constValue2)
         Assert.assertSame(constValue1, constValue2)
+    }
+
+    @Test
+    fun `Call singleton function outside and get the right result`() {
+        val result = Singleton.add(3, 5);
+        Assert.assertEquals(result, 3 + 5)
     }
 }
--- kotlin-patterns/src/test/kotlin/com/baeldung/singleton/SingletonWithParamTest.kt
+++ kotlin-patterns/src/test/kotlin/com/baeldung/singleton/SingletonWithParamTest.kt
@@ -2,42 +2,15 @@ package com.baeldung.singleton
 
 import org.junit.Assert
 import org.junit.jupiter.api.Test
-import java.util.*
 import java.util.concurrent.Executors
-import kotlin.math.sin
-import kotlin.random.Random
 
 /**
  *
  */
 class SingletonWithParamTest {
     @Test
-    fun `Get two singleton object and assert them same`() {
-        val param = "Test Param"
-        val singletonA = SingletonWithParam.getInstance(param)
-        val singletonB = SingletonWithParam.getInstance(param)
-
-        Assert.assertSame(singletonA, singletonB)
-        Assert.assertEquals(singletonA.toString(), singletonB.toString())
-    }
-
-    @Test
-    fun `Get two singleton object with different param and assert all param same`() {
-        val paramA = "Test Param"
-        val singletonA = SingletonWithParam.getInstance(paramA)
-
-        val paramB = "Test ParamB"
-        val singletonB = SingletonWithParam.getInstance(paramB)
-
-        Assert.assertSame(paramA, singletonA.paramValue)
-        Assert.assertSame(singletonA.paramValue, singletonB.paramValue)
-        Assert.assertNotSame(paramB, singletonA.paramValue)
-        Assert.assertNotEquals(paramB, singletonA.paramValue)
-    }
-
-    @Test
     fun `Create multi singleton and test thread safe`() {
-        val param: String = "InitParam"
+        val param = "Test Param"
         val singletonSet = mutableSetOf<SingletonWithParam>()
 
         val executor = Executors.newFixedThreadPool(5)
