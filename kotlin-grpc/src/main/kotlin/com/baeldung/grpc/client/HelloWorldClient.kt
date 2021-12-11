883bc98d5ecd0f14e5c2f40b1988c34760e0dee5
maryarm <maryam@nilin.co>
2021-12-11 13:31:42 +0100

    KTLN-322: Remove extra spaces

--- kotlin-grpc/src/main/kotlin/com/baeldung/grpc/client/HelloWorldClient.kt
+++ kotlin-grpc/src/main/kotlin/com/baeldung/grpc/client/HelloWorldClient.kt
@@ -9,18 +9,14 @@ fun helloClient() {
     val channel = ManagedChannelBuilder.forAddress("localhost", 15001)
         .usePlaintext()
         .build()
-
     val stub = HelloServiceGrpc.newBlockingStub(channel)
-
     val response = stub.hello(HelloRequest.newBuilder().setName("Baeldung").build())
-
-    print(response.message)
+    println(response.message)
 }
 
 fun main(args: Array<String>) {
     helloClient()
 }
-
 
 
 
