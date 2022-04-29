8204f9cd7d054562ae90b76ae66c52559981b3f8
Behnam Khani <behnamkhani@hotmail.com>
2022-04-29 20:21:40 +0430

    Wrap inside class

--- core-kotlin-modules/core-kotlin-lang-oop/src/main/kotlin/com/baeldung/universalObjects/DataClass.kt
+++ core-kotlin-modules/core-kotlin-lang-oop/src/main/kotlin/com/baeldung/universalObjects/DataClass.kt
@@ -1,8 +1,5 @@
 fun main() {
-    val storage1 = Storage("980Pro M.2 NVMe", 1024)
-    val storage2 = Storage("980Pro M.2 NVMe", 1024)
-    println(storage1 == storage2)
+    println("Baeldung" == "Baeldung")
 }
 
-data class Storage(private val name: String, private val capacity: Int)
 
