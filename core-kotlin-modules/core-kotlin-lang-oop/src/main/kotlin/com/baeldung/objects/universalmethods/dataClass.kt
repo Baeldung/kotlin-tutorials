238c6adaaf33b5a00703a4774579b5bfa307e4e8
Behnam Khani <behnamkhani@hotmail.com>
2022-04-29 19:18:04 +0430

    Compare objects

--- core-kotlin-modules/core-kotlin-lang-oop/src/main/kotlin/com/baeldung/objects/universalmethods/dataClass.kt
+++
@@ -1,9 +0,0 @@
-package com.baeldung.objects.universalmethods
-
-fun main() {
-    data class Storage(private val name: String, private val capacity: Int)
-
-    val storage1 = Storage("980Pro M.2 NVMe", 1024)
-    val storage2 = Storage("980Pro M.2 NVMe", 1024)
-    println(storage1 == storage2)
-}
