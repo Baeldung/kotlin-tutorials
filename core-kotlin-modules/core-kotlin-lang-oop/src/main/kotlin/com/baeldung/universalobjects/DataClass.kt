28f60ed2fe18cc408d7def691c648a5db99cab41
Behnam Khani <behnamkhani@hotmail.com>
2022-04-29 20:28:04 +0430

    Wrap inside class

--- core-kotlin-modules/core-kotlin-lang-oop/src/main/kotlin/com/baeldung/universalobjects/DataClass.kt
+++ core-kotlin-modules/core-kotlin-lang-oop/src/main/kotlin/com/baeldung/universalobjects/DataClass.kt
@@ -1,7 +1,19 @@
 package com.baeldung.universalobjects
 
-fun main() {
-    println("Baeldung" == "Baeldung")
-}
+import kotlinx.coroutines.*
 
+fun main() = runBlocking {
+    val job: Job = GlobalScope.launch {
+        for (i in 1..45)
+            println("Fibonacci of $i is ${fibonacci(i)}")
+    }
+
+    delay(2000)
+    job.cancel()
+    job.join()
+}
+private fun fibonacci(num: Int): Int {
+    return if (num == 0 || num == 1) num
+    else fibonacci(num - 1) + fibonacci(num - 2)
+}
 
