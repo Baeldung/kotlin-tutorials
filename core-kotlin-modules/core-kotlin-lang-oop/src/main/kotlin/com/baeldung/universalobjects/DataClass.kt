e15ef8a3721a154a4421a95b97132620d27a76d4
Behnam Khani <behnamkhani@hotmail.com>
2022-05-09 11:00:58 +0430

    Remove verbose classes

--- core-kotlin-modules/core-kotlin-lang-oop/src/main/kotlin/com/baeldung/universalobjects/DataClass.kt
+++ core-kotlin-modules/core-kotlin-lang-oop/src/main/kotlin/com/baeldung/universalobjects/DataClass.kt
@@ -1,15 +1,12 @@
 package com.baeldung.universalObjects
 
 private fun main() {
-    val storage1 = DataClass.Storage("980Pro M.2 NVMe", 1024)
-    val storage2 = DataClass.Storage("980Pro M.2 NVMe", 1024)
-    val dataClass = DataClass()
-    println(dataClass.areEqual(storage1, storage2))
+    val storage1 = DataClassStorage("980Pro M.2 NVMe", 1024)
+    val storage2 = DataClassStorage("980Pro M.2 NVMe", 1024)
+
+    println(storage1 == storage2)
 }
-class DataClass{
-    fun areEqual(obj1: Storage, obj2: Storage): Boolean {
-        return obj1 == obj2
-    }
-    data class Storage(private val name: String, private val capacity: Int)
-}
+
+data class DataClassStorage(private val name: String, private val capacity: Int)
+
 
