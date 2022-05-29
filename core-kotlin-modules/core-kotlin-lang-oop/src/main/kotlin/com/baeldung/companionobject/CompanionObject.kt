c34f3dff0a45016ad5d7112b2889f26b00fe225b
Behnam Khani <behnamkhani@hotmail.com>
2022-05-29 12:25:10 +0430

    Use companion object

---
+++ core-kotlin-modules/core-kotlin-lang-oop/src/main/kotlin/com/baeldung/companionobject/CompanionObject.kt
@@ -0,0 +1,62 @@
+package com.baeldung.companionobject
+
+class CompanionObject {
+    class ClassName {
+        fun anotherFun() {
+            println(propertyName)
+        }
+
+        companion object {
+            const val propertyName: String = "Something..."
+            fun funName() {
+                //...
+            }
+        }
+    }
+
+    class MyClass {
+        companion object Factory {
+            fun createInstance(): MyClass = MyClass()
+        }
+    }
+
+
+    abstract class AbstractFactory {
+        interface Theme {
+            fun someFunction(): String
+        }
+
+        abstract class FactoryCreator {
+            abstract fun produce(): Theme
+        }
+
+        class FirstRelatedClass : Theme {
+            companion object Factory : FactoryCreator() {
+                override fun produce() = FirstRelatedClass()
+            }
+
+            override fun someFunction(): String {
+                return "I am from the first factory."
+            }
+        }
+
+        class SecondRelatedClass : Theme {
+            companion object Factory : FactoryCreator() {
+                override fun produce() = SecondRelatedClass()
+            }
+
+            override fun someFunction(): String {
+                return "I am from the second factory."
+            }
+        }
+    }
+}
+
+
+fun main() {
+    val firstFactory: CompanionObject.AbstractFactory.FactoryCreator = CompanionObject.AbstractFactory.FirstRelatedClass.Factory
+    println(firstFactory.produce().someFunction())
+
+    val secondFactory: CompanionObject.AbstractFactory.FactoryCreator = CompanionObject.AbstractFactory.SecondRelatedClass.Factory
+    println(secondFactory.produce().someFunction())
+}
