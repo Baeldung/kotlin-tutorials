4238c454052b1841209d7c152b36c10fff45aaf0
Vinay Wadhwa <vinaywadhwa@gmail.com>
2022-06-06 17:29:04 +0530

    correction in code-comments for KTLN-355

--- core-kotlin-modules/core-kotlin-4/src/test/kotlin/com/baeldung/vararg_spread/VarargSpread.kt
+++ core-kotlin-modules/core-kotlin-4/src/test/kotlin/com/baeldung/vararg_spread/VarargSpread.kt
@@ -5,14 +5,14 @@ import org.junit.jupiter.api.Test
 
 class VarargSpread {
     /**
-     * * Concatenates the strings passed as vararg parameters and returns the result
+     * Concatenates the strings passed as vararg parameters and returns the result
      */
     private fun concat(vararg strings: String): String {
         return strings.fold("") { acc, next -> "$acc$next" }
     }
 
     /**
-     * * Concatenates the strings passed as vararg parameters and returns the result
+     * Concatenates the strings passed as vararg parameters and returns the result
      */
     private fun concat2(vararg strings: String, initialValue: String = "01"): String {
         return strings.fold(initialValue) { acc, next -> "$acc$next" }
@@ -26,7 +26,7 @@ class VarargSpread {
     }
 
     @Test
-    fun `given array when passed to sum then should return correct sum`() {
+    fun `given array when passed to concat functions then should return concatenated string`() {
         val strings = arrayOf("ab", "cd")
         val moreStrings = arrayOf("gh", "ij")
         val listOfStrings = listOf("ab", "cd")
