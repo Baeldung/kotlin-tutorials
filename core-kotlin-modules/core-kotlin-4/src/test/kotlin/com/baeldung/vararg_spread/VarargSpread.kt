12cbadb2f14b53a2d0f47a86ef617c88beca547f
Vinay Wadhwa <vinaywadhwa@gmail.com>
2022-06-06 17:13:23 +0530

    correction in code-comments for KTLN-355

--- core-kotlin-modules/core-kotlin-4/src/test/kotlin/com/baeldung/vararg_spread/VarargSpread.kt
+++ core-kotlin-modules/core-kotlin-4/src/test/kotlin/com/baeldung/vararg_spread/VarargSpread.kt
@@ -5,21 +5,21 @@ import org.junit.jupiter.api.Test
 
 class VarargSpread {
     /**
-     * Returns the sum of all integers passed as vararg parameters
+     * * Concatenates the strings passed as vararg parameters and returns the result
      */
     private fun concat(vararg strings: String): String {
         return strings.fold("") { acc, next -> "$acc$next" }
     }
 
     /**
-     * Returns the sum of all integers passed as vararg parameters
+     * * Concatenates the strings passed as vararg parameters and returns the result
      */
     private fun concat2(vararg strings: String, initialValue: String = "01"): String {
         return strings.fold(initialValue) { acc, next -> "$acc$next" }
     }
 
     /**
-     * Returns the sum of all integers passed as vararg parameters
+     * Concatenates the strings passed as vararg parameters and returns the result
      */
     private fun concat3(vararg strings: String, initialValue: String): String {
         return strings.fold(initialValue) { acc, next -> "$acc$next" }
