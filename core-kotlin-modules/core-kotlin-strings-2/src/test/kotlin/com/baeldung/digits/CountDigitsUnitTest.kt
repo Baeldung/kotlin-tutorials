28462d5167f2909e2998f572753d6a7f4bb3830d
Diego Torres <dtorres.py@gmail.com>
2022-06-13 21:52:57 -0400

    KTLN-365: Unit tests for count digits
    Signed-off-by: Diego Torres <dtorres.py@gmail.com>

---
+++ core-kotlin-modules/core-kotlin-strings-2/src/test/kotlin/com/baeldung/digits/CountDigitsUnitTest.kt
@@ -0,0 +1,76 @@
+package com.baeldung.digits
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class CountDigitsUnitTest {
+
+    private fun Int.countDigits() = when (this) {
+        0 -> 1
+        else -> {
+            var count = 0
+            var value = this
+
+            do {
+                value /= 10; count++
+            } while (value != 0)
+
+            count
+        }
+    }
+
+    private fun CharSequence.countDigits(): Int = when (this) {
+        "" -> 0
+        else -> this.count { it.isDigit() }
+    }
+
+    private fun CharSequence.countDigitsCustom(): Int = when (this) {
+        "" -> 0
+        else -> this.count { it in ("0123456789") }
+    }
+
+    private fun CharSequence.countDigitsRegex(): Int = when (this) {
+        "" -> 0
+        else -> this.length - this.replace(Regex("\\d"), "").length
+    }
+
+    @Test
+    fun `given a value when counting digits then number of digits should be returned`() {
+
+        val number = 12345
+
+        assertThat(number.toString().countDigits()).isNotNull().isEqualTo(5)
+    }
+
+    @Test
+    fun `given a negative value when counting digits then number of digits should be returned`() {
+
+        val number = -12345
+
+        assertThat(number.toString().countDigitsCustom()).isNotNull().isEqualTo(5)
+    }
+
+    @Test
+    fun `given a double value when counting digits then number of digits should be returned`() {
+
+        val number = 1234.509
+
+        assertThat(number.toString().countDigitsRegex()).isNotNull().isEqualTo(7)
+    }
+
+    @Test
+    fun `given a negative double value when counting digits then number of digits should be returned`() {
+
+        val number = -1234.509
+
+        assertThat(number.toString().countDigitsRegex()).isNotNull().isEqualTo(7)
+    }
+
+    @Test
+    fun `given an integer value when counting digits then number of digits should be returned`() {
+
+        val number = -123450
+
+        assertThat(number.countDigits()).isNotNull().isEqualTo(6)
+    }
+}
