388d5513852cdb7c1cde9069ac0c660c2ab4aac2
Lei Zhang <zhanglei728@gmail.com>
2022-07-30 00:51:13 +0800

    KTLN-160 coroutines: runBlocking vs coroutineScope

--- core-kotlin-modules/core-kotlin-5/README.md
+++ core-kotlin-modules/core-kotlin-5/README.md
@@ -1,3 +1,3 @@
-
 ### Relevant Articles:
 - [Pad a Number in Kotlin](https://www.baeldung.com/kotlin/pad-number)
+- [Coroutines: runBlocking vs coroutineScope](https://www.baeldung.com/kotlin/coroutines-runblocking-vs-coroutinescope)
---
+++ core-kotlin-modules/core-kotlin-5/src/main/kotlin/com/baeldung/coroutine/demo/CancellationDemo.kt
@@ -0,0 +1,45 @@
+package com.baeldung.coroutine.demo
+
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlin.system.measureTimeMillis
+
+fun main() = runBlocking {
+    val cancelCoroutineScopeTime = measureTimeMillis {
+        cancelCoroutineScope()
+    }
+    println("cancelCoroutineScopeTime = $cancelCoroutineScopeTime")
+
+    val cancelRunBlockingTime = measureTimeMillis {
+        cancelRunBlocking()
+    }
+    println("cancelCoroutineScopeTime = $cancelRunBlockingTime")
+}
+
+private fun cancelCoroutineScope() = runBlocking {
+    val job = launch {
+        coroutineScope {
+            println("Start coroutineScope...")
+            delay(500)
+            println("End coroutineScope...")
+        }
+    }
+    delay(100)
+    job.cancel()
+    job.join()
+}
+
+private fun cancelRunBlocking() = runBlocking {
+    val job = launch {
+        runBlocking {
+            println("Start runBlocking...")
+            delay(500)
+            println("End runBlocking...")
+        }
+    }
+    delay(100)
+    job.cancel()
+    job.join()
+}
