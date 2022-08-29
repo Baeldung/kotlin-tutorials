e8c660e6e75e3e70a866b30a03fa82746b3c2e2e
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2022-08-29 17:41:50 -0300

    Add code for FirstNElements
    KTLN-379

---
+++ core-kotlin-modules/core-kotlin-5/src/test/kotlin/com/baeldung/evenodd/FirstNElementsTest.kt
@@ -0,0 +1,44 @@
+package com.baeldung.evenodd
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertContentEquals
+
+class FirstNElementsTest {
+
+  @Test
+  fun `Given an array take first n elements`() {
+    val originalArray = intArrayOf(1, 2, 3, 4, 5)
+
+    val n = 3
+    val tempArray = IntArray(n)
+
+    for (i in 0 until n) {
+      tempArray[i] = originalArray[i]
+    }
+
+    assertContentEquals(intArrayOf(1, 2, 3), tempArray)
+  }
+
+  @Test
+  fun `Given an array take first n elements using kotlin function`() {
+    val originalArray = intArrayOf(1, 2, 3, 4, 5)
+
+    val n = 3
+
+    val tempArray: List<Int> = originalArray.take(n)
+
+    assertContentEquals(listOf(1, 2, 3), tempArray)
+  }
+
+  @Test
+  fun `Given a list take first n elements using kotlin function`() {
+    val originalList = listOf(1, 2, 3, 4, 5)
+
+    val n = 3
+
+    val tempList = originalList.take(n)
+
+    assertContentEquals(listOf(1, 2, 3), tempList)
+  }
+
+}
