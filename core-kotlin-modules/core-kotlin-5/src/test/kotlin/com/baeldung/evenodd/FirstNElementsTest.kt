aa3d115000bcf32818b3f77c2cd43d96e9d01e50
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2022-09-01 18:30:11 -0300

    Add code for FirstNElements
    KTLN-379

--- core-kotlin-modules/core-kotlin-5/src/test/kotlin/com/baeldung/evenodd/FirstNElementsTest.kt
+++ core-kotlin-modules/core-kotlin-5/src/test/kotlin/com/baeldung/evenodd/FirstNElementsTest.kt
@@ -47,4 +47,22 @@ class FirstNElementsTest {
     val array: Array<Int> = list.toTypedArray()
   }
 
+  @Test
+  fun `Given a list take elements while predicate`() {
+    val list = listOf(1, 2, 3, 4, 5)
+
+    val takenList = list.takeWhile { it < 4 }
+
+    assertContentEquals(takenList, listOf(1, 2, 3))
+  }
+
+  @Test
+  fun `Given a list take elements while predicate in an unsorted list`() {
+    val list = listOf(5, 4, 3, 2, 1)
+
+    val takenList = list.takeWhile { it < 4 }
+
+    assertContentEquals(takenList, listOf())
+  }
+
 }
