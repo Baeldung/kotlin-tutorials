486270cecad8ffd8eb9cbf01ffb881a53a450e19
Suresh Raghavan <6439979+nrsuresh@users.noreply.github.com>
2022-09-12 22:59:58 -0500

    KTLN-250: Kotlin & Spring Boot @ConfigurationProperties
    https://drafts.baeldung.com/wp-admin/post.php?post=141731&action=edit

--- spring-boot-kotlin-v2/src/test/kotlin/com/baeldung/configurationproperties/SpringBootV2ApplicationIntegrationTest.kt
+++ spring-boot-kotlin-v2/src/test/kotlin/com/baeldung/configurationproperties/SpringBootV2ApplicationIntegrationTest.kt
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
-class SpringBootV2ApplicationTest {
+class SpringBootV2ApplicationIntegrationTest {
 
     @Autowired
     lateinit var apiConfiguration: ApiConfiguration
