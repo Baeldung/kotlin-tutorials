d17596428685a23e14eb644a72793e6ce650becb
Kent@lhind.hp.g5 <kent.yuan@Gmail.com>
2022-09-15 12:57:16 +0200

    [staticInEnum] No Java 11 available on Jenkins?... :-(

--- core-kotlin-modules/core-kotlin-lang-oop-3/src/test/java/com/baeldung/staticFunInEnum/StaticMethodInEnumUnitTest.java
+++ core-kotlin-modules/core-kotlin-lang-oop-3/src/test/java/com/baeldung/staticFunInEnum/StaticMethodInEnumUnitTest.java
@@ -2,7 +2,7 @@ package com.baeldung.staticFunInEnum;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
+import java.util.Arrays;
 
 import static com.baeldung.staticFunInEnum.MagicNumber.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -12,10 +12,10 @@ public class StaticMethodInEnumUnitTest {
     @Test
     void givenKotlinEnum_WhenCallStaticMethod_ShouldGetExpectedResult() {
         // the greaterThan() function with @JvmStatic
-        assertEquals(List.of(THREE, FOUR, FIVE, SIX), MagicNumber.greaterThan(2));
+        assertEquals(Arrays.asList(THREE, FOUR, FIVE, SIX), MagicNumber.greaterThan(2));
 
         // calling it through the Companion object
-        assertEquals(List.of(THREE, FOUR, FIVE, SIX), MagicNumber.Companion.greaterThan(2));
+        assertEquals(Arrays.asList(THREE, FOUR, FIVE, SIX), MagicNumber.Companion.greaterThan(2));
 
         // the pickOneRandomly() function without @JvmStatic
         assertNotNull(MagicNumber.Companion.pickOneRandomly());
