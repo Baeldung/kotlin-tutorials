9b29a330d9a99997743355e7955f84fb71e273b3
andrsim66 <sevenander@gmail.com>
2022-09-21 09:47:29 +0200

    update examples after draft review

--- core-kotlin-modules/core-kotlin-5/src/main/kotlin/com/baeldung/dataclass/deepcopy/Models.kt
+++ core-kotlin-modules/core-kotlin-5/src/main/kotlin/com/baeldung/dataclass/deepcopy/Models.kt
@@ -4,10 +4,10 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Person(val firstName: String, var lastName: String) : Cloneable {
-    public override fun clone() = super.clone()
+    public override fun clone(): Person = super.clone() as Person
 }
 
 @Serializable
 data class Movie(val title: String, val year: Int, val director: Person, val actors: List<Person> = emptyList()) : Cloneable {
-    public override fun clone() = Movie(title, year, director.clone() as Person, actors.map { it.copy() })
+    public override fun clone() = Movie(title, year, director.clone(), actors.map { it.copy() })
 }
--- core-kotlin-modules/core-kotlin-5/src/test/kotlin/com/baeldung/dataclass/deepcopy/DeepCopyUnitTest.kt
+++ core-kotlin-modules/core-kotlin-5/src/test/kotlin/com/baeldung/dataclass/deepcopy/DeepCopyUnitTest.kt
@@ -1,8 +1,5 @@
 package com.baeldung.dataclass.deepcopy
 
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
 import org.junit.Test
 import org.junit.jupiter.api.Assertions.assertEquals
 import kotlin.test.assertNotEquals
@@ -58,31 +55,6 @@ class DeepCopyUnitTest {
         assertNotSame(movieCopy, movie)
         assertEquals(movieCopy, movie)
         assertNotSame(movieCopy.director, movie.director)
-
-        movie.director.lastName = "Brown"
-        assertNotEquals(movieCopy.director.lastName, movie.director.lastName)
-    }
-
-    @org.junit.jupiter.api.Test
-    fun givenComplexObject_whenSerializeDeserialize_thenStructurallyEqualAndDifferentReferenceAndDifferentInternalReferences() {
-        val movie = Movie(
-                "Avatar 2",
-                2022,
-                Person("James", "Cameron"),
-                listOf(
-                        Person("Sam", "Worthington"),
-                        Person("Zoe", "Saldana")
-                )
-        )
-
-        val json = Json { encodeDefaults = true }
-        val movieJson = json.encodeToString(movie)
-        val movieCopy = json.decodeFromString<Movie>(movieJson)
-
-        assertNotSame(movieCopy, movie)
-        assertEquals(movieCopy, movie)
-        assertNotSame(movieCopy.director, movie.director)
-        assertNotSame(movieCopy.actors[0], movie.actors[0])
 
         movie.director.lastName = "Brown"
         assertNotEquals(movieCopy.director.lastName, movie.director.lastName)
