ac7cbcdbebde474b3d70cdd4de31e26b36bd8d29
andrsim66 <sevenander@gmail.com>
2022-09-21 09:48:57 +0200

    Update Models.kt

--- core-kotlin-modules/core-kotlin-5/src/main/kotlin/com/baeldung/dataclass/deepcopy/Models.kt
+++ core-kotlin-modules/core-kotlin-5/src/main/kotlin/com/baeldung/dataclass/deepcopy/Models.kt
@@ -1,13 +1,9 @@
 package com.baeldung.dataclass.deepcopy
 
-import kotlinx.serialization.Serializable
-
-@Serializable
 data class Person(val firstName: String, var lastName: String) : Cloneable {
     public override fun clone(): Person = super.clone() as Person
 }
 
-@Serializable
 data class Movie(val title: String, val year: Int, val director: Person, val actors: List<Person> = emptyList()) : Cloneable {
     public override fun clone() = Movie(title, year, director.clone(), actors.map { it.copy() })
 }
