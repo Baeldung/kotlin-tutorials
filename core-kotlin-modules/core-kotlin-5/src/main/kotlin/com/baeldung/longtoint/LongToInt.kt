1d427cc6d9276991d6c333fcfb4b752b08ee5eed
Vinay Wadhwa <vinaywadhwa@gmail.com>
2022-09-29 15:40:09 +0530

    added test

--- core-kotlin-modules/core-kotlin-5/src/main/kotlin/com/baeldung/longtoint/LongToInt.kt
+++ core-kotlin-modules/core-kotlin-5/src/main/kotlin/com/baeldung/longtoint/LongToInt.kt
@@ -1,12 +1,6 @@
 package com.baeldung.longtoint
 
 class LongToInt {
-
-    private fun usingToInt() {
-        val longValue = 100L
-        val intValue = longValue.toInt()
-    }
-
     private fun Long.toIntOrNull(): Int? {
         return if (this >= Int.MIN_VALUE && this <= Int.MAX_VALUE) {
             toInt()
