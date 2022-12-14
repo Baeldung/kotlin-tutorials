242b98b19566f6b65bb1bfd364fe74fa034c31a7
Mikhail Rykov <mikhail.rykov@gmail.com>
2022-12-15 00:47:35 +0200

    Add Kafka experiments code

--- kotlin-apache-kafka/app/build.gradle.kts
+++ kotlin-apache-kafka/app/build.gradle.kts
@@ -8,11 +8,21 @@ repositories {
     mavenCentral()
 }
 
+val testcontainersVersion = "1.17.6"
+val kafkaApiVersion = "3.3.1"
 dependencies {
+    implementation("org.apache.kafka:kafka-clients:$kafkaApiVersion")
+
+    implementation("org.slf4j:slf4j-simple:2.0.5")
+    implementation("org.apache.kafka:kafka-streams:$kafkaApiVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+
+    testImplementation("org.apache.kafka:kafka-streams-test-utils:$kafkaApiVersion")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-
-
+    testImplementation("org.testcontainers:testcontainers:$testcontainersVersion")
+    testImplementation("org.testcontainers:junit-jupiter:$testcontainersVersion")
+    testImplementation("org.testcontainers:kafka:$testcontainersVersion")
 }
 
 application {
