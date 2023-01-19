4bd51bd126c12cd95ba6b24a86d19e74fb2663b0
andrsim66 <andrsim66@users.noreply.github.com>
2023-01-19 15:57:45 +0100

    Update FlowVsChannelUnitTest.kt

--- core-kotlin-modules/core-kotlin-concurrency/src/test/kotlin/com/baeldung/flowvschannel/FlowVsChannelUnitTest.kt
+++ core-kotlin-modules/core-kotlin-concurrency/src/test/kotlin/com/baeldung/flowvschannel/FlowVsChannelUnitTest.kt
@@ -17,7 +17,7 @@ class FlowVsChannelUnitTest {
     fun when_collected_flow_multiple_time_then_return_same_values() = runBlocking {
         val coldStream = flow {
             for (i in 1..5) {
-                delay(100)
+                delay(100L)
                 emit(i)
             }
         }
@@ -29,141 +29,30 @@ class FlowVsChannelUnitTest {
         val collect2 = buildString {
             coldStream.collect { append(it).append(", ") }
         }.removeSuffix(", ")
-        assertEquals(collect1, "1, 2, 3, 4, 5")
-        assertEquals(collect2, "1, 2, 3, 4, 5")
+        assertEquals("1, 2, 3, 4, 5", collect1)
+        assertEquals("1, 2, 3, 4, 5", collect2)
     }
 
     @Test
     fun should_pass_data_from_one_coroutine_to_another() = runBlocking {
-        val channel = Channel<String>()
-        val message = "message"
+        val channel = Channel<Int>()
 
         launch { // coroutine #1
-            channel.send(message)
+            for (i in 1..5) {
+                delay(100L)
+                channel.send(i)
+            }
+            channel.close()
         }
         val result = async { // coroutine #2
-            channel.receive()
-        }
-
-        assertEquals(result.await(), message)
-    }
-
-    @OptIn(ExperimentalCoroutinesApi::class)
-    @Test
-    fun when_conflated_channel_then_drop_first_emission() = runBlocking {
-        val channel = Channel<Int>(capacity = Channel.CONFLATED)
-
-        produce<Int> {
-            for (i in 1..5) {
-                delay(100L)
-                channel.send(i)
-            }
-            channel.close()
-        }
-        val result = async {
-            val str1 = StringBuilder()
-            val str2 = StringBuilder()
-
-            delay(250L)
-            var a = channel.receive()
-            str1.append(a).append(", ")
-
-            // receive remaining items from the buffer
-            while (!channel.isClosedForReceive) {
-                a = channel.receive()
-                str2.append(a).append(", ")
-            }
-
-            str1.removeSuffix(", ") to str2.removeSuffix(", ")
-        }
-
-        assertEquals(result.await(), "2" to "3, 4, 5")
-    }
-
-    @OptIn(ExperimentalCoroutinesApi::class)
-    @Test
-    fun when_fixed_buffer_size_then_collect_awaiting_emission_to_second_result() = runBlocking {
-        val channel = Channel<Int>(capacity = 2)
-
-        produce<Int> {
-            for (i in 1..5) {
-                delay(100L)
-                channel.send(i)
-            }
-            channel.close()
-        }
-        val result = async {
-            val str1 = StringBuilder()
-            val str2 = StringBuilder()
-
-            delay(250L)
-            var a = channel.receive()
-            str1.append(a).append(", ")
-
-            delay(250L)
-            a = channel.receive()
-            str1.append(a).append(", ")
-
-            // receive remaining items from the buffer
-            while (!channel.isClosedForReceive) {
-                a = channel.receive()
-                str2.append(a).append(", ")
-            }
-
-            str1.removeSuffix(", ") to str2.removeSuffix(", ")
-        }
-
-        assertEquals(result.await(), "1, 2" to "3, 4, 5")
-    }
-
-    @OptIn(ExperimentalCoroutinesApi::class)
-    @Test
-    fun when_buffer_drop_oldest_then_receive_only_2_last_items() = runBlocking {
-        val channel = Channel<Int>(onBufferOverflow = BufferOverflow.DROP_OLDEST)
-
-        produce<Int> {
-            for (i in 1..5) {
-                delay(100L)
-                channel.send(i)
-            }
-            channel.close()
-        }
-        val result = async {
             buildString {
-                while (!channel.isClosedForReceive) {
-                    delay(500L)
-                    val a = channel.receive()
-                    append(a).append(", ")
+                channel.consumeEach {
+                    append(it).append(", ")
                 }
             }.removeSuffix(", ")
         }
 
-        assertEquals(result.await(), "4, 5")
-    }
-
-    @OptIn(ExperimentalCoroutinesApi::class)
-    @Test
-    fun when_buffer_drop_latest_then_receive_only_first_item() = runBlocking {
-        val channel = Channel<Int>(onBufferOverflow = BufferOverflow.DROP_LATEST)
-
-        produce<Int> {
-            for (i in 1..5) {
-                delay(100L)
-                channel.send(i)
-            }
-            channel.close()
-        }
-        val result = async {
-            buildString {
-                while (!channel.isClosedForReceive) {
-                    delay(550L)
-                    val a = channel.receive()
-                    append(a).append(", ")
-                }
-            }.removeSuffix(", ")
-        }
-
-        assertEquals(result.await(), "1")
+        assertEquals("1, 2, 3, 4, 5", result.await())
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
@@ -196,8 +85,10 @@ class FlowVsChannelUnitTest {
                 delay(100)
                 emit(i)
             }
-        }.buffer(capacity = 2, onBufferOverflow = BufferOverflow.SUSPEND)
-            .produceIn(this)
+        }.buffer(
+                capacity = 2,
+                onBufferOverflow = BufferOverflow.SUSPEND
+        ).produceIn(this)
 
         val result = async {
             buildString {
