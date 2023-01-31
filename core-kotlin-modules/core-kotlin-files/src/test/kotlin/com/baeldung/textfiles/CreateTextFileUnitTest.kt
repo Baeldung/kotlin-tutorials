cabd2db021dbc4f151b0efb0abb5a13ce14072b3
Burhanuddin Rashid <burhanuddin@shiny.team>
2023-01-31 11:36:26 +0530

    Formatting changes

--- core-kotlin-modules/core-kotlin-files/src/test/kotlin/com/baeldung/textfiles/CreateTextFileUnitTest.kt
+++ core-kotlin-modules/core-kotlin-files/src/test/kotlin/com/baeldung/textfiles/CreateTextFileUnitTest.kt
@@ -6,12 +6,14 @@ import java.io.File
 
 private const val testPath = "src/test/resources"
 
-class CreateTextFileTest {
+class CreateTextFileUnitTest {
 
     @Test
     fun `create text file directly using writeText`() {
         val file = File("$testPath/write_example.txt")
+
         file.writeText("Hello, World!")
+
         assertThat(file).exists()
         assertThat(file).hasContent("Hello, World!")
     }
@@ -19,10 +21,12 @@ class CreateTextFileTest {
     @Test
     fun `create text file using PrintWriter`() {
         val file = File("$testPath/print_example.txt")
+
         file.printWriter().use {
             it.println("Hello, World!")
             it.println("Hello, World!")
         }
+
         assertThat(file).exists()
         assertThat(file).hasContent("Hello, World!\nHello, World!")
     }
@@ -30,10 +34,12 @@ class CreateTextFileTest {
     @Test
     fun `create text file using FileOutputStream`() {
         val file = File("$testPath/file_output_example.txt")
+
         file.outputStream().use {
             val text = "Hello, World!"
             it.write(text.toByteArray())
         }
+
         assertThat(file).exists()
         assertThat(file).hasContent("Hello, World!")
     }
