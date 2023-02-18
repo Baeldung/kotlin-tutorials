707ee5314f9a1df28b11a3a134024ee5733c4c74
Gaetano_Piazzolla <gae.piaz@gmail.com>
2023-02-18 19:30:50 +0100

    KTLN-404 | Adding dokka plugin

---
+++ kotlin-libraries-2/src/main/kotlin/com/baeldung/dokka/KotlinClassToDocument.kt
@@ -0,0 +1,23 @@
+package com.baeldung.dokka
+
+/**
+ * This is documentation for this class.
+ */
+class KotlinClassToDocument {
+
+
+    /**
+     * This is documentation for nested class.
+     */
+    class App {
+
+    }
+
+    /**
+     * This instead is the documentation of a method.
+     */
+    fun doSomething() {
+
+    }
+
+}
