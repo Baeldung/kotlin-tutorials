b53487ea3be1ccd9cb7d4954c779b55025abc535
nappy29 <ewaneenombe@gmail.com>
2023-03-10 18:35:59 +0100

    added test case for Lombok usage in kotlin

---
+++ core-kotlin-modules/core-kotlin-annotations/src/main/kotlin/com/baeldung/Person.java
@@ -0,0 +1,19 @@
+package com.baeldung;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class Person {
+    private String name;
+    private int age;
+    private String gender;
+    private int id;
+}
+
