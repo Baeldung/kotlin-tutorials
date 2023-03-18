4bd75f3a381c1a57f2efaabb0529bed0955367c1
Mikhail Rykov <mikhail.rykov@gmail.com>
2023-03-18 13:07:08 +0200

    Create some examples of suspendCoroutine usage

---
+++ kotlin-libraries-2/src/test/kotlin/com/baeldung/kotlin/suspendcoroutine/SuspendCoroutineUnitTest.kt
@@ -0,0 +1,37 @@
+package com.baeldung.kotlin.suspendcoroutine
+
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Test
+import kotlin.test.assertNotNull
+
+class SuspendCoroutineUnitTest {
+    @Test
+    fun when_using_callback_version_then_it_looks_the_usual() = runBlocking {
+        loadData { result ->
+            result.onFailure {
+                assertNotNull(it)
+                it.printStackTrace(System.out)
+            }
+                .onSuccess {
+                    assertNotNull(it)
+                    println(it)
+                }
+        }
+
+    }
+
+    @Test
+    fun when_using_coroutine_version_then_nothing_is_changed() = runBlocking {
+        loadDataSequential { result ->
+            result.onFailure {
+                assertNotNull(it)
+                it.printStackTrace(System.out)
+            }
+                .onSuccess {
+                    assertNotNull(it)
+                    println(it)
+                }
+        }
+
+    }
+}
