33e5041c2d4559d27a23b1638e5535a9fec77d50
nappy29 <ewaneenombe@gmail.com>
2023-03-21 03:29:31 +0100

    changes added to unit tests

---
+++ core-kotlin-modules/core-kotlin-datastructures/src/test/kotlin/com/baeldung/powerofnumber/PowerOfNumberUnitTest.kt
@@ -0,0 +1,42 @@
+package com.baeldung.powerofnumber
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import kotlin.math.pow
+
+class PowerOfNumberUnitTest {
+    private val DELTA = 1e-15
+    @Test
+    fun `get power of number algorithmically`(){
+        assertEquals(4, PowerOfNumber.power(2,2))
+        assertEquals(1024, PowerOfNumber.power(4, 5))
+    }
+
+    @Test
+    fun `get power of number recursively`(){
+        assertEquals(4, PowerOfNumber.powerRec(2,2))
+        assertEquals(1024, PowerOfNumber.powerRec(4, 5))
+    }
+
+    @Test
+    fun `get power of number using pow() function`(){
+        val baseDouble = 5.0
+        val exponentDouble = 2.5
+        assertEquals(55.90169943749474, baseDouble.pow(exponentDouble), DELTA)
+
+        val baseInt = 3
+        val exponentInt = -4
+        assertEquals(0.012345679012345678, baseInt.toDouble().pow(exponentInt.toDouble()), DELTA)
+    }
+
+    @Test
+    fun `get power of number using Math pow() function`(){
+//        println(Math.pow(3.0, -4.0))
+        assertEquals(55.90169943749474, Math.pow(5.0, 2.5), DELTA)
+        assertEquals(0.012345679012345678, Math.pow(3.0, -4.0), DELTA)
+
+        val baseInt = 3
+        val exponentInt = -4
+        assertEquals(0.012345679012345678, Math.pow(baseInt.toDouble(), exponentInt.toDouble()), DELTA)
+    }
+}
