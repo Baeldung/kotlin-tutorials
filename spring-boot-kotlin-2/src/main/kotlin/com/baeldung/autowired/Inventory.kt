32be9073d4e22e57ad405adc2491d682842e80db
Suresh Raghavan <6439979+nrsuresh@users.noreply.github.com>
2023-04-09 16:58:50 -0500

    removed the unused import

--- spring-boot-kotlin-2/src/main/kotlin/com/baeldung/autowired/Inventory.kt
+++ spring-boot-kotlin-2/src/main/kotlin/com/baeldung/autowired/Inventory.kt
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.Mapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
