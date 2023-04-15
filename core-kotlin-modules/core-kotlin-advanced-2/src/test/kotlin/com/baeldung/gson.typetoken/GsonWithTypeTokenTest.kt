b5665e9a374f68c17b4fef21744fa9051c9fc0dc
nappy29 <ewaneenombe@gmail.com>
2023-04-15 02:39:03 +0100

    implemented changes

--- core-kotlin-modules/core-kotlin-advanced-2/src/test/kotlin/com/baeldung/gson.typetoken/GsonWithTypeTokenTest.kt
+++ core-kotlin-modules/core-kotlin-advanced-2/src/test/kotlin/com/baeldung/gson.typetoken/GsonWithTypeTokenTest.kt
@@ -10,11 +10,8 @@ import kotlin.test.assertFalse
 
 data class Book(val id: Int, val title: String, val author: String)
 
-class GsonWithTypetokenTest {
-    var userJson = ("[{'id': 1,'title': 'First title','author': 'Micheal L'}, "
-            + "{'id': 2,'title': 'Second title','author': 'Enombe N'}, "
-            + "{'id': 1,'title': 'Third title','author': 'Steve P'}]")
-    val json_str: String = """
+class GsonWithTypeTokenTest {
+    val jsonStr: String = """
         [
           {"id": 1,"title": "First title","author": "Micheal L"},
           {"id": 2,"title": "Second title","author": "Enombe N"},
@@ -23,8 +20,16 @@ class GsonWithTypetokenTest {
     """.trimIndent()
 
     @Test
+    fun `explicit deserialization of JSON to Kotlin object`(){
+        val books = Gson().fromJson(jsonStr, Array<Book>::class.java)
+        assertEquals(3, books.size)
+        assertFalse(books.isEmpty())
+        assertEquals("First title", books[0].title)
+    }
+
+    @Test
     fun `get list of Books with Gson extension function`() {
-        val books = Gson().fromJson<Book>(json_str)
+        val books = Gson().fromJson<Book>(jsonStr)
         assertEquals(3, books.size)
         assertFalse(books.isEmpty())
         assertEquals("Second title", books[1].title)
@@ -33,7 +38,7 @@ class GsonWithTypetokenTest {
     @Test
     fun `get list of Books with explicit TypeToken object`(){
         val bookType = object : TypeToken<List<Book>>() {}.type
-        val books = Gson().fromJson<List<Book>>(json_str, bookType)
+        val books = Gson().fromJson<List<Book>>(jsonStr, bookType)
         assertEquals(3, books.size)
         assertFalse(books.isEmpty())
         assertEquals("Second title", books[1].title)
@@ -42,18 +47,10 @@ class GsonWithTypetokenTest {
     @Test
     fun `get list of Books with generic helper function`(){
         val bookType = genericType<List<Book>>()
-        val books = Gson().fromJson<List<Book>>(userJson, bookType)
+        val books = Gson().fromJson<List<Book>>(jsonStr, bookType)
         assertEquals(3, books.size)
         assertFalse(books.isEmpty())
         assertEquals("Third title", books[2].title)
-    }
-
-    @Test
-    fun `get list of Books using toMutableList method`(){
-        val books = Gson().fromJson(json_str, Array<Book>::class.java)
-        assertEquals(3, books.size)
-        assertFalse(books.isEmpty())
-        assertEquals("First title", books[0].title)
     }
 }
 
