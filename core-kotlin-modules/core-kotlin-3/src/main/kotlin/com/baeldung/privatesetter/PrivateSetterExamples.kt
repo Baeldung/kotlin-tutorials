28f55881b06c8969dea69ba191dd1c82c50b8663
PonzaMatteo <matteoponza@gmail.com>
2023-04-23 13:56:04 +0200

    KTLN-204: Add examples for "Define a variable with a public getter and private setter in Kotlin" article (#461)
    Co-authored-by: Ponza Matteo <matteo@stashaway.com>

---
+++ core-kotlin-modules/core-kotlin-3/src/main/kotlin/com/baeldung/privatesetter/PrivateSetterExamples.kt
@@ -0,0 +1,54 @@
+package com.baeldung.privatesetter
+
+
+object PrivateSetterExamples {
+
+//    fun myClassExample() {
+//        val obj = MyClass()
+//        obj.myProperty = "new value"
+//        // Compilation fails with message:
+//        // Cannot assign to 'myProperty': the setter is private in 'MyClass'
+//    }
+
+    fun bankAccountExampleSuccess(): Int {
+        print("Bank Account Example: ")
+        val bankAccount = BankAccount()
+        bankAccount.deposit(100)
+        bankAccount.withdraw(50)
+        println("Bank Account balance is: " + bankAccount.balance)
+        // Output: Bank Account balance is: 50
+        return bankAccount.balance
+    }
+
+    fun bankAccountExampleException() {
+        val bankAccount = BankAccount()
+        bankAccount.deposit(100)
+        bankAccount.withdraw(1000)
+        // Throws: IllegalArgumentException: Withdraw Amount cannot be greater than balance
+    }
+
+//    fun bankAccountPropertyAccessError() {
+//        val bankAccount = BankAccount()
+//        bankAccount.balance -= 1000
+//        // Compilation fails with message:
+//        //
+//    }
+
+    fun loggingBankAccountExample(): Int {
+        println("Logging Bank Account Example: ")
+        val bankAccount = LoggingBankAccount()
+        bankAccount.deposit(100)
+        bankAccount.withdraw(50)
+        println("Bank Account balance is: " + bankAccount.balance)
+        return bankAccount.balance
+    }
+
+}
+
+/**
+ * Simple class that have a private setter for a property
+ */
+class MyClass {
+    var myProperty: String = "default"
+        private set
+}
