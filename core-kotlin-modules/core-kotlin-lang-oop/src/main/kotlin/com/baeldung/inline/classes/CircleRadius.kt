cdac6d0bed19b36b58252b107d91ee24623a3805
dariusandz <37371180+dariusandz@users.noreply.github.com>
2023-04-26 21:48:20 +0300

    KTLN-313: add inline class init block example

--- core-kotlin-modules/core-kotlin-lang-oop/src/main/kotlin/com/baeldung/inline/classes/CircleRadius.kt
+++ core-kotlin-modules/core-kotlin-lang-oop/src/main/kotlin/com/baeldung/inline/classes/CircleRadius.kt
@@ -9,6 +9,10 @@ value class CircleRadius(private val circleRadius : Double) : Drawable {
     val diameterOfCircle get() = 2 * circleRadius
     fun areaOfCircle() = 3.14 * circleRadius * circleRadius
 
+    init {
+        require(circleRadius > 0) { "Circle radius must be positive" }
+    }
+
     override fun draw() {
         println("Draw my circle")
     }
