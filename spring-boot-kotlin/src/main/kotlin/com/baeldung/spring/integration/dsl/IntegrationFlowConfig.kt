19b61741d9ff8fb491c144ce9d6a6e2091661e7c
exaucae <chrysexaucet@hotmail.fr>
2023-05-01 17:24:28 +0000

    feat: add Spring Integration Kotlin DSL

---
+++ spring-boot-kotlin/src/main/kotlin/com/baeldung/spring/integration/dsl/IntegrationFlowConfig.kt
@@ -0,0 +1,61 @@
+package com.baeldung.spring.integration.dsl
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.integration.config.EnableIntegration
+import org.springframework.integration.core.MessageSource
+import org.springframework.integration.dsl.*
+import org.springframework.integration.file.FileReadingMessageSource
+import org.springframework.integration.file.FileWritingMessageHandler
+import org.springframework.messaging.MessageChannel
+import org.springframework.messaging.MessageHandler
+import java.io.File
+
+@EnableIntegration
+@Configuration
+class IntegrationFlowConfig {
+
+    val inputDir = "source"
+    val outputDir = "target"
+
+    @Bean
+    fun sourceDirectory(): MessageSource<File?>? {
+        val messageSource = FileReadingMessageSource()
+        messageSource.setDirectory(File(inputDir))
+        return messageSource
+    }
+
+    @Bean
+    fun targetDirectory(): MessageHandler? {
+        val handler = FileWritingMessageHandler(File(outputDir))
+        handler.setExpectReply(false)
+        return handler
+    }
+
+    @Bean
+    fun kotlinFileMover(): IntegrationFlow = integrationFlow (
+                { "${sourceDirectory()}"}, { poller { it.fixedDelay(10000)}} )
+                    {
+                filter { message: Any ->
+                    (message as File).name
+                            .endsWith(".jpg")
+                }
+                handle("${targetDirectory()}")
+            }
+
+
+    @Bean
+    fun simpleFlow(): IntegrationFlow {
+        return integrationFlow {
+            filter<String> { it  === "Spring Integration with Kotlin | Baeldung" }
+            wireTap {
+                handle { message -> println(message.payload) }
+            }
+            transform<String> { it.uppercase() }
+        }
+    }
+
+    fun baeldung(): MessageChannel = MessageChannels.queue().get()
+    @Bean
+    fun mixedFlow(): StandardIntegrationFlow = IntegrationFlow.from(simpleFlow()).channel("baeldung").get()
+}
