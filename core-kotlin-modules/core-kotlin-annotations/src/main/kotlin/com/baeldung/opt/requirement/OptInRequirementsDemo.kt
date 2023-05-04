495b7710a90468b2b66794b6339c7421634ec7f5
nappy29 <ewaneenombe@gmail.com>
2023-05-04 17:13:06 +0100

    added opt-in codes

---
+++ core-kotlin-modules/core-kotlin-annotations/src/main/kotlin/com/baeldung/opt/requirement/OptInRequirementsDemo.kt
@@ -0,0 +1,26 @@
+package com.baeldung.opt.requirement
+@RequiresOptIn
+@Retention(AnnotationRetention.BINARY)
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
+annotation class MyClass
+
+@RequiresOptIn(level = RequiresOptIn.Level.WARNING, message = "This is an experimental API. It may be changed or removed in the future.")
+@Retention(AnnotationRetention.BINARY)
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
+annotation class ExperimentalClassApi
+
+
+class OptInRequirementsDemo {
+
+    @ExperimentalClassApi
+    private fun someExperimentalApi(): Unit {
+    }
+
+    @ExperimentalClassApi
+    private fun anotherExperimentalApi() {
+        someExperimentalApi()
+    }
+
+    @OptIn(ExperimentalClassApi::class)
+    var i = someExperimentalApi()
+}
