246b3728170e3c911a53c08ed20b02128127f266
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2023-05-08 15:31:23 -0400

    [KTLN-506] Replace samples with unit tests

---
+++ core-kotlin-modules/core-kotlin-7/src/main/kotlin/com/baeldung/mapfunction/mapInPlace.kt
@@ -0,0 +1,8 @@
+package com.baeldung.mapfunction
+
+fun <T> Array<T>.mapInPlace(transform: (T) -> T): Array<T> {
+  for (i in this.indices) {
+    this[i] = transform(this[i])
+  }
+  return this
+}
