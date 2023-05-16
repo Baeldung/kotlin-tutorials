f2486cacb974595a3c348401bc1643e2c93ca8f1
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2023-05-16 10:38:41 -0400

    [KTLN-506] Fix indentation

--- core-kotlin-modules/core-kotlin-7/src/test/kotlin/com/baeldung/mapfunction/MapInPlaceTests.kt
+++ core-kotlin-modules/core-kotlin-7/src/test/kotlin/com/baeldung/mapfunction/MapInPlaceTests.kt
@@ -5,27 +5,27 @@ import kotlin.test.assertContentEquals
 
 class MapInPlaceTests {
 
-  @Test
-  fun `Should map an array by doubling all numbers`() {
-    val numbers = arrayOf(1, 2, 3, 4, 5)
-    val doubledNumbers = numbers.map { it * 2 }
+    @Test
+    fun `Should map an array by doubling all numbers`() {
+        val numbers = arrayOf(1, 2, 3, 4, 5)
+        val doubledNumbers = numbers.map { it * 2 }
 
-    assertContentEquals(listOf(2, 4, 6, 8, 10), doubledNumbers)
-  }
+        assertContentEquals(listOf(2, 4, 6, 8, 10), doubledNumbers)
+    }
 
-  @Test
-  fun `Should reassign the original variable to the new array`() {
-    var numbers = arrayOf(1, 2, 3, 4, 5)
-    numbers = numbers.map { it * 2 }.toTypedArray()
+    @Test
+    fun `Should reassign the original variable to the new array`() {
+        var numbers = arrayOf(1, 2, 3, 4, 5)
+        numbers = numbers.map { it * 2 }.toTypedArray()
 
-    assertContentEquals(arrayOf(2, 4, 6, 8, 10), numbers)
-  }
+        assertContentEquals(arrayOf(2, 4, 6, 8, 10), numbers)
+    }
 
-  @Test
-  fun `Should replace array values in the original array`() {
-    val numbers = arrayOf(1, 2, 3, 4, 5)
-    numbers.mapInPlace { it * 2 }
+    @Test
+    fun `Should replace array values in the original array`() {
+        val numbers = arrayOf(1, 2, 3, 4, 5)
+        numbers.mapInPlace { it * 2 }
 
-    assertContentEquals(arrayOf(2, 4, 6, 8, 10), numbers)
-  }
+        assertContentEquals(arrayOf(2, 4, 6, 8, 10), numbers)
+    }
 }
