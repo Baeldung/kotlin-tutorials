e504d11b596dd227fd91abbf3535e4a192ff1888
vahram-papazyan <papazyanvahram.vp@gmail.com>
2023-05-22 10:20:47 +0400

    review-2

--- core-kotlin-modules/core-kotlin-7/src/test/kotlin/typeconversion/TestCharToInt.kt
+++ core-kotlin-modules/core-kotlin-7/src/test/kotlin/typeconversion/TestCharToInt.kt
@@ -9,14 +9,14 @@ class TestCharToInt {
 
     @Test
     fun `Given a Char, When it's a valid Unicode character, Then should convert to the corresponding Int`() {
-        assertEquals('A'.code, 65)
-        assertEquals('\u0041'.code, 65)
+        assertEquals(65, 'A'.code)
+        assertEquals(65, '\u0041'.code)
     }
 
     @Test
     fun `Given a Char, When it's a valid digit, Then should convert to the corresponding Int`() {
-        assertEquals('0'.digitToInt(), 0)
-        assertEquals('9'.digitToInt(), 9)
+        assertEquals(0, '0'.digitToInt())
+        assertEquals(9, '9'.digitToInt())
 
         assertThrows<IllegalArgumentException> { 'A'.digitToInt() }
         assertNull('A'.digitToIntOrNull())
@@ -24,15 +24,15 @@ class TestCharToInt {
 
     @Test
     fun `Given a Char, When it's a valid digit, Then should convert to the corresponding Int with the specified radix`() {
-        assertEquals('1'.digitToInt(2), 1)
+        assertEquals(1, '1'.digitToInt(2))
         // Not a binary digit
         assertNull('2'.digitToIntOrNull(2))
 
-        assertEquals('F'.digitToInt(16), 15)
+        assertEquals(15, 'F'.digitToInt(16))
         // Not a Hexadecimal digit
         assertNull('G'.digitToIntOrNull(16))
 
-        assertEquals('Z'.digitToInt(36), 35)
+        assertEquals(35, 'Z'.digitToInt(36))
         // Not a Hexatrigesimal digit
         assertNull('@'.digitToIntOrNull(36))
     }
--- core-kotlin-modules/core-kotlin-7/src/test/kotlin/typeconversion/TestIntToChar.kt
+++ core-kotlin-modules/core-kotlin-7/src/test/kotlin/typeconversion/TestIntToChar.kt
@@ -8,14 +8,14 @@ class TestIntToChar {
 
     @Test
     fun `Given an Int, When it's valid number, Then should convert Int to the corresponding Unicode character`() {
-        assertEquals(65.toChar(), 'A')
+        assertEquals('A', 65.toChar())
         // The escaped Unicode representation of 'A'
-        assertEquals(65.toChar(), '\u0041')
+        assertEquals('\u0041', 65.toChar())
     }
 
     @Test fun `Given an Int, When it's a valid digit, Then should convert to the corresponding Char`() {
-        assertEquals(0.digitToChar(), '0')
-        assertEquals(9.digitToChar(), '9')
+        assertEquals('0', 0.digitToChar())
+        assertEquals('9', 9.digitToChar())
 
         assertThrows<IllegalArgumentException> { (-1).digitToChar() }
         assertThrows<IllegalArgumentException> { 10.digitToChar() }
@@ -23,13 +23,13 @@ class TestIntToChar {
 
     @Test
     fun `Given an Int, When it's a valid digit, Then should convert to the corresponding Char with the specified radix`() {
-        assertEquals(1.digitToChar(2), '1')
+        assertEquals('1', 1.digitToChar(2))
         assertThrows<IllegalArgumentException> { 2.digitToChar(2) }
 
-        assertEquals(15.digitToChar(16), 'F')
+        assertEquals('F', 15.digitToChar(16))
         assertThrows<IllegalArgumentException> { 16.digitToChar(16) }
 
-        assertEquals(35.digitToChar(36), 'Z')
+        assertEquals('Z', 35.digitToChar(36))
         assertThrows<IllegalArgumentException> { 36.digitToChar(36) }
     }
 
