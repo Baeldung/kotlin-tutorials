4632f8adb021a1bf2c5562743cff367aed573d44
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2023-05-30 15:20:52 -0400

    [KTLN-515] Fix formatting

--- core-kotlin-modules/core-kotlin-7/src/test/kotlin/com/baeldung/optional/OptionalTest.kt
+++ core-kotlin-modules/core-kotlin-7/src/test/kotlin/com/baeldung/optional/OptionalTest.kt
@@ -10,29 +10,29 @@ import kotlin.test.assertEquals
 
 class OptionalTest {
 
-  @Test
-  fun `Get or Null`() {
-    val optionalValue: Optional<String> = Optional.of("Hello, Baeldung!")
-    val value: String? = optionalValue.getOrNull()
+    @Test
+    fun `Get or Null`() {
+        val optionalValue: Optional<String> = Optional.of("Hello, Baeldung!")
+        val value: String? = optionalValue.getOrNull()
 
-    assertEquals("Hello, Baeldung!", value)
-  }
+        assertEquals("Hello, Baeldung!", value)
+    }
 
-  @Test
-  fun `Get or Else`() {
-    val optionalValue: Optional<String> = Optional.empty()
+    @Test
+    fun `Get or Else`() {
+        val optionalValue: Optional<String> = Optional.empty()
 
-    assertThrows<IllegalStateException> { optionalValue.getOrElse { throw IllegalStateException("Value is missing") } }
+        assertThrows<IllegalStateException> { optionalValue.getOrElse { throw IllegalStateException("Value is missing") } }
 
-    val value2: String = optionalValue.getOrElse { "Default Value" }
-    assertEquals("Default Value", value2)
-  }
+        val value2: String = optionalValue.getOrElse { "Default Value" }
+        assertEquals("Default Value", value2)
+    }
 
-  @Test
-  fun `Get or Default`() {
-    val optionalValue: Optional<String> = Optional.empty()
-    val value1: String = optionalValue.getOrDefault("Default Value")
+    @Test
+    fun `Get or Default`() {
+        val optionalValue: Optional<String> = Optional.empty()
+        val value1: String = optionalValue.getOrDefault("Default Value")
 
-    assertEquals("Default Value", value1)
-  }
+        assertEquals("Default Value", value1)
+    }
 }
