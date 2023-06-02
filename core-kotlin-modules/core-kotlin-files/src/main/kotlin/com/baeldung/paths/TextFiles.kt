05ce36537ca6ea62c31afb9f4488b6c863de5bc4
kanake10 <10kanake@gmail.com>
2023-06-02 12:30:41 +0300

    creating text files in kotlin

---
+++ core-kotlin-modules/core-kotlin-files/src/main/kotlin/com/baeldung/paths/TextFiles.kt
@@ -0,0 +1,38 @@
+package com.baeldung.paths
+
+import java.io.File
+
+object TextFiles {
+
+    fun main() {
+        val fileName = "data.txt"
+        val file = File(fileName)
+        val isNewFileCreated :Boolean = file.createNewFile()
+        if(isNewFileCreated){
+            println("$fileName is created successfully.")
+        } else{
+            println("$fileName already exists.")
+        }
+        // try creating a file that already exists
+        val isFileCreated :Boolean = file.createNewFile()
+        if(isFileCreated){
+            println("$fileName is created successfully.")
+        } else{
+            println("$fileName already exists.")
+        }
+    }
+
+    fun writeText() {
+        val myFileName = "example.txt"
+        val file = File(myFileName)
+        file.writeText("")
+    }
+
+    fun writeBytes() {
+        val fileName = "file.txt"
+        val content = "This is my content"
+        val bytes = content.toByteArray()
+        File(fileName).writeBytes(bytes)
+        println("Text file created: $fileName")
+    }
+}
