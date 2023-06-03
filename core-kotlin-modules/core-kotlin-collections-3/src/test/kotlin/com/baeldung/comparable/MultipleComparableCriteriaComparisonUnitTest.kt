b50f4c42fc7f259ea7a52a225b33a168dc7243e6
David Parlanti <david.parlanti@gmail.com>
2023-06-03 16:07:53 +0200

    dparlanti| Updated examplefor comparison multicriteria

--- core-kotlin-modules/core-kotlin-collections-3/src/test/kotlin/com/baeldung/comparable/MultipleComparableCriteriaComparisonUnitTest.kt
+++ core-kotlin-modules/core-kotlin-collections-3/src/test/kotlin/com/baeldung/comparable/MultipleComparableCriteriaComparisonUnitTest.kt
@@ -7,15 +7,25 @@ import kotlin.test.assertTrue
 class MultipleComparableCriteriaComparisonUnitTest {
 
     class Version(val value: Int, val timestamp: Instant) : Comparable<Version> {
-        override fun compareTo(other: Version): Int = when {
-            value.compareTo(other.value) == 0 -> timestamp compareTo other.timestamp
-            else -> value compareTo other.value
+        override fun compareTo(other: Version): Int = when(val valueComparison = value.compareTo(other.value))  {
+            0 -> timestamp compareTo other.timestamp
+            else -> valueComparison
         }
     }
 
     @Test
     fun whenVersionIsMoreRecent_thenCorrect() {
         assertTrue(Version(0, Instant.EPOCH) < Version(0, Instant.now()))
+    }
+
+    @Test
+    fun whenValueIsGreater_thenCorrect() {
+        assertTrue(Version(1, Instant.EPOCH) > Version(0, Instant.now()))
+    }
+
+    @Test
+    fun whenValueIsEqual_thenCorrect() {
+        assertTrue(Version(1, Instant.EPOCH).compareTo(Version(1, Instant.EPOCH)) == 0)
     }
 
 }
