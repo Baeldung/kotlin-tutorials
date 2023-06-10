a35df24d05c85395f3fe5c5e1d9a27ef57c48abc
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2023-06-10 14:06:24 -0400

    [KTLN-545] Add sample module

---
+++ gradle-kotlin-dsl/custom-source-set/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    java
+    application
+    kotlin("jvm") version "1.8.22"
+}
+
+sourceSets {
+    create("analytics") {
+        java.srcDir("src/analytics/java")
+        java.srcDir("src/analytics/kotlin")
+        resources.srcDir("src/analytics/resources")
+    }
+}
+
+dependencies {
+    // "analyticsImplementation"("org.apache.commons:commons-lang3:3.12.0")
+    // /\ \/ are equal
+    sourceSets.named("analytics") {
+        implementation("org.apache.commons:commons-lang3:3.12.0")
+    }
+
+
+    api(sourceSets.named("analytics").get().output)
+}
+
+application {
+    mainClass.set("com.baeldung.SampleKt")
+    applicationDefaultJvmArgs = listOf("-Dconfig.file=analytics.conf")
+}
