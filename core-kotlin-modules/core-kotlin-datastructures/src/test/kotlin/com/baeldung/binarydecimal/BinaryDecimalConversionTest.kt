9ce238fbf30db6f5fa1b0f62f97fb77af75a7f3d
nappy29 <ewaneenombe@gmail.com>
2023-06-25 23:23:35 +0100

    added unit test

---
+++ core-kotlin-modules/core-kotlin-datastructures/src/test/kotlin/com/baeldung/binarydecimal/BinaryDecimalConversionTest.kt
@@ -0,0 +1,72 @@
+package com.baeldung.binarydecimal
+
+import org.junit.jupiter.api.Test
+import kotlin.math.pow
+import kotlin.test.assertEquals
+
+class BinaryDecimalConversionTest {
+
+    @Test
+    fun `binary to decimal using toInt() method`(){
+        assertEquals(439, "110110111".toInt(2))
+        assertEquals(109, "1101101".toInt(2))
+        assertEquals(27, "11011".toInt(2))
+        assertEquals(122979, "11110000001100011".toInt(2))
+    }
+
+    @Test
+    fun `binary to decimal custom approach`(){
+
+        assertEquals(439, binaryToDecimal(110110111))
+        assertEquals(109, binaryToDecimal(1101101))
+        assertEquals(27, binaryToDecimal(11011))
+        assertEquals(122979, binaryToDecimal(11110000001100011))
+
+    }
+
+    fun binaryToDecimal(binary: Long): Int{
+        var binaryNumber = binary
+        var decimalNumber = 0
+        var i = 0
+        var remainder: Long
+
+        while (binaryNumber.toInt() != 0) {
+            remainder = binaryNumber % 10
+            binaryNumber /= 10
+            decimalNumber += (remainder * 2.0.pow(i.toDouble())).toInt()
+            ++i
+        }
+        return decimalNumber
+
+    }
+
+    @Test
+    fun `decimal to binary custom approach`(){
+
+        assertEquals(110110111, decimalToBinary(439))
+        assertEquals(1101101, decimalToBinary(109))
+        assertEquals(11011, decimalToBinary(27))
+    }
+
+    @Test
+    fun `decima to binary using toBinaryString() method`(){
+        assertEquals("110110111", Integer.toBinaryString(439))
+        assertEquals("1101101", Integer.toBinaryString(109))
+        assertEquals("11011", Integer.toBinaryString(27))
+    }
+
+    fun decimalToBinary(n: Int): Long {
+        var decimal = n
+        var binaryNumber: Long = 0
+        var remainder: Int
+        var i = 1
+
+        while (decimal != 0) {
+            remainder = decimal % 2
+            decimal /= 2
+            binaryNumber += (remainder * i).toLong()
+            i *= 10
+        }
+        return binaryNumber
+    }
+}
