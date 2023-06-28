860b29f1a24f5041fb6bda9c0d20d249d6d15b7a
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2023-06-28 12:28:20 -0400

    [KTLN-563] Realign testImplementation

--- kotest/kotest-spring/build.gradle.kts
+++ kotest/kotest-spring/build.gradle.kts
@@ -14,8 +14,8 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
 
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.kotest:kotest-runner-junit5:5.6.2")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
 
