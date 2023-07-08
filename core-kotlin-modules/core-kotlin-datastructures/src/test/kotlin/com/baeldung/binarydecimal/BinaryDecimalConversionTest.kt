d4ad195f92556decdf3fbb26ac38cc16a9f87c74
nappy29 <ewaneenombe@gmail.com>
2023-07-08 17:13:20 +0100

    Code fixes

--- core-kotlin-modules/core-kotlin-datastructures/src/test/kotlin/com/baeldung/binarydecimal/BinaryDecimalConversionTest.kt
+++ core-kotlin-modules/core-kotlin-datastructures/src/test/kotlin/com/baeldung/binarydecimal/BinaryDecimalConversionTest.kt
@@ -12,16 +12,18 @@ class BinaryDecimalConversionTest {
         assertEquals(109, "1101101".toInt(2))
         assertEquals(27, "11011".toInt(2))
         assertEquals(122979, "11110000001100011".toInt(2))
+        assertEquals(12, "1100".toInt(2))
+        assertEquals(98, "1100010".toInt(2))
     }
 
     @Test
     fun `binary to decimal mathematical approach`(){
-
         assertEquals(439, binaryToDecimal("110110111"))
         assertEquals(109, binaryToDecimal("1101101"))
         assertEquals(27, binaryToDecimal("11011"))
         assertEquals(122979, binaryToDecimal("11110000001100011"))
-
+        assertEquals(12, binaryToDecimal("1100"))
+        assertEquals(98, binaryToDecimal("1100010"))
     }
 
     fun binaryToDecimal(binary: String): Int{
@@ -34,15 +36,15 @@ class BinaryDecimalConversionTest {
             i++
         }
         return decimalNumber
-
     }
 
     @Test
     fun `decimal to binary mathematical approach`(){
-
         assertEquals("110110111", decimalToBinary(439))
         assertEquals("1101101", decimalToBinary(109))
         assertEquals("11011", decimalToBinary(27))
+        assertEquals("1100", decimalToBinary(12))
+        assertEquals("1100010", decimalToBinary(98))
     }
 
     @Test
@@ -50,6 +52,8 @@ class BinaryDecimalConversionTest {
         assertEquals("110110111", Integer.toBinaryString(439))
         assertEquals("1101101", Integer.toBinaryString(109))
         assertEquals("11011", Integer.toBinaryString(27))
+        assertEquals("1100", Integer.toBinaryString(12))
+        assertEquals("1100010", Integer.toBinaryString(98))
     }
 
     fun decimalToBinary( n: Int): String {
