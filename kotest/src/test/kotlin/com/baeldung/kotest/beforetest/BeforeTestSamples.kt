ec3d530fdd4fc93cc4c1c56debd098df11994e90
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2023-08-02 01:14:41 -0400

    [KTLN-559] Add samples (#542)
    * [KTLN-559] Add samples
    
    * [KTLN-559] Add samples
    
    * Update kotest/src/test/kotlin/com/baeldung/kotest/beforetest/BeforeTestSamples.kt
    
    Co-authored-by: Brandon Ward <brandon.ward1993@gmail.com>
    
    ---------
    
    Co-authored-by: Brandon Ward <brandon.ward1993@gmail.com>

---
+++ kotest/src/test/kotlin/com/baeldung/kotest/beforetest/BeforeTestSamples.kt
@@ -0,0 +1,48 @@
+package com.baeldung.kotest.beforetest
+
+import io.kotest.core.listeners.BeforeInvocationListener
+import io.kotest.core.spec.IsolationMode
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.core.test.TestCase
+import io.kotest.matchers.shouldBe
+
+class BeforeTestSamples : FunSpec({
+
+    val userRepository = UserRepository()
+
+    isolationMode = IsolationMode.InstancePerTest
+
+    beforeEach {
+        userRepository.addUser(User(1, "Admin"))
+    }
+
+
+    test("Accessing all users should include added user and Admin") {
+        userRepository.addUser(User(2, "SimpleUser"))
+
+        userRepository.all() shouldBe listOf(
+            User(1, "Admin"),
+            User(2, "SimpleUser")
+        )
+    }
+})
+
+class BeforeInvocationSamples : FunSpec({
+
+    val userRepository = UserRepository()
+
+    isolationMode = IsolationMode.InstancePerTest
+
+    extension(object : BeforeInvocationListener {
+        override suspend fun beforeInvocation(testCase: TestCase, iteration: Int) {
+            userRepository.addUser(User(iteration.toLong(), "Admin"))
+        }
+    })
+
+
+    test("Accessing all users should include added user and Admin").config(invocations = 30) {
+        userRepository.addUser(User(2, "SimpleUser"))
+
+        userRepository.all().last() shouldBe User(2, "SimpleUser")
+    }
+})
