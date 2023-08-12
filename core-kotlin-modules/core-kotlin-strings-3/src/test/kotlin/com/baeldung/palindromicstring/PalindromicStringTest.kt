46630e2f79c548ba5f4a48fe1e3c322d18c23eb6
nappy29 <ewaneenombe@gmail.com>
2023-08-12 14:59:42 +0100

    code fixes

--- core-kotlin-modules/core-kotlin-strings-3/src/test/kotlin/com/baeldung/palindromicstring/PalindromicStringTest.kt
+++ core-kotlin-modules/core-kotlin-strings-3/src/test/kotlin/com/baeldung/palindromicstring/PalindromicStringTest.kt
@@ -1,33 +1,35 @@
 package com.baeldung.palindromicstring
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class PalindromicStringTest {
 
     @Test fun `palindrome check using loop function`(){
-        assert(isPalindromeUsingLoop("racecar") == true)
-        assert(isPalindromeUsingLoop("redivider") == true)
-        assert(isPalindromeUsingLoop("Palindrome") == false)
-        assert(isPalindromeUsingLoop("sever") == false)
-        assert(isPalindromeUsingLoop("3553") == true)
-        assert(isPalindromeUsingLoop("tattarrattat") == true)
+        assertTrue(isPalindromeUsingLoop("racecar"))
+        assertTrue(isPalindromeUsingLoop("redivider"))
+        assertFalse(isPalindromeUsingLoop("Palindrome"))
+        assertFalse(isPalindromeUsingLoop("sever"))
+        assertTrue(isPalindromeUsingLoop("3553"))
+        assertTrue(isPalindromeUsingLoop("tattarrattat"))
     }
 
     @Test fun `palindrome check using built in function`(){
-        assert(isPalindromeWithBuiltInFunction("racecar") == true)
-        assert(isPalindromeWithBuiltInFunction("redivider") == true)
-        assert(isPalindromeWithBuiltInFunction("Palindrome") == false)
-        assert(isPalindromeWithBuiltInFunction("sever") == false)
-        assert(isPalindromeWithBuiltInFunction("3553") == true)
-        assert(isPalindromeWithBuiltInFunction("tattarrattat") == true)
+        assertTrue(isPalindromeWithBuiltInFunction("racecar"))
+        assertTrue(isPalindromeWithBuiltInFunction("redivider") )
+        assertFalse(isPalindromeWithBuiltInFunction("Palindrome") )
+        assertFalse(isPalindromeWithBuiltInFunction("sever"))
+        assertTrue(isPalindromeWithBuiltInFunction("3553") )
+        assertTrue(isPalindromeWithBuiltInFunction("tattarrattat"))
     }
 
     @Test fun `palindrome check using recursion`(){
-        assert(isPalindromeUsingRecursion("racecar") == true)
-        assert(isPalindromeUsingRecursion("redivider") == true)
-        assert(isPalindromeUsingRecursion("Palindrome") == false)
-        assert(isPalindromeUsingRecursion("sever") == false)
-        assert(isPalindromeUsingRecursion("3553") == true)
-        assert(isPalindromeUsingRecursion("tattarrattat") == true)
+        assertTrue(isPalindromeUsingRecursion("racecar"))
+        assertTrue(isPalindromeUsingRecursion("redivider"))
+        assertFalse(isPalindromeUsingRecursion("Palindrome"))
+        assertFalse(isPalindromeUsingRecursion("sever"))
+        assertTrue(isPalindromeUsingRecursion("3553"))
+        assertTrue(isPalindromeUsingRecursion("tattarrattat"))
     }
 
     fun isPalindromeUsingLoop(str: String): Boolean {
