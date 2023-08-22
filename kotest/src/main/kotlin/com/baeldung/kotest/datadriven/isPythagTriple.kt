1c5621cd0220403ebe380f0f84190e90a9e884ba
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2023-08-22 18:29:59 -0300

    [KTLN-627] Add samples

--- kotest/src/main/kotlin/com/baeldung/kotest/datadriven/isPythagTriple.kt
+++ kotest/src/main/kotlin/com/baeldung/kotest/datadriven/isPythagTriple.kt
@@ -1,3 +1,6 @@
 package com.baeldung.kotest.datadriven
 
-fun isPythagTriple(a: Int, b: Int, c: Int): Boolean = a * a + b * b == c * c
+fun isPythagTriple(a: Int, b: Int, c: Int): Boolean {
+    if(a <= 0 || b <= 0 || c <= 0) return false
+    return a * a + b * b == c * c
+}
