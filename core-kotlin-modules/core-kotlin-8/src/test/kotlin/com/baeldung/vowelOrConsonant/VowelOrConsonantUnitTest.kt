863eaae3899640bfba152f04e014e8500e064802
nappy29 <ewaneenombe@gmail.com>
2023-08-24 17:06:24 +0100

    code fixes

--- core-kotlin-modules/core-kotlin-8/src/test/kotlin/com/baeldung/vowelOrConsonant/VowelOrConsonantUnitTest.kt
+++ core-kotlin-modules/core-kotlin-8/src/test/kotlin/com/baeldung/vowelOrConsonant/VowelOrConsonantUnitTest.kt
@@ -6,59 +6,51 @@ import kotlin.test.assertFalse
 
 class VowelOrConsonantUnitTest {
     @Test
-    fun `using if-else method`() {
+    fun `using when expression`() {
         assertTrue(isVowel('e'))
         assertTrue(isVowel('I'))
         assertTrue(isVowel('o'))
-        assertTrue(isVowel('U'))
+        assertFalse(isConsonant('o'))
         assertFalse(isVowel('H'))
-        assertFalse(isVowel('R'))
-        assertFalse(isVowel('w'))
-        assertFalse(isVowel('y'))
-    }
-
-    @Test fun `using when method`() {
-        assertTrue(usingWhenStatement('e'))
-        assertTrue(usingWhenStatement('I'))
-        assertTrue(usingWhenStatement('o'))
-        assertTrue(usingWhenStatement('U'))
-        assertFalse(usingWhenStatement('H'))
-        assertFalse(usingWhenStatement('R'))
-        assertFalse(usingWhenStatement('w'))
-        assertFalse(usingWhenStatement('y'))
+        assertTrue(isConsonant('H'))
+        assertFalse(isVowel('@'))
+        assertFalse(isConsonant('@'))
     }
 
     @Test
     fun `using Map method`() {
-        assertTrue(usingMap('e'))
-        assertTrue(usingMap('I'))
-        assertTrue(usingMap('o'))
-        assertTrue(usingMap('U'))
-        assertFalse(usingMap('H'))
-        assertFalse(usingMap('R'))
-        assertFalse(usingMap('w'))
-        assertFalse(usingMap('y'))
+        assertTrue(isVowelUsingMap('e'))
+        assertTrue(isVowelUsingMap('I'))
+        assertTrue(isVowelUsingMap('o'))
+        assertFalse(isConsonantUsingMap('o'))
+        assertFalse(isVowelUsingMap('H'))
+        assertTrue(isConsonantUsingMap('H'))
+        assertFalse(isVowelUsingMap('@'))
+        assertFalse(isConsonantUsingMap('@'))
     }
 
-    @Test fun `using regex method`() { assertTrue(usingRegexMethod('e'))
-        assertTrue(usingRegexMethod('I'))
-        assertTrue(usingRegexMethod('o'))
-        assertTrue(usingRegexMethod('U'))
-        assertFalse(usingRegexMethod('H'))
-        assertFalse(usingRegexMethod('R'))
-        assertFalse(usingRegexMethod('w'))
-        assertFalse(usingRegexMethod('y'))
+    @Test
+    fun `using regex method`() {
+        assertTrue(isVowelUsingRegexMethod('e'))
+        assertTrue(isVowelUsingRegexMethod('I'))
+        assertTrue(isVowelUsingRegexMethod('o'))
+        assertFalse(isConsonantUsingRegex('o'))
+        assertFalse(isVowelUsingRegexMethod('H'))
+        assertTrue(isConsonantUsingRegex('H'))
+        assertFalse(isVowelUsingRegexMethod('@'))
+        assertFalse(isConsonantUsingRegex('@'))
     }
 
-    @Test fun `using ASCII values method`() {
-        assertTrue(usingAsciiValues('e'))
-        assertTrue(usingAsciiValues('I'))
-        assertTrue(usingAsciiValues('o'))
-        assertTrue(usingAsciiValues('U'))
-        assertFalse(usingAsciiValues('H'))
-        assertFalse(usingAsciiValues('R'))
-        assertFalse(usingAsciiValues('w'))
-        assertFalse(usingAsciiValues('y'))
+    @Test
+    fun `using ASCII values method`() {
+        assertTrue(isVowelUsingAsciiValues('E'))
+        assertTrue(isVowelUsingAsciiValues('I'))
+        assertTrue(isVowelUsingAsciiValues('o'))
+        assertFalse(isConsonantUsingAscii('o'))
+        assertFalse(isVowelUsingAsciiValues('H'))
+        assertTrue(isConsonantUsingAscii('H'))
+        assertFalse(isVowelUsingAsciiValues('@'))
+        assertFalse(isConsonantUsingAscii('@'))
     }
 
 
@@ -69,29 +61,42 @@ class VowelOrConsonantUnitTest {
         }
     }
 
-    fun usingWhenStatement(c: Char): Boolean {
-        return when (c) {
-            'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' -> true
-            else -> false
-        }
+    fun isConsonant(c: Char): Boolean {
+        return !isVowel(c) && c.isLetter()
     }
 
-    fun usingMap(character: Char): Boolean {
+
+
+    fun isVowelUsingMap(c: Char): Boolean {
         val vowels = mapOf(
             'a' to true,
             'e' to true,
             'i' to true,
             'o' to true,
             'u' to true )
-        return vowels.containsKey(character.toLowerCase())
+        return vowels.containsKey(c.lowercaseChar())
     }
 
-    fun usingRegexMethod(c: Char): Boolean {
+    fun isConsonantUsingMap(c: Char): Boolean {
+        return !isVowelUsingMap(c) && c.isLetter()
+    }
+
+    fun isVowelUsingRegexMethod(c: Char): Boolean {
         return c.toString().matches("[AEIOUaeiou]".toRegex())
     }
 
-    fun usingAsciiValues(c: Char): Boolean {
+    fun isConsonantUsingRegex(c: Char): Boolean {
+        val consonants = "[bcdfghjklmnpqrstvwxyz]"
+        return c.toString().matches(consonants.toRegex())
+    }
+
+    fun isVowelUsingAsciiValues(c: Char): Boolean {
         val ascii = c.code
         return ascii == 65 || ascii == 69 || ascii == 73 || ascii == 79 || ascii == 85 || ascii == 97 || ascii == 101 || ascii == 105 || ascii == 111 || ascii == 117
+    }
+
+    fun isConsonantUsingAscii(c: Char): Boolean {
+        val ascii = c.code
+        return ascii in 97..122 && ascii !in setOf(97, 101, 105, 111, 117)
     }
 }
