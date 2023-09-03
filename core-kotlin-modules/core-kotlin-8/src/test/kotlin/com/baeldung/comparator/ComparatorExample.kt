4537914e97af8f1c54ecb025ab9c41c2a1fd895b
Vinay Wadhwa <vinaywadhwa@gmail.com>
2023-09-03 16:25:09 +0530

    using assertIterableEquals instead of assert

--- core-kotlin-modules/core-kotlin-8/src/test/kotlin/com/baeldung/comparator/ComparatorExample.kt
+++ core-kotlin-modules/core-kotlin-8/src/test/kotlin/com/baeldung/comparator/ComparatorExample.kt
@@ -1,5 +1,6 @@
 package com.baeldung.comparator
 
+import org.junit.jupiter.api.Assertions.assertIterableEquals
 import org.junit.jupiter.api.Test
 
 class ComparatorExample {
@@ -13,21 +14,21 @@ class ComparatorExample {
                 return string1.length - string2.length
             }
         }
-        assert(words.sortedWith(lengthComparator) == listOf("date", "apple", "cherry", "blueberry"))
-        assert(words.sortedWith(compareBy(String::length)) == listOf("date", "apple", "cherry", "blueberry"))
+        assertIterableEquals(words.sortedWith(lengthComparator), listOf("date", "apple", "cherry", "blueberry"))
+        assertIterableEquals(words.sortedWith(compareBy(String::length)), listOf("date", "apple", "cherry", "blueberry"))
     }
 
     @Test
     fun `when custom comparator used to sort list of user defined objects then list sorted correctly`() {
         data class Person(val name: String, val age: Int)
         val people = listOf(Person("Alice", 29), Person("Bob", 31), Person("Bob", 29))
-        assert(people.sortedWith(compareBy({ it.age }, { it.name })) == listOf(Person("Alice", 29), Person("Bob", 29), Person("Bob", 31)))
+        assertIterableEquals(people.sortedWith(compareBy({ it.age }, { it.name })), listOf(Person("Alice", 29), Person("Bob", 29), Person("Bob", 31)))
     }
 
     @Test
     fun `when reversed function used then list sorted correctly`() {
         val words = listOf("apple", "blueberry", "cherry", "date")
-        assert(words.sortedWith(compareBy<String> { it.length }.reversed()) == listOf("blueberry","cherry","apple","date"))
+        assertIterableEquals(words.sortedWith(compareBy<String> { it.length }.reversed()), listOf("blueberry","cherry","apple","date"))
     }
 
     @Test
@@ -35,7 +36,7 @@ class ComparatorExample {
         data class Person(val name: String, val age: Int)
         val people = listOf(Person("Alice", 29), Person("Bob", 31), Person("Cleo", 29))
         val sortedPeople = people.sortedWith(compareBy(Person::age).then(compareBy(Person::name)))
-        assert(sortedPeople == listOf(Person("Alice", 29), Person("Cleo", 29), Person("Bob", 31)))
+        assertIterableEquals(sortedPeople, listOf(Person("Alice", 29), Person("Cleo", 29), Person("Bob", 31)))
     }
 
     @Test
@@ -43,7 +44,7 @@ class ComparatorExample {
         data class Person(val name: String, val age: Int)
         val people = listOf(Person("Alice", 29), Person("Bob", 31), Person("Cleo", 29))
         val sortedPeople = people.sortedWith(compareBy(Person::age).thenBy(Person::name))
-        assert(sortedPeople == listOf(Person("Alice", 29), Person("Cleo", 29), Person("Bob", 31)))
+        assertIterableEquals(sortedPeople, listOf(Person("Alice", 29), Person("Cleo", 29), Person("Bob", 31)))
     }
 
     @Test
@@ -51,9 +52,9 @@ class ComparatorExample {
         data class Person(val name: String, val age: Int)
         val people = listOf(Person("Alice", 29), Person("Bob", 31), Person("Cleo", 29))
         val sortedPeople = people.sortedWith(compareBy(Person::age).thenDescending(compareBy(Person::name)))
-        assert(sortedPeople == listOf(Person("Cleo", 29), Person("Alice", 29), Person("Bob", 31)))
+        assertIterableEquals(sortedPeople, listOf(Person("Cleo", 29), Person("Alice", 29), Person("Bob", 31)))
         val sortedPeople2 = people.sortedWith(compareByDescending(Person::age).thenByDescending(Person::name))
-        assert(sortedPeople2 == listOf(Person("Bob", 31), Person("Cleo", 29), Person("Alice", 29)))
+        assertIterableEquals(sortedPeople2, listOf(Person("Bob", 31), Person("Cleo", 29), Person("Alice", 29)))
     }
 
     @Test
@@ -62,6 +63,6 @@ class ComparatorExample {
         val people = listOf(Person("Alice", 29), Person("Bob", 31), Person("Cleo", 29))
         val combinedComparator = compareBy(Person::age).thenComparator { person1, person2 -> person1.name.length - person2.name.length }
         val sortedPeople = people.sortedWith(combinedComparator)
-        assert(sortedPeople == listOf(Person("Cleo", 29), Person("Alice", 29), Person("Bob", 31)))
+        assertIterableEquals(sortedPeople, listOf(Person("Cleo", 29), Person("Alice", 29), Person("Bob", 31)))
     }
 }
