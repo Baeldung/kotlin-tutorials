c56f8655ca3317dc59f57ed3e8d361e27843e911
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2023-09-03 12:21:33 -0300

    [KTLN-660] Add samples

---
+++ core-kotlin-modules/core-kotlin-8/src/test/kotlin/com/baeldung/todo/CalculatorTest.kt
@@ -0,0 +1,16 @@
+package com.baeldung.todo
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class CalculatorTest {
+
+    @Test
+    fun `calculator should add two numbers`() {
+        val calculator = Calculator()
+
+        assertThrows<NotImplementedError>("Implement a + b") {
+            calculator.add(1, 2)
+        }
+    }
+}
