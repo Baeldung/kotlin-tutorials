9c2b907857c8cd57068f184533a67166e7f00565
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2023-09-11 00:03:46 -0300

    [KTLN-660] Add samples (#600)

---
+++ core-kotlin-modules/core-kotlin-8/src/main/kotlin/com/baeldung/todo/Calculator.kt
@@ -0,0 +1,11 @@
+package com.baeldung.todo
+
+class Calculator {
+    fun add(a: Int, b: Int): Int {
+        TODO("Implement a + b")
+    }
+
+    fun subtract(a: Int, b: Int): Int {
+        TODO("Implement a - b")
+    }
+}
