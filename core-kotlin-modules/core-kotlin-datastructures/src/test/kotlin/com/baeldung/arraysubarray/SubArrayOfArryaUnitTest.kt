b7e6df0c2a6ba88b18c59e98eb52752ee9cc371a
albertache1998 <albertache1998@gmail.com>
2023-09-28 05:42:18 +0100

    Included unit tests

---
+++ core-kotlin-modules/core-kotlin-datastructures/src/test/kotlin/com/baeldung/arraysubarray/SubArrayOfArryaUnitTest.kt
@@ -0,0 +1,75 @@
+package com.baeldung.arraysubarray
+
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Test
+
+class SubArrayOfArryaUnitTest {
+
+    @Test
+    fun `using slice() method`() {
+        val array = arrayOf(1, 2, 3, 4, 5, 6)
+        val subArray = array.slice(2..4)
+        assertArrayEquals(intArrayOf(3, 4, 5), subArray.toIntArray())
+    }
+
+    @Test
+    fun `using copyOfRange() method`() {
+        val array = arrayOf(1, 2, 3, 4, 5, 6)
+        val subArray = array.copyOfRange(2, 5)
+        assertArrayEquals(intArrayOf(3, 4, 5), subArray.toIntArray())
+    }
+
+    @Test
+    fun `using subList() method`() {
+        val array = arrayOf(1, 2, 3, 4, 5, 6)
+        val subArray = array.toList().subList(2, 5).toTypedArray()
+        assertArrayEquals(intArrayOf(3, 4, 5), subArray.toIntArray())
+    }
+
+    @Test
+    fun `using filterIndexed() method`() {
+        val array = arrayOf(1, 2, 3, 4, 5, 6)
+        val subArray = array.filterIndexed { index, _ -> index in 2..4 }.toTypedArray()
+        assertArrayEquals(intArrayOf(3, 4, 5), subArray.toIntArray())
+    }
+
+    @Test
+    fun `using drop() and take() methods`() {
+        val array = arrayOf(1, 2, 3, 4, 5, 6)
+        val subArray = array.drop(2).take(3).toTypedArray()
+        assertArrayEquals(intArrayOf(3, 4, 5), subArray.toIntArray())
+    }
+
+    @Test
+    fun `using filter() method`() {
+        val array = arrayOf(1, 2, 3, 4, 5, 6)
+        val subArray = array.filter { it in 3..5 }.toTypedArray()
+        assertArrayEquals(intArrayOf(3, 4, 5), subArray.toIntArray())
+    }
+
+    @Test
+    fun `using map() method`() {
+        val array = arrayOf(1, 2, 3, 4, 5, 6)
+        val subArray = (2..4)
+            .map { i: Int -> array[i] }
+            .toTypedArray()
+        assertArrayEquals(intArrayOf(3, 4, 5), subArray.toIntArray())
+    }
+
+    @Test
+    fun `using custom approach`() {
+        val array = arrayOf(1, 2, 3, 4, 5, 6)
+        val subArray = customMethod(array, 2, 4)
+        assertArrayEquals(intArrayOf(3, 4, 5), subArray)
+    }
+}
+
+fun customMethod(array: Array<Int>, start: Int, end: Int): IntArray {
+    val subArray = IntArray(end - start + 1)
+    for (i in subArray.indices) {
+        subArray[i] = array[start + i]
+        println(subArray[i])
+    }
+
+    return subArray
+}
