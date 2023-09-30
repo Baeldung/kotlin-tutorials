efff1e67b049f6e4b26e09948fb76aba07e4c284
gaepi <gae.piaz@gmail.com>
2023-09-30 15:32:48 +0200

    JAVA-25295 | jvm annotation comment

--- core-kotlin-modules/core-kotlin-annotations/src/main/kotlin/com/baeldung/jvmannotations/Document.kt
+++ core-kotlin-modules/core-kotlin-annotations/src/main/kotlin/com/baeldung/jvmannotations/Document.kt
@@ -3,6 +3,7 @@ package com.baeldung.jvmannotations
 
 interface Document {
 
+    // @JvmDefault deprecation level ERROR. Switched to new -Xjvm-default modes: all or all-compatibility
     fun getTypeDefault() = "document"
 
     fun getType() = "document"
