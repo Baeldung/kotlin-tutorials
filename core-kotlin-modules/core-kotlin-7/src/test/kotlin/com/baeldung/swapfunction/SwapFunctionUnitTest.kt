b6efa0dc4fecf06b5b527992e2dd6beea9e6462d
kent <kent.yuan@gmail.com>
2023-10-03 17:21:08 +0200

    [impr-swap-fun] swap two variables fix and refactoring

---
+++ core-kotlin-modules/core-kotlin-7/src/test/kotlin/com/baeldung/swapfunction/SwapFunctionUnitTest.kt
@@ -0,0 +1,43 @@
+package com.baeldung.swapfunction
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+
+class SwapFunctionUnitTest {
+
+    @Test
+    fun `when using a temp var, then get the expected result`() {
+        var a = 1
+        var b = 2
+        val temp = a
+        a = b
+        b = temp
+        assertEquals(2, a)
+        assertEquals(1, b)
+    }
+
+    @Test
+    fun `when using the also() function, then get the expected result`() {
+        var a = 1
+        var b = 2
+        val temp = a
+        a = b
+        b = temp
+        assertEquals(2, a)
+        assertEquals(1, b)
+    }
+
+    @Test
+    fun `when using Pair, then get the expected result`() {
+        var x = 100
+        var y = 200
+
+        (x to y).apply {
+            x = second
+            y = first
+        }
+        assertEquals(200, x)
+        assertEquals(100, y)
+    }
+}
