728e1ce6a08c3380d9afbd6e8510a0858c63d4ef
n3 <10kanake@gmail.com>
2023-10-11 10:25:25 +0300

    update
    Co-authored-by: Brandon Ward <brandon.ward1993@gmail.com>

--- core-kotlin-modules/core-kotlin-datastructures/src/test/kotlin/com/baeldung/insertionsort/SortArrayTest.kt
+++ core-kotlin-modules/core-kotlin-datastructures/src/test/kotlin/com/baeldung/insertionsort/SortArrayTest.kt
@@ -14,7 +14,7 @@ fun insertionSortByAge(people: Array<Person>) {
         val key = people[i]
         var j = i - 1
 
-        while (j >= 0 && people[j].compareTo(key) > 0) {
+        while (j >= 0 && people[j] > people[i]) {
             people[j + 1] = people[j]
             j--
         }
