65e027b4f1365650877dddf4c0c5eaecbec613b8
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2023-10-14 17:39:51 -0300

    [KTLN-732] Add samples

--- core-kotlin-modules/core-kotlin-arrays-2/src/test/kotlin/com/baeldung/removefirstelement/RemoveFirstElementTest.kt
+++ core-kotlin-modules/core-kotlin-arrays-2/src/test/kotlin/com/baeldung/removefirstelement/RemoveFirstElementTest.kt
@@ -1,7 +1,6 @@
-package com.baeldung.removeFirstElement
+package com.baeldung.removefirstelement
 
 import org.junit.jupiter.api.Assertions.assertArrayEquals
-import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class RemoveFirstElementTest {
