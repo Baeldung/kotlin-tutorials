790b60ee90086a5307a826adf4af4016ae831994
kanake10 <10kanake@gmail.com>
2023-10-24 22:50:47 +0300

    update

--- core-kotlin-modules/core-kotlin-lang-4/src/test/kotlin/com/baeldung/pair/NullabilityTests.kt
+++ core-kotlin-modules/core-kotlin-lang-4/src/test/kotlin/com/baeldung/pair/NullabilityTests.kt
@@ -1,25 +1,14 @@
 package com.baeldung.pair
 
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.Test
-import kotlin.test.assertEquals
 
 class NullabilityTests {
     @Test
-    fun testPrintNameLength() {
-        val name: String? = "John"
-        val expectedLength = 4
-        val actualLength = name?.length
-        assertEquals(expectedLength, actualLength)
-    }
-
-    @Test
-    fun testPrintNonNullValue() {
-        val nullableValue: String? = "Hello, World!"
-        val expectedValue = "Hello, World!"
-        var actualValue: String? = null
-        nullableValue?.let {
-            actualValue = it
-        }
-        assertEquals(expectedValue, actualValue)
+    fun Test(){
+        val name: String? = null
+        assertThatThrownBy {
+            name!!.length
+        }.isInstanceOf(NullPointerException::class.java)
     }
 }
