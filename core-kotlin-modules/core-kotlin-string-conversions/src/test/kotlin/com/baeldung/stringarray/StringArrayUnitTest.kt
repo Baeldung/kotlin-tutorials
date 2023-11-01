32ee173f5f0ce44b411eebe3bc766f5c5b2243c2
albertache1998 <albertache1998@gmail.com>
2023-11-01 15:51:20 +0100

    placed unit test in new package

--- core-kotlin-modules/core-kotlin-string-conversions/src/test/kotlin/com/baeldung/stringarray/StringArrayUnitTest.kt
+++ core-kotlin-modules/core-kotlin-string-conversions/src/test/kotlin/com/baeldung/stringarray/StringArrayUnitTest.kt
@@ -1,7 +1,6 @@
 package com.baeldung.stringarray
 
 import org.junit.jupiter.api.Assertions.assertArrayEquals
-import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import java.io.BufferedReader
 import java.io.StringReader
