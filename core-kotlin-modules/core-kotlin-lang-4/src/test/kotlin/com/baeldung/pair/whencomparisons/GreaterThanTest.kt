4d20a6e731d909b0c8493eecb580c512c7d74c48
Kanake <10kanake@gmail.com>
2023-11-07 09:55:12 +0300

    when_comparisons (#675)
    * when clause tests
    
    * delete repeated code
    
    * change package name
    
    * typo fix

---
+++ core-kotlin-modules/core-kotlin-lang-4/src/test/kotlin/com/baeldung/pair/whencomparisons/GreaterThanTest.kt
@@ -0,0 +1,19 @@
+package com.baeldung.pair.whencomparisons
+
+import org.junit.Assert.assertEquals
+import org.junit.jupiter.api.Test
+
+class GreaterThanTest {
+    fun evaluateTemperature(temperature: Int): String {
+        return when {
+            temperature > 30 -> "Hot"
+            else -> "Cold"
+        }
+    }
+    @Test
+    fun testEvaluateTemperature() {
+        assertEquals("Hot", evaluateTemperature(35))
+        assertEquals("Cold", evaluateTemperature(0))
+    }
+}
+
---
+++ core-kotlin-modules/core-kotlin-lang-4/src/test/kotlin/com/baeldung/pair/whencomparisons/LessThanTest.kt
@@ -0,0 +1,22 @@
+package com.baeldung.pair.whencomparisons
+
+import org.junit.Assert.assertEquals
+import org.junit.jupiter.api.Test
+
+class LessThanTest {
+    fun evaluateScore(score: Int): String {
+        return when {
+            score < 50 -> "Fail"
+            score < 75 -> "Pass"
+            score < 90 -> "Good"
+            else -> "Excellent"
+        }
+    }
+
+    @Test
+    fun testEvaluateScore() {
+        assertEquals("Fail", evaluateScore(35))
+        assertEquals("Pass", evaluateScore(60))
+        assertEquals("Good", evaluateScore(85))
+    }
+}
