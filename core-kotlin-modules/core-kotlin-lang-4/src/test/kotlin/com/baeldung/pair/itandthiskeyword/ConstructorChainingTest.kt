e66bf4ad8f2ff79c5bebcd3e1e4c613104a53542
kanake10 <10kanake@gmail.com>
2023-11-11 23:55:29 +0300

    test

--- core-kotlin-modules/core-kotlin-lang-4/src/test/kotlin/com/baeldung/pair/itandthiskeyword/ConstructorChainingTest.kt
+++ core-kotlin-modules/core-kotlin-lang-4/src/test/kotlin/com/baeldung/pair/itandthiskeyword/ConstructorChainingTest.kt
@@ -8,6 +8,12 @@ class ConstructorChainingTest {
     data class PersonData(val name: String, val age: Int) {
         constructor(name: String) : this(name, 0)
     }
+    @Test
+    fun testSecondaryConstructor() {
+        val person = PersonData("Alice")
+        assertEquals("Alice", person.name)
+        assertEquals(0, person.age)
+    }
 
     @Test
     fun testPersonWithAge() {
