38edabc6e6e44e007c85b6005433eaee419d2c3f
nappy29 <ewaneenombe@gmail.com>
2023-11-15 16:54:08 +0100

    added unit tests

---
+++ core-kotlin-modules/core-kotlin-strings-4/src/test/kotlin/com/baeldung/repeatstring/RepeatStringUnitTest.kt
@@ -0,0 +1,54 @@
+package com.baeldung.repeatstring
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class RepeatStringUnitTest {
+
+    @Test
+    fun testRepeatString() {
+        val string = "Hello World"
+        val n = 3
+        assertEquals("Hello WorldHello WorldHello World", repeatString(string, n))
+    }
+
+    @Test
+    fun `repeat string n times using repeat() method`() {
+        val str = "Hello World"
+        val repeated = str.repeat(3)
+        assertEquals("Hello WorldHello WorldHello World", repeated)
+    }
+
+    @Test
+    fun `repeat string n times using stringbuilder method`() {
+        val str = "Hello World"
+        val repeated = StringBuilder().apply {
+            repeat(3) {
+                append(str)
+            }
+        }.toString()
+        assertEquals("Hello WorldHello WorldHello World", repeated)
+    }
+
+    @Test
+    fun `repeat string n times using CharArray and String constructor method`() {
+        val str = "Hello World"
+        val repeated = String(CharArray(33) { str[it % str.length] })
+        assertEquals("Hello WorldHello WorldHello World", repeated)
+    }
+
+    @Test
+    fun `repeat string n times using Sequence and JoinToString method`() {
+        val str = "Hello World"
+        val repeated = generateSequence { str }.take(3).joinToString("")
+        assertEquals("Hello WorldHello WorldHello World", repeated)
+    }
+
+    fun repeatString(string: String, n: Int): String {
+        var result = ""
+        for (i in 1..n) {
+            result += string
+        }
+        return result
+    }
+}
