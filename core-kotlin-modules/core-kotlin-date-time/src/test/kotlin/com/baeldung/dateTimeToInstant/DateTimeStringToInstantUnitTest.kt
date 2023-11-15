d736dc8fa642278688a9ec069c05fd234e35702c
gaepi <gae.piaz@gmail.com>
2023-11-15 19:02:56 +0100

    JAVA-27254 | fixed test

--- core-kotlin-modules/core-kotlin-date-time/src/test/kotlin/com/baeldung/dateTimeToInstant/DateTimeStringToInstantUnitTest.kt
+++ core-kotlin-modules/core-kotlin-date-time/src/test/kotlin/com/baeldung/dateTimeToInstant/DateTimeStringToInstantUnitTest.kt
@@ -13,25 +13,25 @@ class DateTimeStringToInstantUnitTest {
         val strTime = "15:20"
         val strDateTime = strDate + "T" + strTime
         val ldt = LocalDateTime.parse(strDateTime)
-        val instant: Instant = ldt.atZone(ZoneId.systemDefault()).toInstant()
+        val instant: Instant = ldt.atZone(ZoneId.of("Europe/Rome")).toInstant()
         assertEquals("2021-11-25T14:20:00Z", instant.toString())
     }
 
     @Test
     fun `given date and time strings parse directly to localdatetime then use zoneId`(){
-        var strDate = "2021-11-25"
-        var strTime = "15:20"
+        val strDate = "2021-11-25"
+        val strTime = "15:20"
         val ldt = LocalDateTime.of(LocalDate.parse(strDate), LocalTime.parse(strTime))
-        val instant: Instant = ldt.atZone(ZoneId.systemDefault()).toInstant()
+        val instant: Instant = ldt.atZone(ZoneId.of("Europe/Rome")).toInstant()
         assertEquals("2021-11-25T14:20:00Z", instant.toString())
     }
 
     @Test
     fun `given date and time strings, parse directly to  zoneddatetime object`(){
-        var strDate = "2021-11-25"
-        var strTime = "15:20"
-        val zdt1 = ZonedDateTime.of(LocalDate.parse(strDate), LocalTime.parse(strTime),ZoneId.systemDefault() )
-        val zdt2 = ZonedDateTime.of(LocalDateTime.of(LocalDate.parse(strDate), LocalTime.parse(strTime)), ZoneId.systemDefault())
+        val strDate = "2021-11-25"
+        val strTime = "15:20"
+        val zdt1 = ZonedDateTime.of(LocalDate.parse(strDate), LocalTime.parse(strTime), ZoneId.of("Europe/Rome"))
+        val zdt2 = ZonedDateTime.of(LocalDateTime.of(LocalDate.parse(strDate), LocalTime.parse(strTime)), ZoneId.of("Europe/Rome"))
         val instant1: Instant = zdt1.toInstant()
         val instant2: Instant = zdt2.toInstant()
         assertEquals("2021-11-25T14:20:00Z", instant1.toString())
@@ -42,7 +42,7 @@ class DateTimeStringToInstantUnitTest {
     fun `given date and time strings, use it together with datetimeformatter on zoneddatetime object`(){
         val strDate = "2021-11-25"
         val strTime = "15:20"
-        val dtf = DateTimeFormatter.ISO_LOCAL_DATE_TIME.withZone(ZoneId.systemDefault())
+        val dtf = DateTimeFormatter.ISO_LOCAL_DATE_TIME.withZone(ZoneId.of("Europe/Rome"))
         val zdt = ZonedDateTime.parse(strDate + "T" + strTime, dtf)
         val instant = zdt.toInstant()
         assertEquals("2021-11-25T14:20:00Z", instant.toString())
@@ -53,7 +53,7 @@ class DateTimeStringToInstantUnitTest {
         val strDate = "2021-11-25"
         val strTime = "15:20"
         val ldt = LocalDateTime.of(LocalDate.parse(strDate), LocalTime.parse(strTime))
-        val instant = ldt.toInstant(ZoneId.systemDefault().rules.getOffset(ldt))
+        val instant = ldt.toInstant(ZoneId.of("Europe/Rome").rules.getOffset(ldt))
         assertEquals("2021-11-25T14:20:00Z", instant.toString())
     }
 }
