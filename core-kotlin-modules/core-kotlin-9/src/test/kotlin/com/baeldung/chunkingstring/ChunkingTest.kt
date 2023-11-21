1075c82af7415c2f410211471c44259374429ae4
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2023-11-21 03:04:08 -0300

    [KTLN-730] Add samples (#691)
    * Add samples
    
    * Update samples

---
+++ core-kotlin-modules/core-kotlin-9/src/test/kotlin/com/baeldung/chunkingstring/ChunkingTest.kt
@@ -0,0 +1,46 @@
+package com.baeldung.chunkingstring
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class ChunkingTest {
+
+    private val inputString = "HelloWorld"
+
+    @Test
+    fun testBasicChunked() {
+        val chunkSize = 3
+        val expected = listOf("Hel", "loW", "orl", "d")
+        val actual = inputString.chunked(chunkSize)
+        assertEquals(expected, actual, "Basic chunked method failed")
+    }
+
+    @Test
+    fun testChunkedWithPadEnd() {
+        val chunkSize = 3
+        val paddedInput = inputString.padEnd(inputString.length + chunkSize - (inputString.length % chunkSize), ' ')
+        val expected = listOf("Hel", "loW", "orl", "d  ")
+        val actual = paddedInput.chunked(chunkSize)
+        assertEquals(expected, actual, "Chunked with padEnd method failed")
+    }
+
+    @Test
+    fun testWindowedChunking() {
+        val chunkSize = 3
+        val expected = listOf("Hel", "loW", "orl", "d")
+        val actual = inputString.windowed(size = chunkSize, step = chunkSize, partialWindows = true)
+        assertEquals(expected, actual, "Windowed chunking failed")
+    }
+
+    @Test
+    fun testManualChunking() {
+        val chunkSize = 3
+        val expected = listOf("Hel", "loW", "orl", "d")
+        val actual = mutableListOf<String>()
+        for (i in 0 until inputString.length step chunkSize) {
+            val end = (i + chunkSize).coerceAtMost(inputString.length)
+            actual.add(inputString.substring(i, end))
+        }
+        assertEquals(expected, actual, "Manual chunking failed")
+    }
+}
