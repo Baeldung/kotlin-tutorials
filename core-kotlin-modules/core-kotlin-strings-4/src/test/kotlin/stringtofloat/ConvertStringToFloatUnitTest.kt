1068a9c713e20ccf2eacd2c0392a5f731c420a81
nappy29 <ewaneenombe@gmail.com>
2023-12-02 23:30:13 +0100

    KTLN-738: Converting String to a Float Kotlin (#702)
    * added unit tests
    
    * fixed unit tests
    
    * fixed unit tests

---
+++ core-kotlin-modules/core-kotlin-strings-4/src/test/kotlin/stringtofloat/ConvertStringToFloatUnitTest.kt
@@ -0,0 +1,47 @@
+package stringtofloat
+
+import org.junit.jupiter.api.Test
+import java.text.NumberFormat
+import java.util.*
+import kotlin.test.assertEquals
+import kotlin.test.assertNull
+
+class ConvertStringToFloatUnitTest {
+
+    @Test
+    fun `convert string to float using the toFloat method`() {
+        val myString = "8.73"
+
+        assertEquals(8.73F, myString.toFloat())
+    }
+
+    @Test
+    fun `convert string to float using the toFloatOrNull method`() {
+        val myString = "Invalid"
+
+        assertNull(myString.toFloatOrNull())
+    }
+
+    @Test
+    fun `convert string to float using the NumberFormat class`() {
+        assertEquals(8.73F, stringToFloat("8.73"))
+    }
+
+    @Test
+    fun `convert string to float using Regex`() {
+        assertEquals(8.73F, stringToFloatUsingRegex("8.73"))
+        assertEquals(8.73F, stringToFloatUsingRegex("this is a float number 8.73"))
+    }
+
+    fun stringToFloat(input: String): Float {
+        val nf = NumberFormat.getInstance(Locale.US)
+        return nf.parse(input).toFloat()
+    }
+
+    fun stringToFloatUsingRegex(input: String): Float {
+        val regex = Regex("[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)")
+        val match = regex.find(input)
+        return match?.value?.toFloatOrNull() ?: 0f
+    }
+
+}
