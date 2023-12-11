58e43e3cf5dd6d74bab7dbc103ab48317bac543d
kanake10 <10kanake@gmail.com>
2023-12-11 20:52:19 +0300

    update sucesses and failure states

--- core-kotlin-modules/core-kotlin-9/src/test/kotlin/com/baeldung/result/ResultWithStates.kt
+++ core-kotlin-modules/core-kotlin-9/src/test/kotlin/com/baeldung/result/ResultWithStates.kt
@@ -14,8 +14,8 @@ fun failureExample(): Result<Nothing> {
 
 class ResultWithStates {
     @Test
-    fun `Should handle Sucessful States`() {
-        val result = successExample()
+    fun `Should handle Successful States`() {
+        val result = Result.success(42)
         assertTrue(result.isSuccess)
         assertEquals(42, result.getOrNull())
         result.onSuccess { println(it) }
@@ -23,8 +23,8 @@ class ResultWithStates {
 
 
     @Test
-    fun `Should handle failure states`() {
-        val result = failureExample()
+    fun `Should handle Failure States`() {
+        val result = Result.failure<Int>(Exception("We have an error!"))
         assertTrue(result.isFailure)
         assertNotNull(result.exceptionOrNull())
         result.onFailure { println(it.message) }
