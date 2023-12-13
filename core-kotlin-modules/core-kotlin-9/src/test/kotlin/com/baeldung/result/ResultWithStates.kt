48b5d08495cc5a427c2108507dfeef0fe942c550
kanake10 <10kanake@gmail.com>
2023-12-14 01:54:25 +0300

    update sucesses and failure states

--- core-kotlin-modules/core-kotlin-9/src/test/kotlin/com/baeldung/result/ResultWithStates.kt
+++ core-kotlin-modules/core-kotlin-9/src/test/kotlin/com/baeldung/result/ResultWithStates.kt
@@ -2,16 +2,6 @@ package com.baeldung.result
 
 import org.junit.Assert.*
 import org.junit.jupiter.api.Test
-
-fun successExample(): Result<Int> {
-    return Result.success(42)
-}
-
-
-fun failureExample(): Result<Nothing> {
-    return Result.failure(Exception("We have an error!"))
-}
-
 class ResultWithStates {
     @Test
     fun `Should handle Successful States`() {
@@ -20,7 +10,6 @@ class ResultWithStates {
         assertEquals(42, result.getOrNull())
         result.onSuccess { println(it) }
     }
-
 
     @Test
     fun `Should handle Failure States`() {
