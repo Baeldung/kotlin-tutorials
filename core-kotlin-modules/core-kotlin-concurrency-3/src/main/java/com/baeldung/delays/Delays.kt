d34c30cee871ae6a9f54f3e9dd82413216438c5f
kanake <10kanake@gmail.com>
2023-12-28 13:05:12 +0300

    delays

---
+++ core-kotlin-modules/core-kotlin-concurrency-3/src/main/java/com/baeldung/delays/Delays.kt
@@ -0,0 +1,44 @@
+package com.baeldung.delays
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+
+
+import kotlinx.coroutines.*
+
+val lock = Object()
+
+fun main() {
+    println("We're testing .wait()!")
+
+    runBlocking(Dispatchers.Default) {
+        launch(Dispatchers.IO) {
+            testWaitThread2()
+        }
+        testWaitThread1()
+    }
+}
+
+fun testWaitThread1() = synchronized(lock) {
+    lock.wait()
+    println("Print second")
+}
+
+fun testWaitThread2() = synchronized(lock) {
+    println("Print first")
+    lock.notify()
+}
+
+
+fun sleepMethod() {
+    println("Thread 1 is sleeping...")
+    Thread.sleep(2000) // Sleep for 2 seconds
+    println("Thread 1 woke up!")
+}
+
+
+fun delayMethod() = runBlocking {
+    println("Coroutine 1 is delaying...")
+    delay(2000) // Delay for 2 seconds
+    println("Coroutine 1 resumed!")
+}
