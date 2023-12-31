467a3f28ffccf77efc1f3d7862d4eed383a5b9e8
kanake <10kanake@gmail.com>
2023-12-31 17:21:04 +0300

    update

--- kotlin-patterns/src/main/kotlin/com/baeldung/mediator/Mediator.kt
+++ kotlin-patterns/src/main/kotlin/com/baeldung/mediator/Mediator.kt
@@ -1,33 +1,41 @@
 package com.baeldung.mediator
 
-// Mediator interface
-interface ChatMediator {
-    fun sendMessage(message: String, user: User)
+// Step 1: Introduce Mediator Interface
+interface Mediator {
+    fun pressButton()
+}
+// Light module
+class Light(private val mediator: Mediator) {
+    private var isOn: Boolean = false
+    fun turnOn() {
+        println("Light turned on")
+        isOn = true
+        mediator.pressButton()
+    }
+    fun turnOff() {
+        println("Light turned off")
+        isOn = false
+        mediator.pressButton()
+    }
+
+    fun isOn(): Boolean {
+        return isOn
+    }
+}
+class Button(private val mediator: Mediator) {
+    fun press() {
+        mediator.pressButton()
+    }
 }
 // Concrete Mediator
-class ChatMediatorImpl : ChatMediator {
-    private val users = mutableListOf<User>()
-    override fun sendMessage(message: String, user: User) {
-        // Broadcast the message to all users except the sender
-        users.filter { it != user }.forEach { it.receiveMessage(message) }
-    }
-    fun addUser(user: User) {
-        users.add(user)
-    }
-}
-
-// Colleague interface
-interface User {
-    fun sendMessage(message: String)
-    fun receiveMessage(message: String)
-}
-// Concrete Colleague
-class ChatUser(private val name: String, private val mediator: ChatMediator) : User {
-    override fun sendMessage(message: String) {
-        println("$name sends: $message")
-        mediator.sendMessage(message, this)
-    }
-    override fun receiveMessage(message: String) {
-        println("$name receives: $message")
+class ConcreteMediator : Mediator {
+    private val light: Light = Light(this)
+    private val button: Button = Button(this)
+    override fun pressButton() {
+        if (light.isOn()) {
+            light.turnOff()
+        } else {
+            light.turnOn()
+        }
     }
 }
