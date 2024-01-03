8886de62b146f1ac4e4f68c2b3874ac0b841f6b9
@hangga <bazeniancode@gmail.com>
2024-01-03 23:43:48 +0700

    add inline function

--- core-kotlin-modules/core-kotlin-datastructures-2/src/test/kotlin/com/baeldung/sortalphabetically/SortStringAlphabeticallyUnitTest.kt
+++ core-kotlin-modules/core-kotlin-datastructures-2/src/test/kotlin/com/baeldung/sortalphabetically/SortStringAlphabeticallyUnitTest.kt
@@ -1,6 +1,7 @@
 package com.baeldung.sortalphabetically
 
 import org.junit.Test
+import kotlin.math.abs
 import kotlin.test.assertEquals
 
 class SortStringAlphabeticallyUnitTest {
@@ -22,6 +23,12 @@ class SortStringAlphabeticallyUnitTest {
     }
 
     @Test
+    fun `using toCharArray and sorted`() {
+        val sortedString = inputString.toCharArray().sorted().joinToString("")
+        assertEquals("abcde", sortedString)
+    }
+
+    @Test
     fun `using toList and sortedBy`() {
         val sortedString = inputString.toList().sortedBy { it }.joinToString("")
         assertEquals("abcde", sortedString)
@@ -32,4 +39,12 @@ class SortStringAlphabeticallyUnitTest {
         val sortedString = inputString.toList().sortedWith(compareBy { it }).joinToString("")
         assertEquals("abcde", sortedString)
     }
+
+    private fun String.sortAsc() = String(toCharArray().apply { sort() })
+
+    @Test
+    fun `simplify using inline function`() {
+        assertEquals("abcde", inputString.sortAsc())
+    }
+
 }
