199665bbe02cc625f87982b3c935973a8cf5c600
nappy29 <ewaneenombe@gmail.com>
2024-01-05 12:53:14 +0100

    fixed unit tests

--- kotlin-math-2/src/test/kotlin/com/baeldung/math/standardDeviation/StandardDeviationUnitTest.kt
+++ kotlin-math-2/src/test/kotlin/com/baeldung/math/standardDeviation/StandardDeviationUnitTest.kt
@@ -2,16 +2,17 @@ package com.baeldung.math.standardDeviation
 import org.apache.commons.math3.stat.descriptive.moment.StandardDeviation
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import kotlin.math.pow
 
 class StandardDeviationUnitTest {
 
     @Test
-    fun `standard deviation using the math library`() {
+    fun `standard deviation using the math package`() {
         val dataset1 = doubleArrayOf(1.0, 2.0, 3.0, 4.0, 5.0, 6.0)
         val dataset2 = doubleArrayOf(11.0, 14.0, 19.0, 23.0, 28.0, 30.0)
 
-        assertEquals(1.707825127659933, standardDeviationUsingMathLibrary(dataset1))
-        assertEquals(6.914156170897181, standardDeviationUsingMathLibrary(dataset2))
+        assertEquals(1.707825127659933, standardDeviationUsingMathPackage(dataset1))
+        assertEquals(6.914156170897181, standardDeviationUsingMathPackage(dataset2))
     }
 
     @Test
@@ -24,9 +25,9 @@ class StandardDeviationUnitTest {
     }
 
 
-    fun standardDeviationUsingMathLibrary(dataset: DoubleArray): Double {
+    fun standardDeviationUsingMathPackage(dataset: DoubleArray): Double {
         val mean = dataset.average()
-        val variance = dataset.map { (it - mean) * (it - mean) }.average()
+        val variance = dataset.map { (it - mean).pow(2) }.average()
         return Math.sqrt(variance)
     }
 
