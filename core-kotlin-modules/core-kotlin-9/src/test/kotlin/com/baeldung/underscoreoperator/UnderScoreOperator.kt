8b39dadc32aff0621cd890690e57bf11f9ac4a3b
kanake <10kanake@gmail.com>
2024-01-08 09:15:42 +0300

    update

--- core-kotlin-modules/core-kotlin-9/src/test/kotlin/com/baeldung/underscoreoperator/UnderScoreOperator.kt
+++ core-kotlin-modules/core-kotlin-9/src/test/kotlin/com/baeldung/underscoreoperator/UnderScoreOperator.kt
@@ -1,5 +1,4 @@
 package com.baeldung.underscoreoperator
-
 // Before Kotlin 1.7
 class Box<T>(value: T) {
     // Some class implementation
@@ -8,27 +7,19 @@ fun main() {
     val intBox: Box<Int> = Box(42)
     val anyBox: Box<Any> = Box("Some value")
 }
-
-
 fun main1(args: Array<String>) {
     val (_, second) = "ignored" to "hello"
     println(second)
 }
-
 fun mainTwo(args: Array<String>) {
     val list = listOf("hi")
     val list2 = list.mapIndexed { _, item -> item }
     println(list2)
 }
-
-
 inline fun <T> printElementInfo(element: T) {
+    println("Element: $element")
 }
 fun mainForHOF() {
     printElementInfo<_>(42)
     printElementInfo<_>("Hello")
 }
-
-
-
-
