685a932d73b214d04437f9712f7a72d6cc5fbc13
Kanake <10kanake@gmail.com>
2024-01-08 11:08:36 +0300

    Update

--- core-kotlin-modules/core-kotlin-date-time/src/test/kotlin/com/baeldung/currentdatetime/CurrentDateTImeUnitTest.kt
+++ core-kotlin-modules/core-kotlin-date-time/src/test/kotlin/com/baeldung/currentdatetime/CurrentDateTImeUnitTest.kt
@@ -53,7 +53,7 @@ class CurrentDateTImeUnitTest {
 
         val current = LocalDateTime.of(
             calendar.get(Calendar.YEAR),
-            calendar.get(Calendar.MONTH) + 1,
+            calendar.get(Calendar.MONTH),
             calendar.get(Calendar.DAY_OF_MONTH),
             calendar.get(Calendar.HOUR_OF_DAY),
             calendar.get(Calendar.MINUTE),
