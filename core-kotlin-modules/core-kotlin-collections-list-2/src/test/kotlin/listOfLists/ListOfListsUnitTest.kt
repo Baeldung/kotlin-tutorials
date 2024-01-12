a2e7059cf7c7cb0d771d211fd823e7b831bb902c
albertache1998 <albertache1998@gmail.com>
2024-01-12 02:51:44 +0100

    code cleanup

--- core-kotlin-modules/core-kotlin-collections-list-2/src/test/kotlin/listOfLists/ListOfListsUnitTest.kt
+++ core-kotlin-modules/core-kotlin-collections-list-2/src/test/kotlin/listOfLists/ListOfListsUnitTest.kt
@@ -8,32 +8,32 @@ class ListOfListsUnitTest {
 
     @Test
     fun `Creates an immutable list of immutable lists using listOf()`() {
-        val listOfLists = listOf(listOf(1, 2, 3), listOf(4, 5, 6), listOf(7, 8, 9))
+        val listOfLists = listOf(listOf(1, 2), listOf(3, 4), listOf(5, 6))
 
         assertEquals(3, listOfLists.size)
-        assertEquals(listOf(1, 2, 3), listOfLists[0])
-        assertEquals(listOf(4, 5, 6), listOfLists[1])
-        assertEquals(listOf(7, 8, 9), listOfLists[2])
+        assertEquals(listOf(1, 2), listOfLists[0])
+        assertEquals(listOf(3, 4), listOfLists[1])
+        assertEquals(listOf(5, 6), listOfLists[2])
     }
 
     @Test
     fun `Creates an immutable list of mutable lists using List constructor`() {
-        val listOfLists = List(3) { MutableList<Int>(3) {0} }
+        val listOfLists = List(3) { MutableList<Int>(2) {0} }
 
         assertEquals(3, listOfLists.size)
-        assertEquals(listOf(0, 0, 0), listOfLists[0])
-        assertEquals(listOf(0, 0, 0), listOfLists[1])
-        assertEquals(listOf(0, 0, 0), listOfLists[2])
+        assertEquals(listOf(0, 0), listOfLists[0])
+        assertEquals(listOf(0, 0), listOfLists[1])
+        assertEquals(listOf(0, 0), listOfLists[2])
     }
 
     @Test
     fun `Creates an immutable list of immutable lists using map method`() {
-        val listOfLists = (0..2).map { _ -> (0..2).map { 0 } }
+        val listOfLists = (0..2).map { _ -> (0..1).map { 0 } }
 
         assertEquals(3, listOfLists.size)
-        assertEquals(listOf(0, 0, 0), listOfLists[0])
-        assertEquals(listOf(0, 0, 0), listOfLists[1])
-        assertEquals(listOf(0, 0, 0), listOfLists[2])
+        assertEquals(listOf(0, 0), listOfLists[0])
+        assertEquals(listOf(0, 0), listOfLists[1])
+        assertEquals(listOf(0, 0), listOfLists[2])
     }
 
     @Test
