731852f518cd2fdb94aa62a7ae32917523b57def
nappy29 <ewaneenombe@gmail.com>
2024-01-18 03:59:20 +0100

    added unit tests

---
+++ core-kotlin-modules/core-kotlin-collections-map/src/test/kotlin/com/baeldung/skipNullListToMap/SkipNullListToMapUnitTest.kt
@@ -0,0 +1,53 @@
+package com.baeldung.skipNullListToMap
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class SkipNullListToMapUnitTest {
+
+    @Test
+    fun `Skips null values using for loop`() {
+        val pairs = listOf<Pair<String, Int?>>(Pair("a", 1), Pair("b", null), Pair("c", 3))
+        val expected = mapOf("a" to 1, "c" to 3)
+        val map = mutableMapOf<String, Int>()
+        for (pair in pairs) {
+            if (pair.second != null) {
+                map[pair.first] = pair.second!!
+            }
+        }
+        assertEquals(expected, map)
+    }
+
+    @Test
+    fun `Skips null values using the filter method`() {
+        val list = listOf<Pair<String, Int?>>(Pair("a", 1), Pair("b", null), Pair("c", 3))
+        val filteredList = list.filter { it.second != null }
+        val map = filteredList.toMap()
+
+        val expected = mapOf("a" to 1, "c" to 3)
+        assertEquals(expected, map)
+    }
+
+    @Test
+    fun `Skips null values using the mapNotNull method`() {
+        val pairs = listOf<Pair<String, Int?>>(Pair("a", 1), Pair("b", null), Pair("c", 3))
+        val expected = mapOf("a" to 1, "c" to 3)
+        val result = pairs.mapNotNull { it.second?.let { value -> it.first to value } }.toMap()
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `Skips null values using the fold method`() {
+        val pairs = listOf<Pair<String, Int?>>(Pair("a", 1), Pair("b", null), Pair("c", 3))
+        val expected = mapOf("a" to 1, "c" to 3)
+        val map = pairs.fold(mutableMapOf<String, Int>()) { acc, pair ->
+            if (pair.second != null) {
+                acc.apply { put(pair.first, pair.second!!) }
+            } else {
+                acc
+            }
+        }
+        assertEquals(expected, map)
+    }
+}
