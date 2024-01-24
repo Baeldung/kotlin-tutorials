db5a99c61b4bc5b45dc0ed783718aa03068f1029
Kanake <10kanake@gmail.com>
2024-01-24 13:24:01 +0300

    Update
    Co-authored-by: Brandon Ward <brandon.ward1993@gmail.com>

--- core-kotlin-modules/core-kotlin-concurrency-3/src/main/java/com/baeldung/comparecollectandcollectlatest/UsingCollect.kt
+++ core-kotlin-modules/core-kotlin-concurrency-3/src/main/java/com/baeldung/comparecollectandcollectlatest/UsingCollect.kt
@@ -6,7 +6,7 @@ import kotlinx.coroutines.runBlocking
 fun main() {
     runBlocking {
         flowOf(1, 2, 3).collect { value ->
-                    println(value)
+            println(value)
         }
     }
 }
