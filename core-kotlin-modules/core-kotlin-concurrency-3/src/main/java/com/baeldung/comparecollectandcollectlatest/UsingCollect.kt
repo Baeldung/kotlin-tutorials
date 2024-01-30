d8745b409754d67cca41ba0d3623f044f1f270b9
Kanake <10kanake@gmail.com>
2024-01-30 08:10:36 +0300

    collect (#786)
    * commit
    
    * commit
    
    * Update
    
    Co-authored-by: Brandon Ward <brandon.ward1993@gmail.com>
    
    * Update
    
    Co-authored-by: Brandon Ward <brandon.ward1993@gmail.com>
    
    ---------
    
    Co-authored-by: Brandon Ward <brandon.ward1993@gmail.com>

---
+++ core-kotlin-modules/core-kotlin-concurrency-3/src/main/java/com/baeldung/comparecollectandcollectlatest/UsingCollect.kt
@@ -0,0 +1,12 @@
+package com.baeldung.comparecollectandcollectlatest
+
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+    runBlocking {
+        flowOf(1, 2, 3).collect { value ->
+            println(value)
+        }
+    }
+}
