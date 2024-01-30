945281580baf6b2bd3af46b716cbf0ad26144785
nappy29 <ewaneenombe@gmail.com>
2024-01-30 19:44:01 +0100

    fixed unit tests

--- core-kotlin-modules/core-kotlin-collections-list-2/src/test/kotlin/repeatedListElements/RepeatedListElementsUnitTest.kt
+++ core-kotlin-modules/core-kotlin-collections-list-2/src/test/kotlin/repeatedListElements/RepeatedListElementsUnitTest.kt
@@ -6,48 +6,49 @@ import org.junit.jupiter.api.Test
 class RepeatedListElementsUnitTest {
     @Test
     fun `find repeated elements in a list using for loop`() {
-        val list = listOf(1, 2, 3, 2, 4, 3, 5, 3, 6)
+        val list = listOf(1, 2, 3, 2, 4, 3, 5, 2, 6)
 
-        assertEquals(listOf(2, 3), findRepeatedValuesUsingForLoop(list))
+        assertEquals(3, findRepeatedValuesUsingForLoop(2, list))
+        assertEquals(2, findRepeatedValuesUsingForLoop(3, list))
+        assertEquals(1, findRepeatedValuesUsingForLoop(4, list))
+        assertEquals(0, findRepeatedValuesUsingForLoop(9, list))
     }
 
     @Test
     fun `find repeated elements in a list using hashmap`() {
-        val list = listOf(1, 2, 3, 2, 4, 3, 5, 3, 6)
+        val list = listOf(1, 2, 3, 2, 4, 3, 5, 2, 6)
 
-        assertEquals(listOf(2, 3), findRepeatedValuesUsingHashMap(list))
+        assertEquals(3, findRepeatedValuesUsingHashMap(2, list))
+        assertEquals(2, findRepeatedValuesUsingHashMap(3, list))
+        assertEquals(1, findRepeatedValuesUsingHashMap(4, list))
+        assertEquals(0, findRepeatedValuesUsingHashMap(9, list))
     }
 
     @Test
-    fun `find repeated elements in a list using groupBy method`() {
-        val list = listOf(1, 2, 3, 2, 4, 3, 5, 3, 6)
-        val result = list.groupBy { it }.mapValues { it.value.size }.filterValues { it > 1 }.keys.toList()
+    fun `find repeated elements in a list using count method`() {
+        val list = listOf(1, 2, 3, 2, 4, 3, 5, 2, 6)
 
-        assertEquals(listOf(2, 3), result)
+        assertEquals(3, list.count{it == 2})
+        assertEquals(2, list.count{it == 3})
+        assertEquals(1, list.count{it == 4})
+        assertEquals(0, list.count{it == 9})
     }
 }
-fun findRepeatedValuesUsingForLoop(list: List<Int>): List<Int> {
-    val countList = MutableList(list.size) { 1 }
+fun findRepeatedValuesUsingForLoop(value: Int, list: List<Int>): Int {
+    var count = 0
     for (i in 0 until list.size - 1) {
-        for (j in i + 1 until list.size) {
-            if (list[i] == list[j]) {
-                countList[i] += 1
-                countList[j] += 1
-            }
+        if (list[i] == value) {
+            count ++
         }
     }
-    val repeatedValues = mutableListOf<Int>()
-    for (i in 0 until list.size) {
-        if (countList[i] > 1 && !repeatedValues.contains(list[i])) {
-            repeatedValues.add(list[i])
-        }
-    }
-    return repeatedValues
+
+    return count
 }
-fun findRepeatedValuesUsingHashMap(list: List<Int>): List<Int> {
+fun findRepeatedValuesUsingHashMap(value: Int, list: List<Int>): Int {
     val map = HashMap<Int, Int>()
     for (element in list) {
         map[element] = map.getOrDefault(element, 0) + 1
     }
-    return map.filterValues { it > 1 }.keys.toList()
+    return if (!list.contains(value)) 0 else map.getValue(value)
 }
+
