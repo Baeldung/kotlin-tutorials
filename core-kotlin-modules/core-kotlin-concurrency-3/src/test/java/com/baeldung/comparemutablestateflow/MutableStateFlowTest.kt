1b0a94b6f47db264014fea6e72593130c4131a8c
kanake <10kanake@gmail.com>
2024-02-03 15:47:12 +0300

    commit

--- core-kotlin-modules/core-kotlin-concurrency-3/src/test/java/com/baeldung/comparemutablestateflow/MutableStateFlowTest.kt
+++ core-kotlin-modules/core-kotlin-concurrency-3/src/test/java/com/baeldung/comparemutablestateflow/MutableStateFlowTest.kt
@@ -5,23 +5,19 @@ import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
-
 class MutableStateFlowTest {
-
     @Test
     fun `initial value should be Baeldung Team`() {
         val mutableStateFlow = MutableStateFlow("Baeldung Team")
         val currentValue = mutableStateFlow.value
         assertEquals("Baeldung Team", currentValue)
     }
-
     @Test
     fun `emit function should update the value`() = runBlocking {
         val mutableStateFlow = MutableStateFlow("Baledung Team")
         mutableStateFlow.emit("Baledung Team Kotlin")
         assertEquals("Baledung Team Kotlin", mutableStateFlow.value)
     }
-
     @Test
     fun `tryEmit function should update the value`() {
         val mutableStateFlow = MutableStateFlow("Baledung Team Kotlin 2024")
@@ -29,5 +25,4 @@ class MutableStateFlowTest {
         assertTrue(emitResult)
         assertEquals("Baledung Team Kotlin 2024", mutableStateFlow.value)
     }
-
 }
