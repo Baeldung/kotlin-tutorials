59661230cfb53dd359a52198b4285749ce03a12c
husam-e <2340614+husam-e@users.noreply.github.com>
2024-02-04 17:54:03 -0500

    feat(kproperty): How to get a property's name in Kotlin

---
+++ core-kotlin-modules/core-kotlin-7/src/test/kotlin/com/baeldung/kproperty/KPropertyTest.kt
@@ -0,0 +1,29 @@
+package com.baeldung.kproperty
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class KPropertyTest {
+    
+    @Test
+    fun `can get property name of a class's property statically`() {
+        class MyClass(val age: Int, var name: String, val next: MyClass?)
+
+        Assertions.assertEquals("next", MyClass::next.name)
+    }
+
+    @Test
+    fun `can get property name of an instance's property`() {
+        class MyClass(val age: Int, var name: String, val next: MyClass?)
+        val instance = MyClass(5, "test", null)
+
+        Assertions.assertEquals("next", instance::next.name)
+    }
+
+    @Test
+    fun `can get the name of a function`() {
+        fun isValid(num: Int, name: String): Boolean = num > 0 && name.isNotBlank()
+
+        Assertions.assertEquals("isValid", ::isValid.name)
+    }
+}
