a67d07e8ecdd7039fbacfad441caa09c3ba03652
kent <kent.yuan@gmail.com>
2024-02-13 15:06:56 +0100

    [sb-@Value] renaming

--- spring-boot-kotlin/src/main/kotlin/com/baeldung/theValueAnnotation/ValueBean.kt
+++ spring-boot-kotlin/src/main/kotlin/com/baeldung/theValueAnnotation/ValueBean.kt
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 @Component
-class MyMagicBean(
+class ValueBean(
     // without default values
     @Value("\${my-app.magic-number}")
     val magicNumber: Int,
