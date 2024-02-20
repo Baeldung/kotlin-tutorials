740ca1901923297a14677327bf2c23bb0a94f47b
kanake <10kanake@gmail.com>
2024-02-20 19:12:49 +0300

    update

--- kotlin-testing/src/test/kotlin/com/baeldung/usingspy/CalculatorTests.kt
+++ kotlin-testing/src/test/kotlin/com/baeldung/usingspy/CalculatorTests.kt
@@ -1,6 +1,9 @@
 package com.baeldung.usingspy
 
+import io.mockk.clearMocks
+import io.mockk.every
 import io.mockk.spyk
+import io.mockk.verify
 import junit.framework.Assert.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -20,5 +23,15 @@ class CalculatorTest {
         val spy = spyk<Calculator>()
         val result = spy.findAverage(5,5)
         assertEquals(5, result)
+    }
+
+    @Test
+    fun testPartialMocking() {
+        val spy = spyk<Calculator>()
+        every { spy.add(any(), any()) } returns 2
+        val result = spy.findAverage(5, 5)
+        verify { spy.add(5, 5) }
+        assertEquals(1, result)
+        clearMocks(spy)
     }
 }
