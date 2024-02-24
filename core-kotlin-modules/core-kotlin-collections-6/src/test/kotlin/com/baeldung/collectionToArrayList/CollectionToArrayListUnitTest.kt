ceb65854196bb8e59e6e18a867f6065b63059918
albertache1998 <albertache1998@gmail.com>
2024-02-24 16:31:05 +0100

    refactored code

--- core-kotlin-modules/core-kotlin-collections-6/src/test/kotlin/com/baeldung/collectionToArrayList/CollectionToArrayListUnitTest.kt
+++ core-kotlin-modules/core-kotlin-collections-6/src/test/kotlin/com/baeldung/collectionToArrayList/CollectionToArrayListUnitTest.kt
@@ -8,7 +8,7 @@ class CollectionToArrayListUnitTest {
 
     @Test
     fun `converts a Collection to ArrayList using the for loop and add method`() {
-        val collection = listOf("Kotlin", "Java", "Scala")
+        val collection: Collection<String> = listOf("Kotlin", "Java", "Scala")
         val arrayList = ArrayList<String>()
         for (element in collection) {
             arrayList.add(element)
@@ -19,7 +19,7 @@ class CollectionToArrayListUnitTest {
 
     @Test
     fun `converts a Collection to ArrayList using the ArrayList constructor`() {
-        val collection = listOf("Kotlin", "Java", "Scala")
+        val collection: Collection<String>  = listOf("Kotlin", "Java", "Scala")
         val arrayList = ArrayList(collection)
 
         assertEquals(arrayListOf("Kotlin", "Java", "Scala"), arrayList)
@@ -27,7 +27,7 @@ class CollectionToArrayListUnitTest {
 
     @Test
     fun `converts a Collection to ArrayList using the toCollection method`() {
-        val collection = listOf("Kotlin", "Java", "Scala")
+        val collection: Collection<String>  = listOf("Kotlin", "Java", "Scala")
         val arrayList = collection.toCollection(ArrayList())
 
         assertEquals(arrayListOf("Kotlin", "Java", "Scala"), arrayList)
@@ -35,7 +35,7 @@ class CollectionToArrayListUnitTest {
 
     @Test
     fun `converts a Collection to ArrayList using the addAll method`() {
-        val collection = listOf("Kotlin", "Java", "Scala")
+        val collection: Collection<String>  = listOf("Kotlin", "Java", "Scala")
         val arrayList = ArrayList<String>()
         arrayList.addAll(collection)
 
@@ -43,10 +43,10 @@ class CollectionToArrayListUnitTest {
     }
 
     @Test
-    fun `converts a Collection to ArrayList using the set method`() {
-        val collection = listOf("Kotlin", "Java", "Scala")
-        val arrayList = ArrayList(List(collection.size) { "" })
-        collection.forEachIndexed { index, element -> arrayList.set(index, element) }
+    fun `converts a Collection to ArrayList using the mapTo method`() {
+        val collection: Collection<String>  = listOf("Kotlin", "Java", "Scala")
+        val arrayList = ArrayList<String>()
+        collection.mapTo(arrayList) { it }
 
         assertEquals(arrayListOf("Kotlin", "Java", "Scala"), arrayList)
     }
