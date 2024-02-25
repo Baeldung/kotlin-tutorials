dd9bec960e07db1aec3d99f6752e65a52c9407a5
Fabio Tadashi Miyasato <miyasato.fabio@gmail.com>
2024-02-25 16:26:41 -0300

    Testing Ktor Controllers

---
+++ kotlin-ktor/src/main/java/com/baeldung/testing/data/Car.kt
@@ -0,0 +1,7 @@
+package com.baeldung.testing.data
+
+data class Car(
+    val id: String,
+    var brand: String,
+    var price: Double
+)
---
+++ kotlin-ktor/src/main/java/com/baeldung/testing/data/CarStorageMock.kt
@@ -0,0 +1,7 @@
+package com.baeldung.testing.data
+
+object CarStorageMock {
+
+    val carStorage = ArrayList<Car>()
+
+}
