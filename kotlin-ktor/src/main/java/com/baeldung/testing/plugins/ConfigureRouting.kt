5d2cc18fa42168bec00c36876dbafc59be627cdc
Fabio Tadashi Miyasato <miyasato.fabio@gmail.com>
2024-02-25 16:32:55 -0300

    fix formatting

--- kotlin-ktor/src/main/java/com/baeldung/testing/plugins/ConfigureRouting.kt
+++ kotlin-ktor/src/main/java/com/baeldung/testing/plugins/ConfigureRouting.kt
@@ -11,41 +11,41 @@ import io.ktor.server.routing.*
 fun Application.configureRouting() {
     routing {
         route("cars") {
-        get {
-            call.respond(CarStorageMock.carStorage)
-        }
-        get("{id?}") {
-            val id = call.parameters["id"]
-            val car = CarStorageMock.carStorage.find { it.id == id } ?: return@get call.respondText(
-                text = "car.not.found",
-                status = HttpStatusCode.NotFound
-            )
-            call.respond(car)
-        }
-        post {
-            val car = call.receive<Car>()
-            CarStorageMock.carStorage.add(car)
-            call.respond(status = HttpStatusCode.Created, message = car)
-        }
-        put("{id?}") {
-            val id = call.parameters["id"]
-            val car = CarStorageMock.carStorage.find { it.id == id } ?: return@put call.respondText(
-                text = "car.not.found",
-                status = HttpStatusCode.NotFound
-            )
-            val carUpdate = call.receive<Car>()
-            car.brand = carUpdate.brand
-            car.price = carUpdate.price
-            call.respond(car)
-        }
-        delete("{id?}") {
-            val id = call.parameters["id"]
-            if (CarStorageMock.carStorage.removeIf { it.id == id }) {
-                call.respondText(text = "car.deleted", status = HttpStatusCode.OK)
-            } else {
-                call.respondText(text = "car.not.found", status = HttpStatusCode.NotFound)
+            get {
+                call.respond(CarStorageMock.carStorage)
             }
-        }
+            get("{id?}") {
+                val id = call.parameters["id"]
+                val car = CarStorageMock.carStorage.find { it.id == id } ?: return@get call.respondText(
+                    text = "car.not.found",
+                    status = HttpStatusCode.NotFound
+                )
+                call.respond(car)
+            }
+            post {
+                val car = call.receive<Car>()
+                CarStorageMock.carStorage.add(car)
+                call.respond(status = HttpStatusCode.Created, message = car)
+            }
+            put("{id?}") {
+                val id = call.parameters["id"]
+                val car = CarStorageMock.carStorage.find { it.id == id } ?: return@put call.respondText(
+                    text = "car.not.found",
+                    status = HttpStatusCode.NotFound
+                )
+                val carUpdate = call.receive<Car>()
+                car.brand = carUpdate.brand
+                car.price = carUpdate.price
+                call.respond(car)
+            }
+            delete("{id?}") {
+                val id = call.parameters["id"]
+                if (CarStorageMock.carStorage.removeIf { it.id == id }) {
+                    call.respondText(text = "car.deleted", status = HttpStatusCode.OK)
+                } else {
+                    call.respondText(text = "car.not.found", status = HttpStatusCode.NotFound)
+                }
+            }
         }
     }
 }
