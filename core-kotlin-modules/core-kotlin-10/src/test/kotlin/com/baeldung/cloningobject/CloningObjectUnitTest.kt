0a4b163db74194d65cc44e0972ca8f0b25b14d32
@hangga <bazeniancode@gmail.com>
2024-02-27 05:58:33 +0700

    different changes each test

--- core-kotlin-modules/core-kotlin-10/src/test/kotlin/com/baeldung/cloningobject/CloningObjectUnitTest.kt
+++ core-kotlin-modules/core-kotlin-10/src/test/kotlin/com/baeldung/cloningobject/CloningObjectUnitTest.kt
@@ -20,13 +20,15 @@ data class Person(var name: String, var address: Address) : Cloneable {
 class CloningObjectUnitTest {
 
     private val address = Address("Jln. Kemasan", "Yogyakarta")
-    private val person = Person("Hangga", address)
+    private val person = Person("Hangga Aji Sayekti", address)
 
     @Test
     fun `when using secondary constructor then proves that shallow copy`() {
         val clonedPerson = Person(person)
+
         person.address.city = "Surabaya"
         person.address.street = "Jln. Ahmad Yani"
+
         assertThat(clonedPerson).isNotSameAs(person)
         assertThat(clonedPerson.address.city).isEqualTo("Surabaya")
         assertThat(clonedPerson.address.street).isEqualTo("Jln. Ahmad Yani")
@@ -35,18 +37,22 @@ class CloningObjectUnitTest {
     @Test
     fun `when using copy then proves that shallow copy`() {
         val clonedPerson = person.copy()
-        person.address.city = "Palembang"
-        person.address.street = "Jln. Abi Hasan"
+
+        person.address.city = "Jakarta"
+        person.address.street = "Jln. Abdul Muis"
+
         assertThat(clonedPerson).isNotSameAs(person)
-        assertThat(clonedPerson.address.city).isEqualTo("Palembang")
-        assertThat(clonedPerson.address.street).isEqualTo("Jln. Abi Hasan")
+        assertThat(clonedPerson.address.city).isEqualTo("Jakarta")
+        assertThat(clonedPerson.address.street).isEqualTo("Jln. Abdul Muis")
     }
 
     @Test
     fun `when using clone then proves that shallow copy`() {
         val clonedPerson = person.clone()
+
         person.address.city = "Palembang"
         person.address.street = "Jln. Abi Hasan"
+
         assertThat(clonedPerson).isNotSameAs(person)
         assertThat(clonedPerson.address.city).isEqualTo("Palembang")
         assertThat(clonedPerson.address.street).isEqualTo("Jln. Abi Hasan")
@@ -55,8 +61,10 @@ class CloningObjectUnitTest {
     @Test
     fun `when own function then proves that deep copy`() {
         val clonedPerson = person.deepCopy()
+
         person.address.city = "Bandung"
         person.address.street = "Jln. Siliwangi"
+
         assertThat(clonedPerson).isNotSameAs(person)
         assertThat(clonedPerson.address.city).isNotEqualTo("Bandung")
         assertThat(clonedPerson.address.street).isNotEqualTo("Jln. Siliwangi")
