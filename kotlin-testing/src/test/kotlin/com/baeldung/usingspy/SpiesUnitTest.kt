fddfe9c7147955d2c52462df402ed2672da87dc4
Kanake <10kanake@gmail.com>
2024-02-29 09:58:48 +0300

    spies (#815)
    * commit
    
    * update
    
    * Update kotlin-testing/src/test/kotlin/com/baeldung/usingspy/CalculatorTests.kt
    
    Co-authored-by: Brandon Ward <brandon.ward1993@gmail.com>
    
    * Update kotlin-testing/src/test/kotlin/com/baeldung/usingspy/CalculatorTests.kt
    
    Co-authored-by: Brandon Ward <brandon.ward1993@gmail.com>
    
    * renaming the file
    
    * commit
    
    ---------
    
    Co-authored-by: Brandon Ward <brandon.ward1993@gmail.com>

---
+++ kotlin-testing/src/test/kotlin/com/baeldung/usingspy/SpiesUnitTest.kt
@@ -0,0 +1,37 @@
+package com.baeldung.usingspy
+
+import io.mockk.clearMocks
+import io.mockk.every
+import io.mockk.spyk
+import io.mockk.verify
+import junit.framework.Assert.assertEquals
+import org.junit.jupiter.api.Test
+
+class Calculator {
+    fun add(a: Int, b: Int): Int {
+        return a + b
+    }
+    fun findAverage(a: Int, b: Int): Int {
+        val total = add(a, b)
+        return total / 2;
+    }
+}
+
+class SpiesUnitTest {
+    @Test
+    fun testSpy() {
+        val spy = spyk<Calculator>()
+        val result = spy.findAverage(5, 5)
+        assertEquals(5, result)
+    }
+
+    @Test
+    fun testPartialMocking() {
+        val spy = spyk<Calculator>()
+        every { spy.add(any(), any()) } returns 2
+        val result = spy.findAverage(5, 5)
+        verify { spy.add(5, 5) }
+        assertEquals(1, result)
+        clearMocks(spy)
+    }
+}
