7bc5dc2b75f9c48b42ce932b41a7ed08f35e0b2c
nappy29 <ewaneenombe@gmail.com>
2024-03-05 19:56:59 +0100

    KTLN-522: In-Place Modification of Map Entry in Kotlin (#820)
    * unit test code
    
    * rename test method to use set instead of assign
    
    * fixed cod

---
+++ core-kotlin-modules/core-kotlin-collections-map/src/test/kotlin/com/baeldung/modifyMapEntryInPlace/ModifyMapEntryInPlaceUnitTest.kt
@@ -0,0 +1,47 @@
+package com.baeldung.modifyMapEntryInPlace
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class ModifyMapEntryInPlaceUnitTest {
+
+    @Test
+    fun `modifies mutable map entry using put method`() {
+        val map = mutableMapOf("key1" to "value1", "key2" to "value2")
+        map.put("key1", "new value")
+        assertEquals("new value", map["key1"])
+    }
+
+    @Test
+    fun `modifies mutable map entry using bracket operator`() {
+        val map = mutableMapOf("key1" to "value1", "key2" to "value2")
+        map["key1"] = "new value"
+
+        assertEquals("new value", map["key1"])
+    }
+
+    @Test
+    fun `modifies mutable map entry using replace method`() {
+        val map = mutableMapOf("key1" to "value1", "key2" to "value2")
+        map.replace("key1", "new value")
+        assertEquals("new value", map["key1"])
+    }
+
+
+    @Test
+    fun `modifies mutable map entry using compute method`() {
+        val map = mutableMapOf("key1" to "value1", "key2" to "value2")
+        map.compute("key1") { _, _ -> "new value" }
+        assertEquals("new value", map["key1"])
+    }
+
+    @Test
+    fun `modifies mutable map entry using computeIfPresent method`() {
+        val map = mutableMapOf("key1" to "value1", "key2" to "value2")
+        map.computeIfPresent("key1") { key, value ->
+            if (value == "value1") "new value"
+            else value
+        }
+        assertEquals("new value", map["key1"])
+    }
+}
