42f343a50a671fec2c287b4e7dd1b9a0fdb8d4c7
@hangga <bazeniancode@gmail.com>
2024-03-18 01:06:25 +0700

    update

--- core-kotlin-modules/core-kotlin-concurrency-3/src/main/java/com/baeldung/schedulingrepeatingtask/UsingScheduledThreadPool.kt
+++ core-kotlin-modules/core-kotlin-concurrency-3/src/main/java/com/baeldung/schedulingrepeatingtask/UsingScheduledThreadPool.kt
@@ -4,11 +4,8 @@ import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 
 fun main(){
-    var count = 0
     val scheduler = Executors.newSingleThreadScheduledExecutor()
     scheduler.scheduleAtFixedRate({
         println("Complex task completed!")
-        count++
-        if (count > 10) scheduler.shutdown()
     }, 0L, 1L, TimeUnit.SECONDS)
 }
--- core-kotlin-modules/core-kotlin-concurrency-3/src/test/java/com/baeldung/schedulingrepeatingtask/SchedulingRepeatingTaskUnitTest.kt
+++ core-kotlin-modules/core-kotlin-concurrency-3/src/test/java/com/baeldung/schedulingrepeatingtask/SchedulingRepeatingTaskUnitTest.kt
@@ -1,20 +1,88 @@
 package com.baeldung.schedulingrepeatingtask
 
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.take
+import kotlinx.coroutines.flow.transform
 import org.junit.jupiter.api.Test
-import java.util.*
-import java.util.concurrent.CountDownLatch
-import kotlin.concurrent.schedule
+import org.junit.jupiter.api.assertThrows
+import kotlin.time.Duration.Companion.milliseconds
 
-class SchedulingRepeatingTaskUnitTest{
+class SchedulingRepeatingTaskUnitTest {
 
     @Test
-    fun `using timer`(){
-        val latch = CountDownLatch(1)
-        val timer = Timer()
-        timer.schedule(0L, 1000L) {
+    fun `using repeat and delay`() = runBlocking {
+        repeat(10) {
             println("Timer ticked!")
-            latch.countDown()
+            delay(1000)
         }
-        latch.await()
     }
+
+    @Test
+    fun `using withTimeout`(): Unit = runBlocking {
+        assertThrows<TimeoutCancellationException> {
+            withTimeout(5000) {
+                while (true) {
+                    println("Waiting for timeout")
+                    delay(1000.milliseconds)
+                }
+            }
+        }
+    }
+
+    // Using flow & collect
+    @Test
+    fun `using flow collect`() = runBlocking {
+        var count = 0
+        val job = launch {
+            flow {
+                while (true) {
+                    emit(Unit)
+                    delay(1000.milliseconds)
+                }
+            }.collect {
+                count++
+                println("Task executed")
+                if (count == 10) cancel() // cancel after 10 executions
+            }
+        }
+        job.join()
+    }
+
+    @Test
+    fun `using flow and take`() = runBlocking {
+        flow {
+            while (true) {
+                emit(Unit)
+                delay(1000.milliseconds)
+            }
+        }.take(10).collect {
+            println("Task executed times")
+        }
+    }
+
+    // using flowOf & collect
+    @Test
+    fun `using flowOf`() = runBlocking {
+        flowOf(1, 2, 3, 4, 5).collect {
+            println("Received value: $it")
+            delay(1000.milliseconds)
+        }
+    }
+
+    // using flowOf, transform and collect
+    @Test
+    fun `using flowOf with transform`() = runBlocking {
+        flowOf(1, 2, 3, 4, 5).transform { value ->
+            emit("Task executed $value times")
+        }.collect {
+            println(it)
+            delay(1000.milliseconds)
+        }
+    }
+
+    // Using flow, take & collect
+
+
 }
