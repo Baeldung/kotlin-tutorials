7682536b0995cfb2e1c37b3bc634fa2e57589286
Yadukrishnan <yadavan88@gmail.com>
2024-03-29 10:44:10 +0100

    removed println

--- core-kotlin-modules/core-kotlin-numbers-2/src/test/kotlin/com/baeldung/decimalformat/DecimalFormatUnitTest.kt
+++ core-kotlin-modules/core-kotlin-numbers-2/src/test/kotlin/com/baeldung/decimalformat/DecimalFormatUnitTest.kt
@@ -37,45 +37,35 @@ class DecimalFormatUnitTest {
     @Test
     fun `format double using format method`() {
         val num = 0.000123456789
-        println(num)
         val result = formatDoubleUsingFormat(num)
-        println(result)
         assertFalse(result.contains("E"))
     }
 
     @Test
     fun `format double using bigdecimal method`() {
         val num: Double = 0.000123456789
-        println(num)
         val result = formatUsingBigDecimal(num)
-        println(result)
         assertFalse(result.contains("E"))
     }
 
     @Test
     fun `format double using DecimalFormat method`() {
         val num: Double = 0.000123456789
-        println(num)
         val result = formatUsingDecimalFormat(num)
-        println(result)
         assertFalse(result.contains("E"))
     }
 
     @Test
     fun `format double using String method`() {
-        val num: Double = 123456789.1234567888889
-        println(num)
+        val num: Double = 0.000123456789
         val result = formatUsingString(num)
-        println(result)
         assertFalse(result.contains("E"))
     }
 
     @Test
     fun `format double using NumberFormat method`() {
         val num: Double = 0.000123456789
-        println(num)
         val result = formatUsingNumberFormat(num)
-        println(result)
         assertFalse(result.contains("E"))
     }
 }
