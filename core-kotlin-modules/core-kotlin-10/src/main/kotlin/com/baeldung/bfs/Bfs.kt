78c720acd006fc5df7a78875f1d3b961f0937e95
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2024-04-03 10:45:27 -0400

    [KTLN-666] Add Samples (#849)
    * [KTLN-666] Add Samples
    
    * [KTLN-666] Add Samples

---
+++ core-kotlin-modules/core-kotlin-10/src/main/kotlin/com/baeldung/bfs/Bfs.kt
@@ -0,0 +1,15 @@
+package com.baeldung.bfs
+
+fun bfs(graph: Map<Int, List<Int>>, start: Int): List<Int> {
+    val visited = mutableListOf<Int>()
+    val queue = ArrayDeque<Int>()
+    queue.add(start)
+    while (queue.isNotEmpty()) {
+        val vertex = queue.removeFirst()
+        if (vertex !in visited) {
+            visited.add(vertex)
+            graph[vertex]?.let { neighbors -> queue.addAll(neighbors.filterNot { it in visited }) }
+        }
+    }
+    return visited
+}
