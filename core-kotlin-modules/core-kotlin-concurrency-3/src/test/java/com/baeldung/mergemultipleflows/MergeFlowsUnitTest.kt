843634d5506faeaf7fd0af4fe1883d8f32e7503d
kanake <10kanake@gmail.com>
2024-04-04 21:37:25 +0300

    commit

--- core-kotlin-modules/core-kotlin-concurrency-3/src/test/java/com/baeldung/mergemultipleflows/MergeFlowsUnitTest.kt
+++ core-kotlin-modules/core-kotlin-concurrency-3/src/test/java/com/baeldung/mergemultipleflows/MergeFlowsUnitTest.kt
@@ -9,7 +9,7 @@ import kotlin.test.assertEquals
 class FlowMergingTests {
 
     @Test
-    fun testMerge() = runBlocking {
+    fun `should merge using the merge function`() = runBlocking {
         val flow1 = flowOf(1, 2, 3, 4)
         val flow2 = flowOf(5, 6, 7, 8)
         val mergedFlow = merge(flow1, flow2)
@@ -21,73 +21,56 @@ class FlowMergingTests {
     }
 
     @Test
-    fun testZip() = runBlocking {
+    fun `should merge using zip`() = runBlocking {
         val flow1 = flowOf(1, 2, 3)
         val flow2 = flowOf("A", "B", "C")
-        val result = mutableListOf<String>()
-        flow1.zip(flow2) { num, letter ->
+
+        val result = flow1.zip(flow2) { num, letter ->
             "$num$letter"
-        }.collect { mergedValue ->
-            result.add(mergedValue)
-        }
+        }.toList()
+
         assertEquals(listOf("1A", "2B", "3C"), result)
     }
 
+
     @Test
-    fun testCombine() = runBlocking {
+    fun `should merge using combine`() = runBlocking {
         val flow1 = MutableStateFlow(0)
         val flow2 = flowOf(1, 2, 3)
-        val result = mutableListOf<Int>()
-        flow1.combine(flow2) { num1, num2 ->
+        val result = flow1.combine(flow2) { num1, num2 ->
             num1 + num2
-        }.collect { mergedValue ->
-            result.add(mergedValue)
-        }
+        }.toList()
         assertEquals(listOf(1, 2, 3, 4, 5, 6), result)
     }
 
+
     @Test
-    fun testFlatMapConcat() = runBlocking {
+    fun `should merge using flatmapconcat`() = runBlocking {
         val flow1 = flowOf(1, 2, 3)
         val flow2 = flowOf(4, 5, 6)
-        val result = mutableListOf<Int>()
-        flow1.flatMapConcat { value1 ->
+        val result = flow1.flatMapConcat { value1 ->
             flow2.map { value2 -> value1 + value2 }
-        }.collect { mergedValue ->
-            result.add(mergedValue)
-        }
+        }.toList()
         assertEquals(listOf(5, 6, 7, 6, 7, 8, 7, 8, 9), result)
     }
 
+
     @Test
-    fun testFlatMapMerge() = runBlocking {
+    fun `should merge using flatmerge`() = runBlocking {
         val flow1 = flowOf(1, 2, 3)
         val flow2 = flowOf(4, 5, 6)
-        val result = mutableListOf<Int>()
-        flow1.flatMapMerge { value1 ->
+        val result = flow1.flatMapMerge { value1 ->
             flow2.map { value2 -> value1 + value2 }
-        }.collect { mergedValue ->
-            result.add(mergedValue)
-        }
+        }.toList()
         assertEquals(listOf(5, 6, 7, 6, 7, 8, 7, 8, 9), result)
     }
 
+
     @Test
-    fun testRunningReduce() = runBlocking {
+    fun `should merge using runningReduce`() = runBlocking {
         val result = flowOf(1, 2, 3, 4)
                 .runningReduce { acc, value -> acc + value }
                 .toList()
         assertEquals(listOf(1, 3, 6, 10), result)
-    }
-
-    @Test
-    fun testRunningFold() = runBlocking {
-        val flow1 = flowOf(1, 2, 3)
-        val flow2 = flowOf(4, 5, 6)
-        val mergedFlow = flow1.zip(flow2) { a, b -> a + b }
-                .runningFold(emptyList<Int>()) { acc, value -> acc + value }
-                .toList()
-                .last()
-        Assert.assertEquals(listOf(5, 7, 9), mergedFlow)
     }
 }
