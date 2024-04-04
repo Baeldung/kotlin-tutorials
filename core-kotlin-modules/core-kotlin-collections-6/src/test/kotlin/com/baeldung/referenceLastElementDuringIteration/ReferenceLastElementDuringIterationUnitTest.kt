083d357405799786bd32ab2e5112b67e82a0d59f
albertache1998 <145406331+albertache1998@users.noreply.github.com>
2024-04-04 07:51:23 +0100

    [KTLN-675] Iterate a Collection Referencing Previous Element in Kotlin (#845)
    * added unit tests
    
    * fixed code
    
    * fixed code
    
    * made code fixes
    
    * code fixes

---
+++ core-kotlin-modules/core-kotlin-collections-6/src/test/kotlin/com/baeldung/referenceLastElementDuringIteration/ReferenceLastElementDuringIterationUnitTest.kt
@@ -0,0 +1,60 @@
+package com.baeldung.referenceLastElementDuringIteration
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class ReferenceLastElementDuringIterationUnitTest {
+
+    @Test
+    fun `creates new list by adding elements from original list using loop`() {
+        val list = listOf(1, 2, 3, 4, 5)
+        val expectedList = listOf("1","12", "23", "34", "45")
+
+        assertEquals(expectedList, iterateListUsingLoop(list))
+    }
+
+    @Test
+    fun `creates new list by adding elements from original list using fold method`() {
+        val list = listOf(1, 2, 3, 4, 5)
+        val expectedList = listOf("1", "12", "23", "34", "45")
+
+        assertEquals(expectedList, iterateListUsingFoldIndexed(list))
+    }
+    @Test
+    fun `creates new list by adding elements from original list using zipWitNnext method`() {
+        val list = listOf("1", "2", "3", "4", "5")
+        val expectedList = listOf("1", "12", "23", "34", "45")
+
+        val result = (list.take(1) + list.zipWithNext { a, b -> "$a$b" })
+
+        assertEquals(expectedList, result)
+    }
+
+    @Test
+    fun `creates new list by adding elements from original list using scan method`() {
+        val list = listOf(1, 2, 3, 4, 5)
+        val expectedList = listOf("1", "12", "23", "34", "45")
+        val result = list.drop(1).scan(list.first().toString()) { acc, i -> acc.takeLast(1) + i }
+
+        assertEquals(expectedList, result)
+    }
+}
+fun iterateListUsingLoop(list: List<Int>): List<String> {
+    val newList = mutableListOf<String>()
+    newList.add(list[0].toString())
+    for (i in 1 until list.size) {
+        newList.add("${list[i - 1]}${list[i]}")
+    }
+    return newList
+}
+
+fun iterateListUsingFoldIndexed(list: List<Int>): List<String> {
+    return list.foldIndexed(mutableListOf()) { i, acc, element ->
+        if(i==0)
+            acc.add(element.toString())
+        if (i > 0) {
+            acc.add("${list[i - 1]}$element")
+        }
+        acc
+    }
+}
