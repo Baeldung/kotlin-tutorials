1ab2f81be57847177be60ef7df8efa22efb794f1
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2024-04-07 18:57:53 -0300

    [KTLN-676] Add samples

---
+++ core-kotlin-modules/core-kotlin-10/src/main/kotlin/com/baeldung/dijkstra/Dijkstra.kt
@@ -0,0 +1,23 @@
+package com.baeldung.dijkstra
+
+import java.util.PriorityQueue
+
+fun dijkstra(graph: Map<Int, List<Pair<Int, Int>>>, start: Int): Map<Int, Int> {
+    val distances = mutableMapOf<Int, Int>().withDefault { Int.MAX_VALUE }
+    val priorityQueue = PriorityQueue<Pair<Int, Int>>(compareBy { it.second }).apply {
+        add(start to 0)
+    }
+    distances[start] = 0
+
+    while (priorityQueue.isNotEmpty()) {
+        val (node, currentDist) = priorityQueue.poll()
+        graph[node]?.forEach { (adjacent, weight) ->
+            val totalDist = currentDist + weight
+            if (totalDist < distances.getValue(adjacent)) {
+                distances[adjacent] = totalDist
+                priorityQueue.add(adjacent to totalDist)
+            }
+        }
+    }
+    return distances
+}
---
+++ core-kotlin-modules/core-kotlin-10/src/test/kotlin/com/baeldung/dijkstra/DijkstraUnitTest.kt
@@ -0,0 +1,32 @@
+package com.baeldung.dijkstra
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class DijkstraUnitTest {
+
+    @Test
+    fun `Should calculate shortest path when using Dijkstra algorithm`() {
+        val graph = mapOf(
+            1 to listOf(
+                Pair(2, 10),
+                Pair(3, 15)
+            ),
+            2 to listOf(
+                Pair(4, 12)
+            ),
+            3 to listOf(
+                Pair(4, 15)
+            ),
+            4 to listOf(
+                Pair(5, 12),
+                Pair(6, 15)
+            ),
+            5 to emptyList(),
+            6 to emptyList()
+        )
+
+        val shortestPaths = dijkstra(graph, 1)
+        assertEquals(37, shortestPaths.getValue(6))
+    }
+}
