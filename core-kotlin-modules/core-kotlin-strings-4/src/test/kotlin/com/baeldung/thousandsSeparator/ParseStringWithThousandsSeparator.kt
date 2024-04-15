f7371ee107281a00d85d068a9ea6f109ee06b7c4
albertache1998 <albertache1998@gmail.com>
2024-04-15 14:39:33 +0100

    code fixes

--- core-kotlin-modules/core-kotlin-strings-4/src/test/kotlin/com/baeldung/thousandsSeparator/ParseStringWithThousandsSeparator.kt
+++ core-kotlin-modules/core-kotlin-strings-4/src/test/kotlin/com/baeldung/thousandsSeparator/ParseStringWithThousandsSeparator.kt
@@ -1,7 +1,6 @@
 package com.baeldung.thousandsSeparator
 
 import org.junit.jupiter.api.Test
-import java.text.DecimalFormat
 import java.text.NumberFormat
 import java.util.*
 import kotlin.test.assertEquals
@@ -10,11 +9,11 @@ class ParseStringWithThousandsSeparator {
 
     @Test
     fun `parses string with thousands separator using replace method`(){
-        val result1 = parseStringWithSeparatorUsingReplace("1,000")
-        val result2 = parseStringWithSeparatorUsingReplace("25.750")
+        val result1 = parseStringWithCommaSeparatorUsingReplace("1,000")
+        val result2 = parseStringWithDotSeparatorUsingReplace("25.750")
 
-        assertEquals("1000", result1)
-        assertEquals("25750", result2)
+        assertEquals(1000, result1.toInt())
+        assertEquals(25750, result2.toInt())
     }
 
     @Test
@@ -22,21 +21,25 @@ class ParseStringWithThousandsSeparator {
         val result1 = parseStringWithSeparatorUsingNumberFormatWithUSLocale("1,000")
         val result2 = parseStringWithSeparatorUsingNumberFormatWithGermanLocale("25.750")
 
-        assertEquals("1000", result1)
-        assertEquals("25750", result2)
+        assertEquals(1000, result1.toInt())
+        assertEquals(25750, result2.toInt())
     }
 
     @Test
     fun `parses string with thousands separator using string tokenizer`(){
-        val result1 = parseStringWithSeparatorUsingTokenizer("1,000")
-        val result2 = parseStringWithSeparatorUsingTokenizer("25.750")
+        val result1 = parseStringWithCommaSeparatorUsingTokenizer("1,000")
+        val result2 = parseStringWithDotSeparatorUsingTokenizer("25.750")
 
-        assertEquals("1000", result1)
-        assertEquals("25750", result2)
+        assertEquals(1000, result1.toInt())
+        assertEquals(25750, result2.toInt())
     }
 }
-fun parseStringWithSeparatorUsingReplace(input: String): String {
-    return input.replace(Regex("[,.]"), "")
+fun parseStringWithCommaSeparatorUsingReplace(input: String): String {
+    return input.replace(Regex("[,]"), "")
+}
+
+fun parseStringWithDotSeparatorUsingReplace(input: String): String {
+    return input.replace(Regex("[.]"), "")
 }
 
 fun parseStringWithSeparatorUsingNumberFormatWithUSLocale(input: String): String {
@@ -49,8 +52,17 @@ fun parseStringWithSeparatorUsingNumberFormatWithGermanLocale(input: String): St
     val num = number.parse(input)
     return num.toString()
 }
-fun parseStringWithSeparatorUsingTokenizer(input: String): String {
+fun parseStringWithCommaSeparatorUsingTokenizer(input: String): String {
     val tokenizer = StringTokenizer(input, ",.")
+    val builder = StringBuilder()
+    while (tokenizer.hasMoreTokens()) {
+        builder.append(tokenizer.nextToken())
+    }
+    return builder.toString()
+}
+
+fun parseStringWithDotSeparatorUsingTokenizer(input: String): String {
+    val tokenizer = StringTokenizer(input, ".")
     val builder = StringBuilder()
     while (tokenizer.hasMoreTokens()) {
         builder.append(tokenizer.nextToken())
