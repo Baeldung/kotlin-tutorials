bd9a9b160b5540534c45abbf70f60db1f2b5ae21
Yadukrishnan <yadavan88@gmail.com>
2024-04-16 09:21:24 +0200

    Added more tests

--- core-kotlin-modules/core-kotlin-strings-5/src/test/kotlin/com/baeldung/extractnumber/NumberExtractUnitTest.kt
+++ core-kotlin-modules/core-kotlin-strings-5/src/test/kotlin/com/baeldung/extractnumber/NumberExtractUnitTest.kt
@@ -1,6 +1,7 @@
 package com.baeldung.extractnumber
 
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
@@ -47,6 +48,8 @@ class NumberExtractUnitTest {
         "another string with 456 and 789, 456",
         "string 123-234, 123",
         "no numbers,",
+        "3 4 50 numbers6, 3",
+        "123456789large numbers0, 123456789"
     )
     fun `extract first occurrence of number from string using regex`(str: String, expected: String?) {
         val number = extractUsingRegex(str)
@@ -59,6 +62,8 @@ class NumberExtractUnitTest {
         "another string with 456 and 789, 456:789",
         "string 123-234, 123:234",
         "no numbers,",
+        "3 4 50 numbers6, 3:4:50:6",
+        "123456789large numbers0, 123456789:0"
     )
     fun `extract all occurrences of numbers from string using regex`(str: String, expected: String?) {
         val numbers = extractMultipleUsingRegex(str)
@@ -72,6 +77,8 @@ class NumberExtractUnitTest {
         "another string with 456 and 789, 456:789",
         "string 123-234, 123:234",
         "no numbers,",
+        "3 4 50 numbers6, 3:4:50:6",
+        "123456789large numbers0, 123456789:0"
     )
     fun `extract all occurrences of numbers from string using split and regex`(str: String, expected: String?) {
         val numbers = extractNumbersUsingSplitAndRegex(str)
@@ -85,11 +92,31 @@ class NumberExtractUnitTest {
         "another string with 456 and 789, 456:789",
         "string 123-234, 123:234",
         "no numbers,",
+        "3 4 50 numbers6, 3:4:50:6",
+        "123456789large numbers0, 123456789:0"
     )
     fun `extract all occurrences of numbers from string using loop`(str: String, expected: String?) {
         val numbers = extractNumbersUsingLoop(str)
         val expectedList = expected?.split(":")?.map { it.toLong() } ?: emptyList()
         Assertions.assertEquals(expectedList, numbers)
+    }
+
+    @Test
+    fun `check empty string scenario for loop`() {
+        val numbers = extractNumbersUsingLoop("")
+        Assertions.assertIterableEquals(numbers, listOf<Long>())
+    }
+
+    @Test
+    fun `check empty string scenario for split`() {
+        val numbers = extractNumbersUsingSplitAndRegex("")
+        Assertions.assertIterableEquals(numbers, listOf<Long>())
+    }
+
+    @Test
+    fun `check empty string scenario for regex`() {
+        val numbers = extractMultipleUsingRegex("")
+        Assertions.assertIterableEquals(numbers, listOf<Long>())
     }
 
 }
