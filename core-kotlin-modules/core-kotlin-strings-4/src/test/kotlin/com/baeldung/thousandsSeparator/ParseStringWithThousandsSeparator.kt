dcf38fe933a7a3de91254d7fefe4f00c10b91f46
albertache1998 <albertache1998@gmail.com>
2024-04-18 14:57:07 +0100

    code fixes

--- core-kotlin-modules/core-kotlin-strings-4/src/test/kotlin/com/baeldung/thousandsSeparator/ParseStringWithThousandsSeparator.kt
+++ core-kotlin-modules/core-kotlin-strings-4/src/test/kotlin/com/baeldung/thousandsSeparator/ParseStringWithThousandsSeparator.kt
@@ -13,8 +13,8 @@ class ParseStringWithThousandsSeparator {
         val result1 = parseStringUsingReplace("1,000", Locale.US)
         val result2 = parseStringUsingReplace("25.750", Locale.GERMAN)
 
-        assertEquals(1000, result1.toInt())
-        assertEquals(25750, result2.toInt())
+        assertEquals(1000, result1)
+        assertEquals(25750, result2)
     }
 
     @Test
@@ -22,8 +22,8 @@ class ParseStringWithThousandsSeparator {
         val result1 = parseStringWithSeparatorUsingNumberFormat("1,000", Locale.US)
         val result2 = parseStringWithSeparatorUsingNumberFormat("25.750", Locale.GERMAN)
 
-        assertEquals(1000, result1.toInt())
-        assertEquals(25750, result2.toInt())
+        assertEquals(1000, result1)
+        assertEquals(25750, result2)
     }
 
     @Test
@@ -31,28 +31,28 @@ class ParseStringWithThousandsSeparator {
         val result1 = parseStringUsingTokenizer("1,000", Locale.US)
         val result2 = parseStringUsingTokenizer("25.750", Locale.GERMAN)
 
-        assertEquals(1000, result1.toInt())
-        assertEquals(25750, result2.toInt())
+        assertEquals(1000, result1)
+        assertEquals(25750, result2)
     }
 }
 
-fun parseStringUsingReplace(input: String, locale: Locale): String {
+fun parseStringUsingReplace(input: String, locale: Locale): Int {
     val separator = DecimalFormatSymbols.getInstance(locale).groupingSeparator
-    return input.replace(Regex("[$separator]"), "")
+    return input.replace(Regex("[$separator]"), "").toInt()
 }
 
-fun parseStringWithSeparatorUsingNumberFormat(input: String, locale: Locale): String {
+fun parseStringWithSeparatorUsingNumberFormat(input: String, locale: Locale): Int {
     val number = NumberFormat.getInstance(locale)
     val num = number.parse(input)
-    return num.toString()
+    return num.toInt()
 }
 
-fun parseStringUsingTokenizer(input: String, locale: Locale): String {
+fun parseStringUsingTokenizer(input: String, locale: Locale): Int {
     val separator = DecimalFormatSymbols.getInstance(locale).groupingSeparator
     val tokenizer = StringTokenizer(input, separator.toString())
     val builder = StringBuilder()
     while (tokenizer.hasMoreTokens()) {
         builder.append(tokenizer.nextToken())
     }
-    return builder.toString()
+    return builder.toString().toInt()
 }
