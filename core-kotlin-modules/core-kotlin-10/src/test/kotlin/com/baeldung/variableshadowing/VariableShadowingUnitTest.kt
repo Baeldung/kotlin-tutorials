dc28bb02cc49d2be1af728e11799afca1b2faee7
@hangga <bazeniancode@gmail.com>
2024-04-20 06:38:00 +0700

    add solution

--- core-kotlin-modules/core-kotlin-10/src/test/kotlin/com/baeldung/variableshadowing/VariableShadowingUnitTest.kt
+++ core-kotlin-modules/core-kotlin-10/src/test/kotlin/com/baeldung/variableshadowing/VariableShadowingUnitTest.kt
@@ -83,4 +83,79 @@ class VariableShadowingUnitTest{
         assertEquals(0, sum)
     }
 
+    @Test
+    fun `solution to avoid shadowing`(){
+        val topLevelNumber = 10 // Top-level variable
+
+        fun getNumber() : Int {
+            val localNumber = 20 // Local variable without shadowing
+            return localNumber
+        }
+
+        assertEquals(20, getNumber())
+        assertEquals(10, topLevelNumber)
+
+        // in class member
+        class Car {
+            val speed: Int = 100
+
+            fun upSpeed() : Int {
+                val newSpeed = speed * 2 // Using a new variable name to avoid shadowing
+                return newSpeed
+            }
+        }
+
+        assertEquals(100, Car().speed)
+        assertEquals(200, Car().upSpeed())
+
+        fun calculateTotalPrice(discount: Int) {
+            val updatedDiscount = discount + 10 // Using a new variable name to avoid shadowing
+            assertEquals(30, updatedDiscount)
+
+            val price = 100 // local variable
+            val discountRate = 0.1
+
+            fun applyDiscount(price: Int): Double { // Nested function with parameter named 'price'
+                val updatedDiscountRate = 0.2  // Using a new variable name to avoid shadowing
+                return price * (1 - updatedDiscountRate) // 'price' here refers to the parameter, not the outer variable
+            }
+
+            assertEquals(80.0, applyDiscount(price))
+        }
+
+        calculateTotalPrice(20)
+
+        val numbers = listOf(1, 2, 3, 4, 5)
+
+        // in loop
+        for (number in numbers) {
+            val doubledNumber = number * 2
+            val newNumber = number * 2 // Using a new variable name to avoid shadowing
+
+            assertEquals(doubledNumber, newNumber)
+        }
+
+        // in extension
+        assertEquals(15, numbers.sum())
+
+        fun List<Int>.sum(): Int { // shadowing built-in function sum()
+            var sum = 0
+            this.forEach { sum += it * 2 }
+            return sum
+        }
+
+        assertEquals(30, numbers.sum())
+
+        // in lambda
+        var sum = 0
+
+        numbers.forEach { number ->
+            val newNumber = 0 // Using a new variable name to avoid shadowing
+            sum += newNumber
+        }
+
+        assertEquals(0, sum)
+
+    }
+
 }
