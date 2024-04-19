516415df580139674af0e0715603043014619840
@hangga <bazeniancode@gmail.com>
2024-04-19 16:44:50 +0700

    suppressed unused

--- core-kotlin-modules/core-kotlin-10/src/test/kotlin/com/baeldung/variableshadowing/VariableShadowingUnitTest.kt
+++ core-kotlin-modules/core-kotlin-10/src/test/kotlin/com/baeldung/variableshadowing/VariableShadowingUnitTest.kt
@@ -1,3 +1,5 @@
+@file:Suppress("UNUSED_PARAMETER", "UNUSED_VARIABLE", "UNUSED_ANONYMOUS_PARAMETER")
+
 package com.baeldung.variableshadowing
 
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -70,7 +72,7 @@ class VariableShadowingUnitTest{
 
         // in lambda
         var sum = 0
-        var values = listOf(1, 2, 3, 4, 5)
+        val values = listOf(1, 2, 3, 4, 5)
 
         values.forEach { value ->
             val value = 0
@@ -79,96 +81,5 @@ class VariableShadowingUnitTest{
 
         assertEquals(0, sum)
     }
-
-//    @Test
-//    fun `local variable or nested function shadowing`() {
-//
-//        fun calculateTotalPrice() {
-//            val price = 100 // local variable
-//            val discountRate = 0.1
-//
-//            fun applyDiscount(price: Int): Double { // Nested function with parameter named 'price'
-//                val discountRate = 0.2  // Inner variable shadows the outer variable
-//                return price * (1 - discountRate) // 'price' here refers to the parameter, not the outer variable
-//            }
-//
-//            assertEquals(80.0, applyDiscount(price))
-//        }
-//
-//        calculateTotalPrice()
-//    }
-
-//    @Test
-//    fun `loop variable shadowing`() {
-//        val numbers = listOf(1, 2, 3, 4, 5)
-//        for (number in numbers) {
-//            val doubledNumber = number * 2
-//            val number = number * 2 // Shadowing the loop variable 'number'
-//
-//            assertEquals(doubledNumber, number)
-//        }
-//    }
-
-
-    @Test
-    fun `parameter shadowing`() {
-        fun calculateTotalPrice(discount: Int) {
-            assertEquals(300, discount)
-
-            val discount = 10 // Shadowing the parameter 'discount'
-            assertEquals(10, discount)
-
-            val price = 100
-            val totalPrice = price - discount
-            assertEquals(90, totalPrice)
-        }
-
-        calculateTotalPrice(300)
-    }
-
-//    @Test
-//    fun `nested function variable shadowing`(){
-//        fun calculateDiscount(originalPrice: Double) {
-//            val discountRate = 0.1  // Outer variable
-//
-//            fun applyDiscount(price: Double): Double {  // Nested function with parameter 'price'
-//                val discountRate = 0.2  // Inner variable shadows the outer variable
-//                return price * (1 - discountRate)
-//            }
-//
-//            val discountedPrice = applyDiscount(originalPrice)
-//            println("Discounted price: $$discountedPrice")
-//        }
-//
-//        calculateDiscount(200.0)
-//    }
-
-//    @Test
-//    fun `variable shadowing in lambda`(){
-//        var sum = 0
-//        var values = listOf(1, 2, 3, 4, 5)
-//
-//        values.forEach { value ->
-//            val value = 0
-//            sum += value
-//        }
-//
-//        assertEquals(0, sum)
-//    }
-
-//    @Test
-//    fun `shadowing in extension`(){
-//        val numbers = listOf(1, 2, 3, 4, 5)
-//
-//        assertEquals(15, numbers.sum())
-//
-//        fun List<Int>.sum(): Int { // Shadowing occur in here
-//            var sum = 0 // shadowing built-in function sum()
-//            this.forEach { sum += it * 2 }
-//            return sum
-//        }
-//
-//        assertEquals(30, numbers.sum())
-//    }
 
 }
