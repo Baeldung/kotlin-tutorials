035339cc3bc247c8421fb055f7e9d8420e2d26b8
@hangga <bazeniancode@gmail.com>
2024-04-24 18:26:21 +0700

    update simpifiying

--- core-kotlin-modules/core-kotlin-collections-6/src/test/kotlin/com/baeldung/parallelOperationsCollections/ParallelOperationCollectionsUnitTest.kt
+++ core-kotlin-modules/core-kotlin-collections-6/src/test/kotlin/com/baeldung/parallelOperationsCollections/ParallelOperationCollectionsUnitTest.kt
@@ -1,75 +1,63 @@
 package com.baeldung.parallelOperationsCollections
 
-import kotlinx.coroutines.*
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test
-import java.util.stream.Collectors
-import java.util.concurrent.Executors
 import io.reactivex.Observable
 import io.reactivex.schedulers.Schedulers
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.flatMapMerge
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.util.concurrent.Executors
+import java.util.stream.Collectors
 
 
 class ParallelOperationCollectionsUnitTest {
 
-    @Test
-    fun `using parallelStream`(){
-        val list = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    private val list = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
-        val result = list.parallelStream()
-            .map { it * it }
-            .collect(Collectors.toList())
+    private inline fun <T, R> Iterable<T>.parallelMap(crossinline transform: (T) -> R): List<R> =
+        runBlocking { map { async { transform(it) } }.map { it.await() } }
+
+    private inline fun <T> Iterable<T>.forEachParallel(crossinline action: suspend (T) -> Unit) =
+        runBlocking { map { async { action(it) } }.forEach { it.await() } }
+
+    @Test
+    fun `using parallelStream`() {
+        val result = list.parallelStream().map { it * it }.collect(Collectors.toList())
 
         assertEquals(listOf(1, 4, 9, 16, 25, 36, 49, 64, 81, 100), result)
     }
 
     @Test
-    fun `using map, async & transform`(){
-        val list = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    fun `using Coroutines map parallel`() {
         val result = list.parallelMap { it * it }
+
         assertEquals(listOf(1, 4, 9, 16, 25, 36, 49, 64, 81, 100), result)
     }
 
-    private inline fun <T, R> Iterable<T>.parallelMap(crossinline transform: (T) -> R): List<R> {
-        return runBlocking {
-            map { async { transform(it) } }
-                .map { it.await() }
-        }
-    }
-
     @Test
-    fun `using forEachParallel from Coroutines`() = runBlocking{
-        val list = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    fun `using Coroutines forEach parallel`()  {
         val mutex = Mutex()
         val result = mutableListOf<Int>()
 
-        list.forEachParallel(Dispatchers.Default) {
+        list.forEachParallel {
             mutex.withLock {
                 result.add(it * it)
             }
         }
+
         assertEquals(listOf(1, 4, 9, 16, 25, 36, 49, 64, 81, 100), result)
     }
 
-    private suspend inline fun <T> Iterable<T>.forEachParallel(
-        dispatcher: CoroutineDispatcher,
-        crossinline action: suspend (T) -> Unit
-    ) = map { (dispatcher) { action(it) } }
-
     @Test
-    fun `using executorService java`(){
-        val list = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-
+    fun `using executorService java`() {
         val executor = Executors.newFixedThreadPool(1)
 
-        val result = list.map {
-            executor.submit<Int> { it * it }.get()
-        }
+        val result = list.map { executor.submit<Int> { it * it }.get() }
 
         assertEquals(listOf(1, 4, 9, 16, 25, 36, 49, 64, 81, 100), result)
 
@@ -77,24 +65,18 @@ class ParallelOperationCollectionsUnitTest {
     }
 
     @Test
-    fun `using rxJava2`(){
-        val list = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-
+    fun `using rxJava2`() {
         val result = Observable.fromIterable(list)
             .concatMap { Observable.just(it * it).subscribeOn(Schedulers.io()) } // instead flatmap
-            .toList()
-            .blockingGet()
+            .toList().blockingGet()
 
         assertEquals(listOf(1, 4, 9, 16, 25, 36, 49, 64, 81, 100), result)
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun `using Kotlin Flow`() = runBlocking{
-        val list = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-
-        val result = list.asFlow()
-            .flatMapMerge { flow { emit(it * it) } } // ExperimentalCoroutinesApi
+    fun `using Kotlin Flow`() = runBlocking {
+        val result = list.asFlow().flatMapMerge { flow { emit(it * it) } } // ExperimentalCoroutinesApi
             .toList()
 
         assertEquals(listOf(1, 4, 9, 16, 25, 36, 49, 64, 81, 100), result)
