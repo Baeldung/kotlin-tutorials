4ad0e848ae1a5e7c8e3d65e9d77e41183d919c6f
Hangga Aji Sayekti <bazeniancode@gmail.com>
2024-05-05 16:22:48 +0700

    refactor

--- core-kotlin-modules/core-kotlin-collections-6/src/test/kotlin/com/baeldung/parallelOperationsCollections/ParallelOperationCollectionsUnitTest.kt
+++ core-kotlin-modules/core-kotlin-collections-6/src/test/kotlin/com/baeldung/parallelOperationsCollections/ParallelOperationCollectionsUnitTest.kt
@@ -8,6 +8,9 @@ import kotlinx.coroutines.flow.*
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import java.text.SimpleDateFormat
+import java.time.Duration
+import java.time.Instant
+import java.util.*
 import java.util.concurrent.Callable
 import java.util.concurrent.Executors
 import java.util.stream.Collectors
@@ -26,212 +29,165 @@ class ParallelOperationCollectionsUnitTest {
         Person("Charlie", 40)
     )
 
-    private val dateFormat = SimpleDateFormat("yyyy-MM-dd:HH:mm:ss:SSS")
-
-    private fun assertResults(filteredPeople: List<Person>) {
-        assertThat(filteredPeople).containsExactly(
+    private fun List<Person>.assertResultsTrue() {
+        assertThat(this).containsExactly(
             Person("Bob", 16, false), Person("Alice", 30, true), Person("Charlie", 40, true), Person("Ahmad", 42, true)
         )
     }
 
-    @Test
-    fun `using coroutines for parallel operations`() = runBlocking {
+    private val dateFormat = SimpleDateFormat("yyyy-MM-dd:HH:mm:ss:SSS")
+
+    private fun Person.printFormattedInfo() {
+        println(
+            "%-30s %-40s %s".format(
+                dateFormat.format(Date.from(Instant.now())), Thread.currentThread().name, this
+            )
+        )
+    }
+
+    private fun printHeader() {
         println(
             "%-30s %-40s %s".format(
                 "Time", "Thread name", "Operation"
             )
         )
-        val startTime = System.currentTimeMillis()
+        println("-".repeat(115))
+    }
+
+    private fun Instant.printFooter() {
+        val endTime = Instant.now()
+        val duration = Duration.between(this, endTime)
+        println("Total time taken: ${duration.toMillis()} ms")
+        println()
+    }
+
+    @Test
+    fun `using coroutines for parallel operations`() = runBlocking {
+        printHeader()
+        val startTime = Instant.now()
+
         val filteredPeople = people.map { person ->
             async {
                 launch {
                     person.isAdult = person.age >= 18
-                    println(
-                        "%-30s %-40s %s".format(
-                            dateFormat.format(System.currentTimeMillis()), Thread.currentThread().name, person
-                        )
-                    )
+                    person.printFormattedInfo()
                 }
                 person
             }
         }.awaitAll().filter { it.age > 15 }.sortedBy { it.age }
 
-        val endTime = System.currentTimeMillis()
-        val duration = endTime - startTime
-        println("Total time taken: $duration ms")
-        println()
+        startTime.printFooter()
 
-        assertResults(filteredPeople)
+        filteredPeople.assertResultsTrue()
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun `using coroutines for parallel operations with Flow`() = runBlocking {
-        println(
-            "%-30s %-40s %s".format(
-                "Time", "Thread name", "Operation"
-            )
-        )
-        val startTime = System.currentTimeMillis()
+        printHeader()
+        val startTime = Instant.now()
+
         val filteredPeople = people.asFlow().flatMapMerge { person ->
             flow {
                 emit(async {
                     person.isAdult = person.age >= 18
-
-                    println(
-                        "%-30s %-40s %s".format(
-                            dateFormat.format(System.currentTimeMillis()), Thread.currentThread().name, person
-                        )
-                    )
+                    person.printFormattedInfo()
                     person
                 }.await())
             }
         }.filter { it.age > 15 }.toList().sortedBy { it.age }
 
-        val endTime = System.currentTimeMillis()
-        val duration = endTime - startTime
-        println("Total time taken: $duration ms")
-        println()
+        startTime.printFooter()
 
-        assertResults(filteredPeople)
+        filteredPeople.assertResultsTrue()
     }
 
     @Test
     fun `using RxJava for parallel operations`() { // Observable.class from io.reactivex;
-        println(
-            "%-30s %-40s %s".format(
-                "Time", "Thread name", "Operation"
-            )
-        )
-        val startTime = System.currentTimeMillis()
+        printHeader()
+        val startTime = Instant.now()
+
         val observable = Observable.fromIterable(people).flatMap({
             Observable.just(it).subscribeOn(Schedulers.computation()).doOnNext { person ->
                 person.isAdult = person.age >= 18
-                println(
-                    "%-30s %-40s %s".format(
-                        dateFormat.format(System.currentTimeMillis()), Thread.currentThread().name, person
-                    )
-                )
+                person.printFormattedInfo()
             }
         }, people.size) // Uses maxConcurrency for the number of elements
             .filter { it.age > 15 }.toList().map { it.sortedBy { person -> person.age } }.blockingGet()
 
-        val endTime = System.currentTimeMillis()
-        val duration = endTime - startTime
-        println("Total time taken: $duration ms")
-        println()
+        startTime.printFooter()
 
-        assertResults(observable)
+        observable.assertResultsTrue()
     }
 
     @Test
     fun `using RxKotlin for parallel operations`() { // ObservableKt.kt.class from io.reactivex.rxkotlin
-        println(
-            "%-30s %-40s %s".format(
-                "Time", "Thread name", "Operation"
-            )
-        )
-        val startTime = System.currentTimeMillis()
+        printHeader()
+        val startTime = Instant.now()
+
         val observable = people.toObservable().flatMap({
             Observable.just(it).subscribeOn(Schedulers.computation()).doOnNext { person ->
                 person.isAdult = person.age >= 18
-                println(
-                    "%-30s %-40s %s".format(
-                        dateFormat.format(System.currentTimeMillis()), Thread.currentThread().name, person
-                    )
-                )
+                person.printFormattedInfo()
             }
         }, people.size) // Uses maxConcurrency for the number of elements
             .filter { it.age > 15 }.toList().map { it.sortedBy { person -> person.age } }.blockingGet()
 
-        val endTime = System.currentTimeMillis()
-        val duration = endTime - startTime
-        println("Total time taken: $duration ms")
-        println()
+        startTime.printFooter()
 
-        assertResults(observable)
+        observable.assertResultsTrue()
     }
 
     @Test
     fun `using RxKotlin but still use 1 thread`() { // ObservableKt.kt.class from io.reactivex.rxkotlin
-        println(
-            "%-30s %-40s %s".format(
-                "Time", "Thread name", "Operation"
-            )
-        )
-        val startTime = System.currentTimeMillis()
+        printHeader()
+        val startTime = Instant.now()
+
         val observable =
             people.toObservable().subscribeOn(Schedulers.io()).flatMap { Observable.just(it) }.doOnNext { person ->
                 person.isAdult = person.age >= 18
-                println(
-                    "%-30s %-40s %s".format(
-                        dateFormat.format(System.currentTimeMillis()), Thread.currentThread().name, person
-                    )
-                )
+                person.printFormattedInfo()
             }.filter { it.age > 15 }.toList().map { it.sortedBy { person -> person.age } }.blockingGet()
 
-        val endTime = System.currentTimeMillis()
-        val duration = endTime - startTime
-        println("Total time taken: $duration ms")
-        println()
+        startTime.printFooter()
 
-        assertResults(observable)
+        observable.assertResultsTrue()
     }
 
     @Test
     fun `using parallelStream()`() {
-        println(
-            "%-30s %-40s %s".format(
-                "Time", "Thread name", "Operation"
-            )
-        )
-        val startTime = System.currentTimeMillis()
+        printHeader()
+        val startTime = Instant.now()
+
         val filteredPeople = people.parallelStream().map { person ->
             person.isAdult = person.age >= 18
-            println(
-                "%-30s %-40s %s".format(
-                    dateFormat.format(System.currentTimeMillis()), Thread.currentThread().name, person
-                )
-            )
+            person.printFormattedInfo()
             person
         }.filter { it.age > 15 }.sorted { p1, p2 -> p1.age.compareTo(p2.age) }.collect(Collectors.toList())
 
-        val endTime = System.currentTimeMillis()
-        val duration = endTime - startTime
-        println("Total time taken: $duration ms")
-        println()
+        startTime.printFooter()
 
-        assertResults(filteredPeople)
+        filteredPeople.assertResultsTrue()
     }
 
     @Test
     fun `using ScheduledExecutorService for parallel operations`() {
-        println(
-            "%-30s %-40s %s".format(
-                "Time", "Thread name", "Operation"
-            )
-        )
-        val startTime = System.currentTimeMillis()
+        printHeader()
+        val startTime = Instant.now()
+
         val executor = Executors.newFixedThreadPool(people.size)
         val futures = people.map { person ->
             executor.submit(Callable {
                 person.isAdult = person.age >= 18
-                println(
-                    "%-30s %-40s %s".format(
-                        dateFormat.format(System.currentTimeMillis()), Thread.currentThread().name, person
-                    )
-                )
+                person.printFormattedInfo()
                 person
             })
         }.map { it.get() }.filter { it.age > 15 }.sortedBy { it.age }
 
-        val endTime = System.currentTimeMillis()
-        val duration = endTime - startTime
-        println("Total time taken: $duration ms")
-        println()
-
-        assertResults(futures)
-
         executor.shutdown()
+
+        startTime.printFooter()
+
+        futures.assertResultsTrue()
     }
 }
