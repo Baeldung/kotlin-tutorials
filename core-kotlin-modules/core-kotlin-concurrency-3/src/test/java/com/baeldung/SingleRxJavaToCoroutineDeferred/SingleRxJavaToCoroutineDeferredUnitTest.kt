e7574ae54bc45fff2efb04f2359fa4cf7f1817d5
@hangga <bazeniancode@gmail.com>
2024-05-10 22:28:22 +0700

    update

--- core-kotlin-modules/core-kotlin-concurrency-3/src/test/java/com/baeldung/SingleRxJavaToCoroutineDeferred/SingleRxJavaToCoroutineDeferredUnitTest.kt
+++ core-kotlin-modules/core-kotlin-concurrency-3/src/test/java/com/baeldung/SingleRxJavaToCoroutineDeferred/SingleRxJavaToCoroutineDeferredUnitTest.kt
@@ -3,14 +3,20 @@ package com.baeldung.SingleRxJavaToCoroutineDeferred
 import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
 import kotlinx.coroutines.*
-import kotlinx.coroutines.rx2.await
-import java.util.*
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+import kotlin.test.assertContains
 
+@OptIn(DelicateCoroutinesApi::class)
 class SingleRxJavaToCoroutineDeferredUnitTest {
 
     data class Product(val id: Int, val name: String, val price: Double)
 
-    fun getProducts(): Single<List<Product>> {
+    private fun getProducts(): Single<List<Product>> {
         return Single.just(
             listOf(
                 Product(1, "Samsung", 1200.0),
@@ -21,112 +27,98 @@ class SingleRxJavaToCoroutineDeferredUnitTest {
         ).subscribeOn(Schedulers.io())
     }
 
-    // Fungsi sederhana yang hanya melakukan pemetaan tanpa perubahan pada data
-    fun getProductsDeferred1(): Single<List<Product>>? {
-        return getProducts().map { it }
+    // using async
+    private fun <T> Single<T>.toDeferredAsync(): Deferred<T> = runBlocking {
+        return@runBlocking async { this@toDeferredAsync.blockingGet() }
     }
 
-    // Fungsi yang menggunakan flatMap untuk melakukan pemetaan dengan perubahan data pada produk
-    fun getProductsDeferred2(): Single<List<Product>>? {
-        return getProducts().flatMap { products ->
-            Single.fromCallable {
-                products
-            }
+    @Test
+    fun `test using async`(): Unit = runBlocking {
+        val deferred = getProducts().toDeferredAsync().await()
+        deferred.forEach { product ->
+            assertThat(deferred).contains(product)
         }
+        assertThat(deferred).containsExactly(
+            Product(1, "Samsung", 1200.0),
+            Product(2, "Oppo", 800.0),
+            Product(3, "Nokia", 400.0),
+            Product(4, "Lenovo", 400.0)
+        )
     }
 
-    // Fungsi yang menggunakan flatMap untuk melakukan pemetaan dengan perubahan data pada produk
-    fun getProductsDeferred3(): Single<List<Product>>? {
-        return getProducts().flatMap { products ->
-            Single.fromCallable {
-                products.map { product ->
-                    Product(product.id, product.name.lowercase(Locale.getDefault()), product.price * 0.9)
-                }
-            }
+    // using GlobalScope.async
+    private fun <T> Single<T>.toDeferred(): Deferred<T> = GlobalScope.async { this@toDeferred.blockingGet() }
+
+    @Test
+    fun `test using GlobalScope async`(): Unit = runBlocking {
+        val deferred = getProducts().toDeferred().await()
+        deferred.forEach { product ->
+            assertThat(deferred).contains(product)
         }
+        assertThat(deferred).containsExactly(
+            Product(1, "Samsung", 1200.0),
+            Product(2, "Oppo", 800.0),
+            Product(3, "Nokia", 400.0),
+            Product(4, "Lenovo", 400.0)
+        )
     }
 
-    // Fungsi yang menggunakan map untuk melakukan pemetaan tanpa perubahan pada data
-    fun getProductsDeferred4(): Single<List<Product>>? {
-        return getProducts().map { it }
-    }
+    // using CoroutineScope(context).async
+    private fun <T> Single<T>.toDeferred(context: CoroutineContext): Deferred<T> =
+        CoroutineScope(context).async { this@toDeferred.blockingGet() }
 
-    // Fungsi yang menggunakan CoroutineScope.async untuk membuat Deferred dengan CoroutineStart.LAZY
-    fun getProductsDeferred5(): Deferred<List<Product>> = CoroutineScope(Dispatchers.IO).async(
-        start = CoroutineStart.LAZY
-    ) {
-        getProducts().await()
-    }
-
-    // Fungsi yang menggunakan CoroutineScope.async untuk membuat Deferred dengan penggunaan supervisorScope
-    suspend fun getProductsAsync1(): List<Product> {
-        return supervisorScope {
-            async {
-                getProducts().await()
-            }.await()
+    @Test
+    fun `test using CoroutineScope with context and async`(): Unit = runBlocking {
+        val deferred = getProducts().toDeferred(Dispatchers.IO).await()
+        deferred.forEach { product ->
+            assertThat(deferred).contains(product)
         }
+        assertThat(deferred).containsExactly(
+            Product(1, "Samsung", 1200.0),
+            Product(2, "Oppo", 800.0),
+            Product(3, "Nokia", 400.0),
+            Product(4, "Lenovo", 400.0)
+        )
     }
 
-    // Fungsi yang menggunakan supervisorScope untuk menghandle failure secara terpisah
-    suspend fun getProductsAsync2(): List<Product> {
-        return supervisorScope {
-            async {
-                getProducts().await()
-            }
-        }.await()
+    // using CompletableDeferred
+    private fun <T> Single<T>.toCompletableDeferred(): CompletableDeferred<T> {
+        val completableDeferred = CompletableDeferred<T>()
+        this.subscribe({ completableDeferred.complete(it) }, { completableDeferred.completeExceptionally(it) })
+        return completableDeferred
     }
 
-    // Fungsi yang menggunakan CoroutineScope.async dan runCatching untuk menghandle exception
-    suspend fun getProductsAsync3(): List<Product> {
-        return runCatching {
-            coroutineScope {
-                async {
-                    getProducts().await()
-                }
-            }.await()
-        }.getOrElse {
-            emptyList()
+    @Test
+    fun `test using CompletableDeferred`(): Unit = runBlocking {
+        val deferred = getProducts().toCompletableDeferred().await()
+        deferred.forEach { product ->
+            assertThat(deferred).contains(product)
         }
+        assertThat(deferred).containsExactly(
+            Product(1, "Samsung", 1200.0),
+            Product(2, "Oppo", 800.0),
+            Product(3, "Nokia", 400.0),
+            Product(4, "Lenovo", 400.0)
+        )
     }
 
-    // Fungsi yang menggunakan GlobalScope.async untuk membuat Deferred
-    @OptIn(DelicateCoroutinesApi::class)
-    fun getProductsDeferred6(): Deferred<List<Product>> = GlobalScope.async(Dispatchers.IO) {
-        getProducts().await()
+    // using suspend
+    private suspend fun <T> Single<T>.toDeferredWithSuspend(): T = suspendCoroutine { continuation ->
+        this.subscribe({ continuation.resume(it) }, { continuation.resumeWithException(it) })
     }
 
-    // Fungsi yang menggunakan subscribeOn untuk menjalankan pemanggilan asinkron
-    fun Single<List<Product>>.toDeferred1(): Deferred<List<Product>> {
-        val deferred = CompletableDeferred<List<Product>>()
-
-        this.subscribeOn(Schedulers.io()).subscribe({ result ->
-            deferred.complete(result)
-        }, { error ->
-            deferred.completeExceptionally(error)
-        })
-
-        return deferred
-    }
-
-    // Fungsi yang menggunakan GlobalScope.async untuk membuat Deferred dengan pemanggilan asinkron
-    @OptIn(DelicateCoroutinesApi::class)
-    fun Single<List<Product>>.toDeferred2(): Deferred<List<Product>> = GlobalScope.async {
-        return@async this@toDeferred2.observeOn(Schedulers.io()).await()
-    }
-
-    // Fungsi yang menggunakan CoroutineScope.async untuk membuat Deferred dengan pemanggilan asinkron
-    fun getProductsDeferred7(): Deferred<Any> = CoroutineScope(Dispatchers.IO).async {
-        runCatching {
-            getProducts().await()
-        }.getOrElse {
-            Result.failure<Any>(Throwable())
+    @Test
+    fun `test using suspend`(): Unit = runBlocking {
+        val deferred = getProducts().toDeferredWithSuspend()
+        deferred.forEach { product ->
+            assertThat(deferred).contains(product)
         }
+        assertThat(deferred).containsExactly(
+            Product(1, "Samsung", 1200.0),
+            Product(2, "Oppo", 800.0),
+            Product(3, "Nokia", 400.0),
+            Product(4, "Lenovo", 400.0)
+        )
     }
 
-    // Fungsi yang menggunakan CoroutineScope.async untuk membuat Deferred dengan pemanggilan asinkron
-    suspend fun getProductsAsync4(): List<Product> {
-        return CoroutineScope(Dispatchers.IO).async {
-            getProducts().await()
-        }.await()
-    }
 }
