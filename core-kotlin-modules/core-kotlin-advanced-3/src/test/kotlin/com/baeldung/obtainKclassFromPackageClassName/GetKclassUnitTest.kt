9e77dddfc95e340078620713b8ab56790a7c675f
nappy29 <ewaneenombe@gmail.com>
2024-05-21 09:25:41 +0100

    added unit tests methods

--- core-kotlin-modules/core-kotlin-advanced-3/src/test/kotlin/com/baeldung/obtainKclassFromPackageClassName/GetKclassUnitTest.kt
+++ core-kotlin-modules/core-kotlin-advanced-3/src/test/kotlin/com/baeldung/obtainKclassFromPackageClassName/GetKclassUnitTest.kt
@@ -2,63 +2,89 @@ package com.baeldung.obtainKclassFromPackageClassName
 
 import io.github.classgraph.ClassGraph
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import java.io.IOException
 import kotlin.reflect.KClass
 import kotlin.test.assertEquals
+import kotlin.test.assertNull
 
 class GetKclassUnitTest {
 
     @Test
     fun `obtain the Kclass from Package Class name using forName method`() {
         val className = "com.baeldung.obtainKclassFromPackageClassName.ClassExample"
+
         val kClass = getClassUsingForName(className)
 
         assertEquals(ClassExample::class, kClass)
     }
 
     @Test
+    fun `obtain the Kclass from Package Class name using forName method and non-existing class`() {
+        val notClass = "com.baeldung.obtainKclassFromPackageClassName.NotAClass"
+
+        val exception = assertThrows<ClassNotFoundException> {
+            getClassUsingForName(notClass)
+        }
+
+        assertEquals("com.baeldung.obtainKclassFromPackageClassName.NotAClass", exception.message)
+
+    }
+
+    @Test
     fun `obtain the Kclass from Package Class name using class loader`() {
         val className = "com.baeldung.obtainKclassFromPackageClassName.ClassExample"
+
         val kClass = getClassFromLoader(className)
 
         assertEquals(ClassExample::class, kClass)
     }
 
     @Test
+    fun `obtain the Kclass from Package Class name using class loader and non-existing class`() {
+        val notClass = "com.baeldung.obtainKclassFromPackageClassName.NotAClass"
+
+        val exception = assertThrows<ClassNotFoundException> {
+            getClassFromLoader(notClass)
+        }
+
+        assertEquals("com.baeldung.obtainKclassFromPackageClassName.NotAClass", exception.message)
+    }
+
+    @Test
+    fun `obtain the Kclass from Package Class name using class path and non-existing class`() {
+        val notClass = "com.baeldung.obtainKclassFromPackageClassName.NotAClass"
+
+        val exception = assertThrows<NullPointerException> {
+            getClassUsingClassGraph(notClass)
+        }
+
+        assertEquals("Cannot invoke \"io.github.classgraph.ClassInfo.loadClass()\" because the return value of \"io.github.classgraph.ScanResult.getClassInfo(String)\" is null", exception.message)
+    }
+
+    @Test
     fun `obtain the Kclass from Package Class name using class path`() {
         val className = "com.baeldung.obtainKclassFromPackageClassName.ClassExample"
-        val kClass = getClassUsingClassPath(className)
+        val kClass = getClassUsingClassGraph(className)
 
         assertEquals(ClassExample::class, kClass)
     }
 }
 
 fun getClassUsingForName(className: String): KClass<*>? {
-    return try {
-        Class.forName(className).kotlin
-    } catch (e: ClassNotFoundException) {
-        null
-    }
+    return Class.forName(className).kotlin
 }
 
 fun getClassFromLoader(className: String): KClass<*>? {
-    return try {
-        ClassLoader.getSystemClassLoader().loadClass(className).kotlin
-    } catch (e: ClassNotFoundException) {
-        null
-    }
+    return ClassLoader.getSystemClassLoader().loadClass(className).kotlin
 }
 
-fun getClassUsingClassPath(className: String): KClass<*>? {
-    return try {
-        ClassGraph()
+fun getClassUsingClassGraph(className: String): KClass<*>? {
+        return ClassGraph()
             .addClassLoader(ClassLoader.getSystemClassLoader())
             .enableClassInfo()
             .scan()
             .getClassInfo(className)
             .loadClass()
             ?.kotlin
-    } catch (e: Exception) {
-        null
-    }
 }
