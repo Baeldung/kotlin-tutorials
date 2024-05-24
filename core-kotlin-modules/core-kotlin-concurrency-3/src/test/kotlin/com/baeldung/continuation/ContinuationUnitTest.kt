f7470c78a5d1637ef481801a50ffce2f50dfe651
Hangga Aji Sayekti <bazeniancode@gmail.com>
2024-05-24 09:58:54 +0700

    update sample

---
+++ core-kotlin-modules/core-kotlin-concurrency-3/src/test/kotlin/com/baeldung/continuation/ContinuationUnitTest.kt
@@ -0,0 +1,79 @@
+package com.baeldung.continuation
+
+import kotlinx.coroutines.*
+import org.junit.jupiter.api.Test
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+import kotlin.test.assertEquals
+
+class ContinuationUnitTest {
+
+    @Test
+    fun `test create simple continuation`() {
+        val simpleContinuation = Continuation<Int>(Dispatchers.IO) { result ->
+            assertEquals(45, result.getOrNull())
+        }
+        simpleContinuation.resume(45)
+    }
+
+    private suspend fun simpleSuspendFunction(): String {
+        return suspendCoroutine { continuation ->
+            continuation.resume("Baeldung")
+        }
+    }
+
+    @Test
+    fun `test using simple suspend function`() = runBlocking {
+        val deferred = async { simpleSuspendFunction() }.await()
+        assertEquals("Baeldung", deferred)
+    }
+
+    private suspend fun simulateNetworkRequest(url: String): String {
+        return suspendCoroutine { continuation ->
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    val urlObj = URL(url)
+                    val connection = urlObj.openConnection() as HttpURLConnection
+                    connection.apply {
+                        requestMethod = "GET"
+                        connectTimeout = 5000
+                        readTimeout = 5000
+                    }
+
+                    val responseCode = connection.responseCode
+                    if (responseCode == HttpURLConnection.HTTP_OK) {
+                        continuation.resume("Successful request to $url : HTTP response code $responseCode")
+                    } else {
+                        continuation.resumeWithException(Exception("Failed to fetch data from $url: HTTP response code $responseCode"))
+                    }
+                } catch (e: Exception) {
+                    continuation.resumeWithException(Exception("Failed to fetch data from $url: ${e.message}"))
+                }
+            }
+        }
+    }
+
+    @Test
+    fun `test network call`() = runBlocking {
+        val urls = listOf(
+            "https://example.com",
+            "https://example.com/fail",
+            "https://example.com/fail2",
+            "https://baeldung.com",
+            "https://hangga.github.io"
+        )
+
+        for (url in urls) {
+            try {
+                val response = simulateNetworkRequest(url)
+                println(response)
+            } catch (e: Exception) {
+                println("Error: ${e.message}")
+            }
+        }
+    }
+}
