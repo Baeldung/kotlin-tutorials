196525eca99da97f594a83d1c5f954ffb9363c79
Mikhail2048 <mikhailpolivakha@gmail.com>
2024-06-08 12:16:11 +0300

    K2 Compiler overview

---
+++ k2-compiler/src/main/kotlin/com/baeldung/kotlin/k2/Examples.kt
@@ -0,0 +1,19 @@
+fun translateExecution(exception: Throwable?): Throwable? {
+  val isInvocationTargetException = exception is InvocationTargetException
+
+  if (isInvocationTargetException) {
+    return (exception as InvocationTargetException).targetException
+  }
+
+  return exception
+}
+
+fun translateExecutionV2(exception: Throwable?) : Throwable? {
+  val isInvocationTargetException = exception is InvocationTargetException
+
+  if (isInvocationTargetException) {
+    return exception.targetException
+  }
+
+  return exception
+}
