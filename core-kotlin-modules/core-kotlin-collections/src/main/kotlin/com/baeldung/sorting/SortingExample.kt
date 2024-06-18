2f66492067c596007d665482f63d9a5eec717a8a
kent <kent.yuan@gmail.com>
2024-06-18 11:33:17 +0200

    [sorted-functions] use logger instead of println

--- core-kotlin-modules/core-kotlin-collections/src/main/kotlin/com/baeldung/sorting/SortingExample.kt
+++ core-kotlin-modules/core-kotlin-collections/src/main/kotlin/com/baeldung/sorting/SortingExample.kt
@@ -1,44 +1,48 @@
 package com.baeldung.sorting
 
+import org.slf4j.LoggerFactory
+
+val log = LoggerFactory.getLogger("SortingExample")
+
 fun sortMethodUsage() {
     val sortedValues = mutableListOf(1, 2, 7, 6, 5, 6)
     sortedValues.sort()
-    println(sortedValues)
+    log.info("$sortedValues")
 }
 
 fun sortByMethodUsage() {
     val sortedValues = mutableListOf(1 to "a", 2 to "b", 7 to "c", 6 to "d", 5 to "c", 6 to "e")
     sortedValues.sortBy { it.second }
-    println(sortedValues)
+    log.info("$sortedValues")
 }
 
 fun sortWithMethodUsage() {
     val sortedValues = mutableListOf(1 to "a", 2 to "b", 7 to "c", 6 to "d", 5 to "c", 6 to "e")
-    sortedValues.sortWith(compareBy({it.second}, {it.first}))
-    println(sortedValues)
+    sortedValues.sortWith(compareBy({ it.second }, { it.first }))
+    log.info("$sortedValues")
 }
 
-fun <T : kotlin.Comparable<T>> getSimpleComparator() : Comparator<T> {
+fun <T : kotlin.Comparable<T>> getSimpleComparator(): Comparator<T> {
     val ascComparator = naturalOrder<T>()
     return ascComparator
 }
 
 fun getComplexComparator() {
-    val complexComparator = compareBy<Pair<Int, String>>({it.first}, {it.second})
-    print("Complex comparator result: $complexComparator" )
+    val complexComparator = compareBy<Pair<Int, String>>({ it.first }, { it.second })
+    log.info("Complex comparator result: $complexComparator")
 }
 
 fun nullHandlingUsage() {
     val sortedValues = mutableListOf(1 to "a", 2 to null, 7 to "c", 6 to "d", 5 to "c", 6 to "e")
     sortedValues.sortWith(nullsLast(compareBy { it.second }))
-    println(sortedValues)
+    log.info("$sortedValues")
 }
 
 fun extendedComparatorUsage() {
     val students = mutableListOf(21 to "Helen", 21 to "Tom", 20 to "Jim")
 
-    val ageComparator = compareBy<Pair<Int, String?>> {it.first}
-    val ageAndNameComparator = ageComparator.thenByDescending {it.second}
+    val ageComparator = compareBy<Pair<Int, String?>> { it.first }
+    val ageAndNameComparator = ageComparator.thenByDescending { it.second }
     students.sortWith(ageAndNameComparator)
-    println(students)
+    log.info("$students")
 }
