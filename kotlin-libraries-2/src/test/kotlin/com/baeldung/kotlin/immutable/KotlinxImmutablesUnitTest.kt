996f73216663c44d7b4d4998046f10f17e47e759
Yadukrishnan <yadavan88@gmail.com>
2024-06-21 17:10:52 +0200

    Fixed compilation error, also added the test task for the new profile

--- kotlin-libraries-2/src/test/kotlin/com/baeldung/kotlin/immutable/KotlinxImmutablesUnitTest.kt
+++ kotlin-libraries-2/src/test/kotlin/com/baeldung/kotlin/immutable/KotlinxImmutablesUnitTest.kt
@@ -3,6 +3,7 @@ package com.baeldung.kotlin.immutable
 import org.junit.Assert.assertEquals
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.immutableListOf
+import kotlinx.collections.immutable.persistentListOf
 import org.junit.Test
 
 class KotlinxImmutablesUnitTest{
@@ -10,9 +11,10 @@ class KotlinxImmutablesUnitTest{
   @Test
   fun givenKICLList_whenAddTried_checkExceptionThrown(){
 
-      val list: ImmutableList<String> = immutableListOf("I", "am", "immutable")
+      val list: ImmutableList<String> = persistentListOf("I", "am", "immutable")
 
-      list.add("My new item")
+      //todo: This is not avalid anymore with latest version of kotlinx.immutableList
+      //list.add("My new item")
 
       assertEquals(listOf("I", "am", "immutable"), list)
   }
