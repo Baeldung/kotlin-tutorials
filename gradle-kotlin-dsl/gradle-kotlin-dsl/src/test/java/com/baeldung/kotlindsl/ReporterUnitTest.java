07f70a3492dfd9924bc4b1fd61dbfd13b96eb4dd
Yadukrishnan <yadavan88@gmail.com>
2024-06-22 00:29:24 +0200

    testing failure

--- gradle-kotlin-dsl/gradle-kotlin-dsl/src/test/java/com/baeldung/kotlindsl/ReporterUnitTest.java
+++ gradle-kotlin-dsl/gradle-kotlin-dsl/src/test/java/com/baeldung/kotlindsl/ReporterUnitTest.java
@@ -13,7 +13,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
-classwrong ReporterTest {
+class ReporterTest {
 
     Repository repository = mock(Repository.class);
     Sorter sorter = mock(Sorter.class);
@@ -40,8 +40,8 @@ classwrong ReporterTest {
 
         // then
         assertEquals(List.of(
-          stockPrice(new BigDecimal("1.57"), Instant.parse("2021-05-05T15:00:00Z")),
-          stockPrice(new BigDecimal("1.89"), Instant.parse("2021-02-03T15:00:00Z")),
+          stockPrice(new BigDecimal("1.57988"), Instant.parse("2021-05-05T15:00:00Z")),
+          stockPrice(new BigDecimal("1.0"), Instant.parse("2021-02-03T15:00:00Z")),
           stockPrice(new BigDecimal("1.32"), Instant.parse("2021-01-01T15:00:00Z"))),
           result
         );
