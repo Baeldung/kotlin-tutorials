8a16ee21e57bc4f0e2b3776305bddbf17f71e479
albertache1998 <albertache1998@gmail.com>
2024-07-01 14:49:15 +0100

    made code fixes

--- core-kotlin-modules/core-kotlin-strings-5/src/test/kotlin/com/baeldung/hexToByte/HexToByteUnitTest.kt
+++ core-kotlin-modules/core-kotlin-strings-5/src/test/kotlin/com/baeldung/hexToByte/HexToByteUnitTest.kt
@@ -22,23 +22,20 @@ class HexToByteUnitTest {
     }
 
     @Test
-    fun `convert hex string to byte array using BigInteger with sign bit handling`() {
-        val positiveHexString = "48656C6C6F20576F726C64"
-        val negativeHexString = "FFFFFFFF"
-
-        val expectedByteArrayPositive = byteArrayOf(72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100)
-        val expectedByteArrayNegative = byteArrayOf(-1, -1, -1, -1)
-
-        assertArrayEquals(expectedByteArrayPositive, hexStringToByteArrayUsingBigIntegerWithSignHandling(positiveHexString))
-        assertArrayEquals(expectedByteArrayNegative, hexStringToByteArrayUsingBigIntegerWithSignHandling(negativeHexString))
-    }
-
-    @Test
     fun `convert hex string to byte array using BigInteger using standard libraries methods`() {
         val hexString = "48656C6C6F20576F726C64"
         val expectedByteArray = byteArrayOf(72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100)
 
         assertArrayEquals(expectedByteArray, hexStringToByteArrayUsingStandardLibraryMethods(hexString))
+    }
+
+    @OptIn(ExperimentalStdlibApi::class)
+    @Test
+    fun `convert hex string to byte array using hexToByteArray`() {
+        val hexString = "48656C6C6F20576F726C64"
+        val expectedByteArray = byteArrayOf(72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100)
+
+        assertArrayEquals(expectedByteArray, hexString.hexToByteArray())
     }
 }
 fun hexStringToByteArrayUsingForLoop(hex: String): ByteArray {
@@ -56,14 +53,6 @@ fun hexStringToByteArrayUsingBigInteger(hexString: String): ByteArray {
     return bigInteger.toByteArray()
 }
 
-fun hexStringToByteArrayUsingBigIntegerWithSignHandling(hex: String): ByteArray {
-    var bigInt = BigInteger(hex, 16)
-    val byteArray = bigInt.toByteArray()
-    if (byteArray.size > (hex.length / 2) && byteArray[0] == 0.toByte()) {
-        return byteArray.copyOfRange(1, byteArray.size)
-    }
-    return byteArray
-}
 fun hexStringToByteArrayUsingStandardLibraryMethods(hex: String): ByteArray {
     return hex.chunked(2)
         .map { it.toInt(16).toByte() }
