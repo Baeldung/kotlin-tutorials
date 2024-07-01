f4fb838c91ecb13a1ce2753e0105a8c808798e6b
timis1 <noreplay@yahoo.com>
2024-07-01 07:33:39 +0300

    JAVA-36508 Upgrade Kotlin parent-boot-2 and parent-boot-3 to the latest version of Boot - Week 26 - 2024 (conti-1)

--- spring-boot-kotlin-2/src/main/kotlin/com/baeldung/configurationproperties/config/ApiConfiguration.kt
+++ spring-boot-kotlin-2/src/main/kotlin/com/baeldung/configurationproperties/config/ApiConfiguration.kt
@@ -1,10 +1,9 @@
 package com.baeldung.configurationproperties.config
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.ConstructorBinding
+import org.springframework.boot.context.properties.bind.ConstructorBinding
 
 @ConfigurationProperties(prefix = "api")
-@ConstructorBinding
-data class ApiConfiguration(
+data class ApiConfiguration /*@ConstructorBinding constructor*/(
     val clientId: String,
     val url: String,
     val key: String
