c6eb377e2a64aa1816afce3be803d129dd1de547
Hangga Aji Sayekti <bazeniancode@gmail.com>
2024-07-29 14:22:37 +0700

    skip

--- core-kotlin-modules/core-kotlin-lang-oop-4/src/test/kotlin/ExtensionFieldsUnitTest.kt
+++ core-kotlin-modules/core-kotlin-lang-oop-4/src/test/kotlin/ExtensionFieldsUnitTest.kt
@@ -1,2 +1,14 @@
+import org.junit.jupiter.api.Test
+
 class ExtensionFieldsUnitTest {
+
+//    val String.firstChar: Char
+//        get() = this[0]
+
+    @Test
+    fun `test extension properties`(){
+        // ExtensionProperties.kt
+        val word = "Hello"
+//        println(word.firstChar)
+    }
 }
