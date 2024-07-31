dac4c0e7643bf405832760364b643688ddc4f14b
Hangga Aji Sayekti <bazeniancode@gmail.com>
2024-07-31 14:48:25 +0700

    add sample

--- core-kotlin-modules/core-kotlin-lang-oop-4/src/test/kotlin/ExtensionFieldsUnitTest.kt
+++ core-kotlin-modules/core-kotlin-lang-oop-4/src/test/kotlin/ExtensionFieldsUnitTest.kt
@@ -1,12 +1,11 @@
 import org.junit.jupiter.api.Test
+import java.util.*
+import kotlin.reflect.KProperty
 import kotlin.test.assertEquals
 
 class ExtensionFieldsUnitTest {
 
-    // extension function
     fun String.toTitleCase(): String = this.split(" ").joinToString(" ") { it.capitalize() }
-
-    // using extension properties
 
     // val String.firstChar: Char = this[0] // this not allowed
 
@@ -17,7 +16,7 @@ class ExtensionFieldsUnitTest {
     fun `test using extension function and properties`() {
         val word = "baeldung kotlin"
 
-        assertEquals("b", word.firstChar.toString())
+        assertEquals(word[0], word.firstChar)
 
         assertEquals("Baeldung Kotlin", word.toTitleCase())
     }
@@ -26,12 +25,12 @@ class ExtensionFieldsUnitTest {
         var name: String = "Foo"
     }
 
-    fun Foo.getName() : String = this.name
+    fun Foo.getName(): String = this.name
 
-    fun Foo.getLength() : Int = this.name.length
+    fun Foo.getLength(): Int = this.name.length
 
     @Test
-    fun `test using extension function`(){
+    fun `test using extension function`() {
         val foo = Foo()
 
         assertEquals("Foo", foo.getName())
@@ -39,30 +38,25 @@ class ExtensionFieldsUnitTest {
     }
 
     val Foo.someProperty: Int
-        get() =  21
+        get() = 21
 
     @Test
-    fun `test extension properties using getter`(){
+    fun `test extension properties using getter`() {
         val foo = Foo()
         assertEquals(21, foo.someProperty)
 
         // foo.someProperty = 22 // not allowed
-        assertEquals(21, foo.someProperty)
     }
 
     val propertyStorage = mutableMapOf<Foo, Any>()
 
     var Foo.someIntProperty: Any
         get() = propertyStorage[this] ?: 0
-        set(value) {
-            propertyStorage[this] = value
-        }
+        set(value) { propertyStorage[this] = value }
 
     var Foo.someStringProperty: Any
         get() = propertyStorage[this] ?: ""
-        set(value) {
-            propertyStorage[this] = value
-        }
+        set(value) { propertyStorage[this] = value }
 
     @Test
     fun `test using custom backing field`() {
@@ -79,5 +73,40 @@ class ExtensionFieldsUnitTest {
 
         foo.someStringProperty = "Kotlin"
         assertEquals("Kotlin", foo.someStringProperty)
+    }
+
+    class FieldProperty<R, T>(
+        private val initializer: (R) -> T
+    ) {
+        private val map = WeakHashMap<R, T>()
+
+        operator fun getValue(thisRef: R, property: KProperty<*>): T {
+            return map[thisRef] ?: setValue(thisRef, property, initializer(thisRef))
+        }
+
+        operator fun setValue(thisRef: R, property: KProperty<*>, value: T): T {
+            map[thisRef] = value
+            return value
+        }
+    }
+
+    var Int.customField: String by FieldProperty { "$it" }
+    var String.customField: Int by FieldProperty { it.length }
+    var Double.customField: Boolean by FieldProperty { it > 0.0 }
+
+    @Test
+    fun `test using field property`() {
+        val x = 0
+        println(x.customField)  // Output: "0"
+        assertEquals("0", x.customField)
+
+        x.customField = "Baeldung"
+        assertEquals("Baeldung", x.customField)
+
+        val str = "Hello"
+        assertEquals(5, str.customField)
+
+        val d = 3.14
+        assertEquals(true, d.customField)
     }
 }
