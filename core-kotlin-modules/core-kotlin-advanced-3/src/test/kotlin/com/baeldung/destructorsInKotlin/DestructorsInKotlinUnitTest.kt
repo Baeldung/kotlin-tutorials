f1ef6a05860363cd502755ab27fc48ddd9b86e17
nappy29 <ewaneenombe@gmail.com>
2024-08-13 15:51:56 +0100

    added unit tests

---
+++ core-kotlin-modules/core-kotlin-advanced-3/src/test/kotlin/com/baeldung/destructorsInKotlin/DestructorsInKotlinUnitTest.kt
@@ -0,0 +1,61 @@
+package com.baeldung.destructorsInKotlin
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito.*
+import java.io.BufferedReader
+import java.io.IOException
+
+
+class DestructorsInKotlinUnitTest {
+    @Test
+    @Throws(IOException::class)
+    fun `perform resource cleaning with try-finally block`() {
+        val mockReader = mock(BufferedReader::class.java)
+        `when`(mockReader.readLine()).thenReturn("Hello, Kotlin!", null) // Mock readLine to return a string, then null to signify EOF.
+
+        val content = readFile(mockReader)
+        assertEquals("Hello, Kotlin!", content)
+
+        verify(mockReader).close()
+    }
+
+    @Test
+    @Throws(IOException::class)
+    fun `perform resource cleaning with use`() {
+        val mockReader = mock(BufferedReader::class.java)
+        `when`(mockReader.readLine()).thenReturn("Hello, Kotlin!", null) // Mock readLine to return a string, then null to signify EOF.
+
+        val content = readFileUsingUse(mockReader)
+        assertEquals("Hello, Kotlin!", content)
+
+        verify(mockReader).close()
+    }
+}
+
+@Throws(IOException::class)
+fun readFile(reader: BufferedReader): String {
+    try {
+        val content = StringBuilder()
+        var line: String?
+        while ((reader.readLine().also { line = it }) != null) {
+            content.append(line)
+        }
+        return content.toString()
+    } finally {
+        reader.close()
+    }
+}
+
+@Throws(IOException::class)
+fun readFileUsingUse(reader: BufferedReader): String {
+    return reader.use { br ->
+        val content = StringBuilder()
+        var line: String?
+        while ((br.readLine().also { line = it }) != null) {
+            content.append(line)
+        }
+        content.toString()
+    }
+}
+
