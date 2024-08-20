678a4a3e1c0994f2ef811f383600a84963a7be23
Graham Cox <graham@grahamcox.co.uk>
2024-08-20 07:59:21 +0100

    Renamed Okio tests to follow correct structure

--- core-kotlin-modules/core-kotlin-io/src/test/kotlin/com/baeldung/okio/FileSystemUnitTest.kt
+++ core-kotlin-modules/core-kotlin-io/src/test/kotlin/com/baeldung/okio/FileSystemUnitTest.kt
@@ -9,13 +9,13 @@ import org.junit.jupiter.api.Test
 
 class FileSystemUnitTest {
     @Test
-    fun test() {
+    fun `given a filesystem when we list files then we find READMEmd` () {
         val files = FileSystem.SYSTEM.list(".".toPath())
         assertTrue(files.contains("README.md".toPath()))
     }
 
     @Test
-    fun readFile() {
+    fun `given a filesystem when we read READMEmd then we get the correct first line` () {
         val line = FileSystem.SYSTEM.source("README.md".toPath()).buffer().readUtf8()
         assertEquals("## Core Kotlin I/O", line.lines().first())
     }
