4ac5596b448f079767c2c1458812d498227c7a07
kent <kent.yuan@gmail.com>
2024-08-22 15:52:28 +0200

    [void-unit-mismatch] void vs unit

---
+++ core-kotlin-modules/core-kotlin-lang-oop-4/src/test/kotlin/unitAndVoidMismatch/UnitAndVoidTypeMismatchUnitTest.kt
@@ -0,0 +1,29 @@
+package unitAndVoidMismatch
+
+import org.junit.jupiter.api.Test
+import org.slf4j.LoggerFactory
+
+fun customGreetingVoid(name: String, greeting: (String) -> Void) = greeting(name)
+
+fun customGreetingUnit(name: String, greeting: (String) -> Unit) = greeting(name)
+
+class UnitAndVoidTypeMismatchUnitTest {
+    val log = LoggerFactory.getLogger(this::class.java)
+
+    @Test
+    fun `when calling customGreetingVoid method, then code does not compile`() {
+        /* the below code doesn't compile
+         ---------------------------------
+        customGreetingVoid("Tom Hanks"){
+            log.info("Hi $it, how do you do? Do you know Forrest Gump?")
+        }
+        */
+    }
+
+    @Test
+    fun `when calling customGreetingUnit method, then works as expected`() {
+        customGreetingUnit("Tom Cruise") {
+            log.info("Hi $it, how are you doing? What kind of missions are impossible?")
+        }
+    }
+}
