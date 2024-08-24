6fba929b6bb7edd344cf8db85247c9634218d324
NAOUSSI Martial <kuitcheiselin@gmail.com>
2024-08-24 04:32:40 +0100

    [KTLN-491] Destructor in Kotlin Programming Language (#1057)
    * added unit tests
    
    * added unit tests
    
    * added unit tests
    
    ---------
    
    Co-authored-by: nappy29 <ewaneenombe@gmail.com>

---
+++ core-kotlin-modules/core-kotlin-advanced-3/src/test/kotlin/com/baeldung/destructorsInKotlin/DestructorsInKotlinUnitTest.kt
@@ -0,0 +1,85 @@
+package com.baeldung.destructorsInKotlin
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito.*
+import java.io.BufferedReader
+import java.io.IOException
+import kotlin.test.assertFailsWith
+
+class DestructorsInKotlinUnitTest {
+    @Test
+    @Throws(IOException::class)
+    fun `perform resource cleaning with try-finally block`() {
+        val mockReader = mock(BufferedReader::class.java)
+        `when`(mockReader.readLine()).thenReturn("Hello, Kotlin!", null)
+
+        val content = readFile(mockReader)
+        assertEquals("Hello, Kotlin!", content)
+
+        verify(mockReader).close()
+    }
+
+    @Test
+    @Throws(IOException::class)
+    fun `perform resource cleaning with try-finally block when IOException occurs`() {
+        val mockReader = mock(BufferedReader::class.java)
+        `when`(mockReader.readLine()).thenThrow(IOException("Test exception"))
+
+        assertFailsWith<IOException> {
+            readFile(mockReader)
+        }
+
+        verify(mockReader).close()
+    }
+
+    @Test
+    @Throws(IOException::class)
+    fun `perform resource cleaning with use`() {
+        val mockReader = mock(BufferedReader::class.java)
+        `when`(mockReader.readLine()).thenReturn("Hello, Kotlin!", null)
+        val content = readFileUsingUse(mockReader)
+        assertEquals("Hello, Kotlin!", content)
+
+        verify(mockReader).close()
+    }
+
+    @Test
+    @Throws(IOException::class)
+    fun `perform resource cleaning with use() when IOException occurs`() {
+        val mockReader = mock(BufferedReader::class.java)
+        `when`(mockReader.readLine()).thenThrow(IOException("Test exception"))
+
+        assertFailsWith<IOException> {
+            readFileUsingUse(mockReader)
+        }
+
+        verify(mockReader).close()
+    }
+}
+
+@Throws(IOException::class)
+fun readFile(reader: BufferedReader): String {
+    try {
+        val content = StringBuilder()
+        var line: String?
+        while ((reader.readLine().also { line = it }) != null) {
+            content.append(line)
+        }
+        return content.toString()
+    } finally {
+        reader.close()
+    }
+}
+
+@Throws(IOException::class)
+fun readFileUsingUse(reader: BufferedReader): String {
+    return reader.use { br ->
+        val content = StringBuilder()
+        var line: String?
+        while ((br.readLine().also { line = it }) != null) {
+            content.append(line)
+        }
+        content.toString()
+    }
+}
