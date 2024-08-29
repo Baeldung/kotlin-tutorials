59aa383abfc401d754d7b44eb8182c188cd0dfa0
Mikhail2048 <mikhailpolivakha@gmail.com>
2024-08-29 18:56:21 +0300

    KTLN-587 added webmvc module

---
+++ kotlin-dsl/src/main/kotlin/com/baeldung/functional_dsl/bean_declaration/BeanDeclration.kt
@@ -0,0 +1,28 @@
+package com.baeldung.functional_dsl.bean_declration
+
+import java.math.BigDecimal
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.support.BeanDefinitionDsl
+import org.springframework.context.support.beans
+
+@SpringBootApplication
+open class Application
+
+fun main(vararg args: String) {
+  runApplication<Application>(*args) {
+    addInitializers(
+      beans {
+        bean(
+          name = "functionallyDeclaredBean",
+          scope = BeanDefinitionDsl.Scope.SINGLETON,
+          isLazyInit = false,
+          isPrimary = false,
+          function = {
+            BigDecimal(1.0)
+          }
+        )
+      }
+    )
+  }
+}
---
+++ kotlin-dsl/src/main/kotlin/com/baeldung/functional_dsl/router_function/RouterFunction.kt
@@ -0,0 +1,32 @@
+package com.baeldung.functional_dsl.router_function
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.servlet.function.RouterFunction
+import org.springframework.web.servlet.function.RouterFunctions
+import org.springframework.web.servlet.function.ServerRequest
+import org.springframework.web.servlet.function.ServerResponse
+
+@SpringBootApplication
+open class Application {
+
+  @Bean
+  open fun configure() : RouterFunction<ServerResponse> {
+    return RouterFunctions.route()
+      .GET("/endpoint/{country}") {
+        ServerResponse.ok().body(
+          mapOf(
+            "name" to it.param("name"),
+            "age" to it.headers().header("X-age")[0],
+            "country" to it.pathVariable("country")
+          )
+        )
+      }
+      .build()
+  }
+}
+
+fun main(vararg args: String) {
+  runApplication<Application>(*args)
+}
