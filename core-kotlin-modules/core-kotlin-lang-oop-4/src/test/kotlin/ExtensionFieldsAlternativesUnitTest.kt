73ae35cf49900f920f55619a979b9bda0b2cc32d
Hangga Aji Sayekti <bazeniancode@gmail.com>
2024-09-09 14:56:46 +0700

    add function test

--- core-kotlin-modules/core-kotlin-lang-oop-4/src/test/kotlin/ExtensionFieldsAlternativesUnitTest.kt
+++ core-kotlin-modules/core-kotlin-lang-oop-4/src/test/kotlin/ExtensionFieldsAlternativesUnitTest.kt
@@ -10,16 +10,36 @@ class ExtensionFieldsAlternativesUnitTest {
     private val String.firstChar: Char
         get() = this.first()
 
-    class Person(var name: String, var age: Int)
+    @Test
+    fun `test using extension functions`() {
+        val word = "baeldung kotlin"
+        assertEquals("Baeldung Kotlin", word.toTitleCase())
+        assertEquals('b', word.firstChar)
+    }
+
+    open class Person(var name: String, var age: Int)
+
+    val Person.isAdult: Boolean
+        get() = this.age >= 18
 
     val Person.details: String
-        get() = "Name: ${this.name}, Age: ${this.age}"
+        get() = "Name: ${this.name}, Age: ${this.age}, isAdult: ${this.isAdult}"
     
     var Person.address: String
         get() = "Address is not set"
         set(value) {
             println("Cannot store the address directly: $value")
         }
+
+    @Test
+    fun `test using properties`() {
+        val person = Person("Hangga Aji Sayekti", 35)
+
+        assertEquals("Name: Hangga Aji Sayekti, Age: 35, isAdult: true", person.details)
+
+        person.address = "Jln. Kemasan Kotagede"
+        assertEquals("Address is not set", person.address)
+    }
 
     interface PersonDecorator {
         var address: String
@@ -42,16 +62,6 @@ class ExtensionFieldsAlternativesUnitTest {
     }
 
     @Test
-    fun `test using properties`() {
-        val person = Person("Hangga Aji Sayekti", 35)
-
-        assertEquals("Name: Hangga Aji Sayekti, Age: 35", person.details)
-
-        person.address = "Jln. Kemasan Kotagede"
-        assertEquals("Address is not set", person.address)
-    }
-
-    @Test
     fun `test using decorator`() {
         val person = Person("Hangga Aji Sayekti", 35)
 
@@ -62,5 +72,22 @@ class ExtensionFieldsAlternativesUnitTest {
 
         personWithAddress.address = "Jln. Kalasan Sleman"
         assertEquals("Name: Hangga Aji Sayekti, Age: 35, Address: Jln. Kalasan Sleman", personWithAddress.getDetails())
+    }
+
+    class PersonExtended(name: String, age: Int) : Person(name, age) {
+        var jobtitle: String = ""
+    }
+
+    @Test
+    fun `test using inheritance`(){
+        val personExtended = PersonExtended("Hangga Aji Sayekti", 35)
+        personExtended.jobtitle = "Software Engineer"
+        assertEquals("Software Engineer", personExtended.jobtitle)
+
+        personExtended.jobtitle = "Data Scientist"
+        assertEquals("Data Scientist", personExtended.jobtitle)
+
+        personExtended.jobtitle = "Mathematicians"
+        assertEquals("Mathematicians", personExtended.jobtitle)
     }
 }
