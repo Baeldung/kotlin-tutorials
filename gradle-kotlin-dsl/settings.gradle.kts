9a7d1b1c7303a91a9d6788b6dcf16951c73e563f
Hangga Aji Sayekti <bazeniancode@gmail.com>
2024-09-22 15:58:46 +0700

    add module

--- gradle-kotlin-dsl/settings.gradle.kts
+++ gradle-kotlin-dsl/settings.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
 rootProject.name = "gradle-kotlin-dsl"
 
 include("gradle-kotlin-dsl", "custom-source-set")
+include("configure-bytecode")
