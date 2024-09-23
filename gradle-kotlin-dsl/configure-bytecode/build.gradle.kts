9bd9a60ff81a688691abbdd2ad2183d08cd87e73
Hangga Aji Sayekti <bazeniancode@gmail.com>
2024-09-23 18:20:45 +0700

    update kts

--- gradle-kotlin-dsl/configure-bytecode/build.gradle.kts
+++ gradle-kotlin-dsl/configure-bytecode/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "2.0.0"
+    kotlin("jvm") version "2.0.0"
 }
 
 repositories {
--- gradle-kotlin-dsl/configure-configureEach/build.gradle.kts
+++ gradle-kotlin-dsl/configure-configureEach/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "2.0.0"
+    kotlin("jvm") version "2.0.0"
 }
 
 repositories {
