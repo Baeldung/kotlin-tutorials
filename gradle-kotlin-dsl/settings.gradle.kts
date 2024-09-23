70d6723f0304439a88c2622d21b950b5d52bf0b7
Hangga Aji Sayekti <bazeniancode@gmail.com>
2024-09-23 19:52:17 +0700

    remove unused plugin

--- gradle-kotlin-dsl/settings.gradle.kts
+++ gradle-kotlin-dsl/settings.gradle.kts
@@ -1,10 +1,8 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-}
 rootProject.name = "gradle-kotlin-dsl"
 
-include("gradle-kotlin-dsl", "custom-source-set")
-include("configure-bytecode")
-include("configure-with-toolchain")
-include("configure-compile-test")
-include("configure-configureEach")
+include("gradle-kotlin-dsl",
+    "custom-source-set",
+    "configure-bytecode",
+    "configure-with-toolchain",
+    "configure-compile-test",
+    "configure-configureEach")
