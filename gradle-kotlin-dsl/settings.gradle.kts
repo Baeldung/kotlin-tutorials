f0139b95d2c1ba0c6736eef40b7dfa4168fd8aec
Hangga Aji Sayekti <bazeniancode@gmail.com>
2024-09-23 18:42:28 +0700

    update

--- gradle-kotlin-dsl/settings.gradle.kts
+++ gradle-kotlin-dsl/settings.gradle.kts
@@ -1,7 +1,3 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-}
 rootProject.name = "gradle-kotlin-dsl"
 
-include("gradle-kotlin-dsl", "custom-source-set")
-include("configure-bytecode")
+include("gradle-kotlin-dsl", "custom-source-set", "configure-bytecode")
