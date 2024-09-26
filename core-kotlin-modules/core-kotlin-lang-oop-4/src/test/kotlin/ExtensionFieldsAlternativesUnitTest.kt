ff5f2e69159c2dc3948ead95c1c1ff1d3e1f3413
Hangga Aji Sayekti <bazeniancode@gmail.com>
2024-09-26 18:00:50 +0700

    KTLN-437 - Extension Fields in Kotlin (#1039)
    * skip
    
    * skip
    
    * update sample
    
    * update example
    
    * simplify
    
    * remove id
    
    * remove default constructor
    
    * add fun example
    
    * add not allowed way
    
    * update sample getter
    
    * add not allowed sample
    
    * add sample
    
    * cleanup
    
    * update
    
    * [update] sample part 1
    
    * add decorated person
    
    * add properties only
    
    * add function test
    
    * remove extra lines
    
    * add new sample
    
    * add car
    
    * add sample
    
    * remove address
    
    * add address
    
    * ;2C;2C;2C fiel\d
    
    * update sample ageInDecade
    
    * apply baeldung formatter
    
    * formatted
    
    * formatted

---
+++ core-kotlin-modules/core-kotlin-lang-oop-4/src/test/kotlin/ExtensionFieldsAlternativesUnitTest.kt
@@ -0,0 +1,115 @@
+import org.junit.jupiter.api.Test
+import java.util.*
+import kotlin.test.assertEquals
+
+class ExtensionFieldsAlternativesUnitTest {
+
+    fun String.toTitleCase(): String = this.split(" ").joinToString(" ") { it.capitalize() }
+
+    private val String.firstChar: Char
+        get() = this.first()
+
+    @Test
+    fun `test using extension functions`() {
+        val word = "baeldung kotlin"
+        assertEquals("Baeldung Kotlin", word.toTitleCase())
+        assertEquals('b', word.firstChar)
+    }
+
+    class Car {
+        var model: String = "Toyota Supra"
+    }
+
+    open class Person(var name: String, var age: Int)
+
+    val Person.isAdult: Boolean
+        get() = this.age >= 18
+
+    val Person.details: String
+        get() = "Name: ${this.name}, Age: ${this.age}, isAdult: ${this.isAdult}"
+
+    var Person.ageInDecades: Int
+        get() = this.age / 10
+        set(value) {
+            this.age = value * 10
+        }
+
+    val externalMap = mutableMapOf<Person, String>()
+
+    var Person.address: String
+        get() = externalMap[this] ?: ""
+        set(value) {
+            externalMap[this] = value
+        }
+
+    @Test
+    fun `test using external Map`() {
+        val person = Person("Hangga Aji Sayekti", 35)
+        person.address = "Jln. Kemasan Kotagede"
+        assertEquals("Jln. Kemasan Kotagede", person.address)
+
+        person.address = "Jln. Kalasan Sleman"
+        assertEquals("Jln. Kalasan Sleman", person.address)
+    }
+
+    @Test
+    fun `test using properties`() {
+        val car = Car()
+        assertEquals("Toyota Supra", car.model)
+
+        val person = Person("Hangga Aji Sayekti", 35)
+        assertEquals("Name: Hangga Aji Sayekti, Age: 35, isAdult: true", person.details)
+
+        person.ageInDecades = 1
+        assertEquals("Name: Hangga Aji Sayekti, Age: 10, isAdult: false", person.details)
+    }
+
+    interface PersonDecorator {
+        var address: String
+        var person: Person
+        fun getDetails(): String
+    }
+
+    class PersonWithAddress(override var person: Person) : PersonDecorator {
+        private var _address: String = ""
+
+        override var address: String
+            get() = _address
+            set(value) {
+                _address = value
+            }
+
+        override fun getDetails(): String {
+            return "Name: ${person.name}, Age: ${person.age}, Address: $address"
+        }
+    }
+
+    @Test
+    fun `test using decorator`() {
+        val person = Person("Hangga Aji Sayekti", 35)
+
+        val personWithAddress = PersonWithAddress(person)
+        personWithAddress.address = "Jln. Kemasan Kotagede"
+        assertEquals("Name: Hangga Aji Sayekti, Age: 35, Address: Jln. Kemasan Kotagede", personWithAddress.getDetails())
+
+        personWithAddress.address = "Jln. Kalasan Sleman"
+        assertEquals("Name: Hangga Aji Sayekti, Age: 35, Address: Jln. Kalasan Sleman", personWithAddress.getDetails())
+    }
+
+    class PersonExtended(name: String, age: Int) : Person(name, age) {
+        var jobtitle: String = ""
+    }
+
+    @Test
+    fun `test using inheritance`() {
+        val personExtended = PersonExtended("Hangga Aji Sayekti", 35)
+        personExtended.jobtitle = "Software Engineer"
+        assertEquals("Software Engineer", personExtended.jobtitle)
+
+        personExtended.jobtitle = "Data Scientist"
+        assertEquals("Data Scientist", personExtended.jobtitle)
+
+        personExtended.jobtitle = "Mathematicians"
+        assertEquals("Mathematicians", personExtended.jobtitle)
+    }
+}
