a903300369207f4618cbea79b8c813213c78d403
nappy29 <ewaneenombe@gmail.com>
2024-09-27 13:27:19 +0100

    code changes

--- core-kotlin-modules/core-kotlin-design-patterns/src/test/kotlin/com/baeldung/di/koin/DependencyInjectionUnitTest.kt
+++ core-kotlin-modules/core-kotlin-design-patterns/src/test/kotlin/com/baeldung/di/koin/DependencyInjectionUnitTest.kt
@@ -8,18 +8,18 @@ import org.koin.core.context.startKoin
 import org.koin.dsl.module
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
+import org.koin.core.module.Module
 
 
 class DependencyInjectionUnitTest: KoinComponent {
 
+    val appModule: Module = module { single<UserService> { UserServiceImpl() } }
     private val service: UserService by inject()
 
     @Test
     fun `DI demonstration with Koin`() {
         startKoin {
-            modules(module {
-                single<UserService> { UserServiceImpl() }
-            })
+            modules(appModule)
         }
         assertEquals("John Doe", service.getUser())
     }
