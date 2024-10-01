9dc76ee5ac6f141617dc00a83009e75116c7ad63
luca <lucacambi77@gmail.com>
2024-10-01 11:28:24 +0200

    fix: indent

--- spring-security-kotlin/src/main/kotlin/com/baeldung/security/jwt/repository/UserRepository.kt
+++ spring-security-kotlin/src/main/kotlin/com/baeldung/security/jwt/repository/UserRepository.kt
@@ -11,24 +11,24 @@ class UserRepository(
     encoder: PasswordEncoder
 ) {
     private val users = mutableSetOf(
-        User(
-            id = UUID.randomUUID(),
-            name = "email-1@gmail.com",
-            password = encoder.encode("pass1"),
-            role = Role.USER,
-        ),
-        User(
-            id = UUID.randomUUID(),
-            name = "email-2@gmail.com",
-            password = encoder.encode("pass2"),
-            role = Role.ADMIN,
-        ),
-        User(
-            id = UUID.randomUUID(),
-            name = "email-3@gmail.com",
-            password = encoder.encode("pass3"),
-            role = Role.USER,
-        )
+      User(
+        id = UUID.randomUUID(),
+        name = "email-1@gmail.com",
+        password = encoder.encode("pass1"),
+        role = Role.USER,
+      ),
+      User(
+        id = UUID.randomUUID(),
+        name = "email-2@gmail.com",
+        password = encoder.encode("pass2"),
+        role = Role.ADMIN,
+      ),
+      User(
+        id = UUID.randomUUID(),
+        name = "email-3@gmail.com",
+        password = encoder.encode("pass3"),
+        role = Role.USER,
+      )
     )
 
     fun findByUsername(email: String): User? =
