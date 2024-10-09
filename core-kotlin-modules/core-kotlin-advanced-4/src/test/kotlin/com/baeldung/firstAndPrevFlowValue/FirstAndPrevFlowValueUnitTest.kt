2d0a23f0ecf0b1f04984a321f3f9061b8c68611c
albertache1998 <albertache1998@gmail.com>
2024-10-09 15:00:19 +0100

    redone code

---
+++ core-kotlin-modules/core-kotlin-advanced-4/src/test/kotlin/com/baeldung/firstAndPrevFlowValue/FirstAndPrevFlowValueUnitTest.kt
@@ -0,0 +1,39 @@
+package com.baeldung.firstAndPrevFlowValue
+
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Test
+
+class FirstAndPrevFlowValueUnitTest {
+    @Test
+    fun `test manually tracking previous and current value`() = runTest {
+        val flow = flowOf(1, 2, 3, 4, 5)
+        var previousValue: Int? = null
+        val results = mutableListOf<Pair<Int?, Int>>()
+
+        flow.collect { currentValue ->
+            results.add(Pair(previousValue, currentValue))
+            previousValue = currentValue
+        }
+
+        assertEquals(listOf(Pair(null, 1), Pair(1, 2), Pair(2, 3), Pair(3, 4), Pair(4, 5)), results)
+    }
+
+    @Test
+    fun `test runningFold for previous and current value`() = runTest {
+        val flow = flowOf(1, 2, 3, 4, 5)
+        val initial: Pair<Int?, Int>? = null
+        val results = mutableListOf<Pair<Int?, Int>>()
+
+        flow.runningFold(initial) { lastPair, next ->
+            lastPair?.run {
+                val (_, last) = this
+                last to next
+            } ?: (null to next)
+        }.filterNotNull()
+            .collect { results.add(it) }
+
+        assertEquals(listOf(Pair(null, 1), Pair(1, 2), Pair(2, 3), Pair(3, 4), Pair(4, 5)), results)
+    }
+}
