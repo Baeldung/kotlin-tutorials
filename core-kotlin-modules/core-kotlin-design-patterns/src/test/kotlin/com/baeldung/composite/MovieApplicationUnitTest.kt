9a7a741ef1ebee9a4190be67ea49ccb2d22c6be2
nappy29 <ewaneenombe@gmail.com>
2024-10-12 04:45:35 +0100

    code refactor

--- core-kotlin-modules/core-kotlin-design-patterns/src/test/kotlin/com/baeldung/composite/MovieApplicationUnitTest.kt
+++ core-kotlin-modules/core-kotlin-design-patterns/src/test/kotlin/com/baeldung/composite/MovieApplicationUnitTest.kt
@@ -3,7 +3,7 @@ package com.baeldung.composite
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-class MovieKodeinUnitTestUnitTest {
+class MovieApplicationUnitTest {
 
     @Test
     fun `should play movie`() {
