11fb7d002d0cdafb31c6fe317dcf3f6ecf8b629c
emanuel.trandafir <emanuel.trandafir@betvictor.com>
2024-10-22 21:12:52 +0200

    KTLN-893: code sample

--- core-kotlin-modules/core-kotlin-lang-oop/src/main/kotlin/com/baeldung/dataclass/Task.kt
+++ core-kotlin-modules/core-kotlin-lang-oop/src/main/kotlin/com/baeldung/dataclass/Task.kt
@@ -1,3 +1,5 @@
 package com.baeldung.dataclass
 
-data class Task(var id: Int, var description: String, var priority: Int)
+data class Task(var id: Int, var description: String, var priority: Int) {
+    constructor(id: Int, priority: Int) : this(id, "anonymous task with priority $priority", priority)
+}
