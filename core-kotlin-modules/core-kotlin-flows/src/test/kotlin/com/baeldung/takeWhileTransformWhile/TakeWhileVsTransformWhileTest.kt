7e189b28fd7620b411655b7ea15540142a01d0ee
Husam <2340614+husam-e@users.noreply.github.com>
2024-10-22 22:44:07 -0400

    feat: add difference between Flow `takeWhile` and `transformWhile` tests

---
+++ core-kotlin-modules/core-kotlin-flows/src/test/kotlin/com/baeldung/takeWhileTransformWhile/TakeWhileVsTransformWhileTest.kt
@@ -0,0 +1,35 @@
+package com.baeldung.concatenateTwoFlows
+
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class TakeWhileVsTransformWhileTest {
+
+    @Test
+    fun `takeWhile less than 3 returns 1 and 2` {
+        runBlocking {
+            val numbersFlow = flowOf(1, 2, 3, 4, 5)
+
+            val taken = testFlow.takeWhile { it < 3 }.toList()
+
+            Assertions.assertEquals(listOf(1, 2), taken)
+        }
+    }
+
+    @Test
+    fun `transformWhile less than 3 can return 1, 2 and 3` {
+        runBlocking {
+            val numbersFlow = flowOf(1, 2, 3, 4, 5)
+
+            val taken = testFlow.transformWhile {
+                emit(it)
+                emit(it)
+                it < 3
+            }.toList()
+
+            Assertions.assertEquals(listOf(1, 1, 2, 2, 3, 3), taken)
+        }
+    }
+}
