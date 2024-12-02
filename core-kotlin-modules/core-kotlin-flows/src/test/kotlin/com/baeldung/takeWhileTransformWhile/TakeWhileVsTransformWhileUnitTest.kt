a98b989497f5c1a1c9f15d7a4cb65a639e9b61fc
Husam E <2340614+husam-e@users.noreply.github.com>
2024-12-01 19:32:29 -0500

    fix test

--- core-kotlin-modules/core-kotlin-flows/src/test/kotlin/com/baeldung/takeWhileTransformWhile/TakeWhileVsTransformWhileUnitTest.kt
+++ core-kotlin-modules/core-kotlin-flows/src/test/kotlin/com/baeldung/takeWhileTransformWhile/TakeWhileVsTransformWhileUnitTest.kt
@@ -24,7 +24,7 @@ class TakeWhileVsTransformWhileUnitTest {
             val numbersFlow = flowOf(1, 2, 3, 4, 5)
 
             val transformed = numbersFlow.transformWhile {
-                it
+                emit(it)
                 it < 3
             }.toList()
 
