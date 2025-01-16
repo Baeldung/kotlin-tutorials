7bd476793702d9fb86d60c096802daa49b57ccbc
Husam E <2340614+husam-e@users.noreply.github.com>
2025-01-16 13:11:13 -0500

    feat(kotlin-collections): add list deep clone tests

--- core-kotlin-modules/core-kotlin-collections-2/src/test/kotlin/com/baeldung/lists/ListCopyUnitTest.kt
+++ core-kotlin-modules/core-kotlin-collections-2/src/test/kotlin/com/baeldung/lists/ListCopyUnitTest.kt
@@ -1,6 +1,7 @@
 package com.baeldung.lists
 
 import org.assertj.core.api.Assertions.assertThat
+import org.jetbrains.annotations.TestOnly
 import org.junit.jupiter.api.Test
 
 class ListCopyUnitTest {
@@ -17,5 +18,38 @@ class ListCopyUnitTest {
 
         assertThat(copied).isNotSameAs(cities)
         assertThat(copied[0]).isSameAs(cities[0])
+    }
+
+    @Test
+    fun `should deep copy a data class with only primitive fields`() {
+        data class Person(var name: String, var age: Int)
+        val persons = listOf(Person("Bob", 20), Person("Alice", 21))
+        val personsCopy = persons.map { it.copy() }
+
+        assertThat(personsCopy).isNotSameAs(persons)
+    }
+
+    @Test
+    fun `should deep copy a data class using a custom clone method`() {
+        data class Address(var streetName: String, var streetNumber: Int, var city: String)
+
+        data class Person(val name: String, val age: Int, val address: Address) {
+            fun clone(): Person {
+                return Person(this.name, this.age, this.address.copy())
+            }
+        }
+
+        val address1 = Address("Sesame Street", 1, "CartoonLand")
+        val address2 = Address("Sesame Street", 2, "CartoonLand")
+        val persons = listOf(Person("Bob", 20, address1), Person("Alice", 21, address2))
+
+        val personCopies = persons.map { it.clone() }
+
+        assertThat(personCopies).isNotSameAs(persons)
+        assertThat(persons[0].address).isEqualTo(personCopies[0].address)
+
+        personCopies[0].address.streetNumber = 10
+
+        assertThat(persons[0].address).isNotEqualTo(personCopies[0].address)
     }
 }
