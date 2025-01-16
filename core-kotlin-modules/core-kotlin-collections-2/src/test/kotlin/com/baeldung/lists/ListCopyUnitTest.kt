72f60d5e293ebe27642ab50399db68ce9028168d
Husam E <2340614+husam-e@users.noreply.github.com>
2025-01-16 13:13:26 -0500

    remove extraneous import

--- core-kotlin-modules/core-kotlin-collections-2/src/test/kotlin/com/baeldung/lists/ListCopyUnitTest.kt
+++ core-kotlin-modules/core-kotlin-collections-2/src/test/kotlin/com/baeldung/lists/ListCopyUnitTest.kt
@@ -1,7 +1,6 @@
 package com.baeldung.lists
 
 import org.assertj.core.api.Assertions.assertThat
-import org.jetbrains.annotations.TestOnly
 import org.junit.jupiter.api.Test
 
 class ListCopyUnitTest {
