4d1135c8419d38ff68c7ac78c4072c46e5d0461c
LeoHelfferich <leo@helfferich.net>
2025-01-22 08:18:39 +0100

    BAEL-8537: test docs

--- core-kotlin-modules/core-kotlin-12/src/test/kotlin/com/baeldung/formatting/NumberFormatUnitTest.kt
+++ core-kotlin-modules/core-kotlin-12/src/test/kotlin/com/baeldung/formatting/NumberFormatUnitTest.kt
@@ -8,38 +8,49 @@ import org.assertj.core.api.Assertions.assertThat
 
 class NumberFormatUnitTest : ShouldSpec({
 
-    listOf( // different implementations of the formatting feature
-        "FormatByChunking" to { it: Int -> FormatByChunking.formatted(it) },
-        "FormatByStringFormat" to { it: Int -> FormatByStringFormat.formatted(it) },
-        "FormatByDecimalFormatGermany" to { it: Int -> FormatByDecimalFormatGermany.formatted(it) },
-        "FormatByDecimalFormat" to { it: Int -> FormatByDecimalFormat.formatted(it) }
-    ).forEach { (type, function) ->
+    // Dynamic generation of tests for each implementation
+    nameToImplementationPairs.forEach { (type, function) ->
 
         // Property based test (for each implementation)
-        should("return correctly formatted string with $type") {
+        should("return correctly formatted string by $type") {
             checkAll(Arb.positiveInt()) { number ->
                 var result = function(number)
 
+                // Check with regex
                 assertThat(result).containsPattern("^(\\d{1,3}(\\.\\d{3})*|\\d+)$")
+                // Check against original, by removing the separators
                 assertThat(number.toString()).isEqualTo(result.replace(".", ""))
+
+                // Check for general presence & absence of separators
                 if (number > 999) assertThat(result).contains(".")
                 else assertThat(result).doesNotContain(".")
             }
         }
 
-        listOf( // examples with given number and expected string
-            100_000 to "100.000",
-            1_234_567 to "1.234.567",
-            0 to "0",
-            12 to "12",
-            456 to "456"
-        ).forEach { (number, expected) ->
+        givenToExpectedPairs.forEach { (number, expected) ->
 
             // Parameterised; Example based test
-            should("return expected string '$expected' for $number with $type") {
+            should("return expected string '$expected' for $number by $type") {
                 assertThat(function(number)).isEqualTo(expected)
             }
         }
 
     }
 })
+
+// Examples to check against, with given number and expected string
+private val givenToExpectedPairs = listOf(
+    0 to "0",
+    12 to "12",
+    456 to "456",
+    100_000 to "100.000",
+    1_234_567 to "1.234.567"
+)
+
+// Different implementations of the formatting feature with the display name
+private val nameToImplementationPairs = listOf(
+    "FormatByChunking" to { it: Int -> FormatByChunking.formatted(it) },
+    "FormatByStringFormat" to { it: Int -> FormatByStringFormat.formatted(it) },
+    "FormatByDecimalFormatGermany" to { it: Int -> FormatByDecimalFormatGermany.formatted(it) },
+    "FormatByDecimalFormat" to { it: Int -> FormatByDecimalFormat.formatted(it) }
+)
