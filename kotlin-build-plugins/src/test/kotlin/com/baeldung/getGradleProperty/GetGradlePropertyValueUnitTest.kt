e499a76a4d6cb1676f9b958adc81d76ab822370c
Loredana Crusoveanu <lore.crusoveanu@gmail.com>
2025-11-25 17:17:31 +0200

    fix detekt failures

--- kotlin-build-plugins/src/test/kotlin/com/baeldung/getGradleProperty/GetGradlePropertyValueUnitTest.kt
+++ kotlin-build-plugins/src/test/kotlin/com/baeldung/getGradleProperty/GetGradlePropertyValueUnitTest.kt
@@ -6,25 +6,25 @@ import java.util.Properties
 
 class GetGradlePropertyValueUnitTest {
 
-    @org.junit.jupiter.api.Test
-    fun `obtain environment variables using System getenv method`() {
-        val customProperty = System.getenv("CUSTOM_PROPERTY")
-        val apiUrl = System.getenv("API_URL")
+  @org.junit.jupiter.api.Test
+  fun `obtain environment variables using System getenv method`() {
+    val customProperty = System.getenv("CUSTOM_PROPERTY")
+    val apiUrl = System.getenv("API_URL")
 
-        assertEquals("CustomValue", customProperty)
-        assertEquals("https://api.example.com", apiUrl)
+    assertEquals("CustomValue", customProperty)
+    assertEquals("https://api.example.com", apiUrl)
+  }
+
+  @Test
+  fun `obtain custom property value by loading properties from generated file`() {
+    val propertiesFileUrl = this::class.java.classLoader.getResource("custom.properties")
+      ?: throw IllegalStateException("Properties file not found")
+
+    val properties = Properties().apply {
+      propertiesFileUrl.openStream().use { load(it) }
     }
 
-    @Test
-    fun `obtain custom property value by loading properties from generated file`() {
-        val propertiesFileUrl = this::class.java.classLoader.getResource("custom.properties")
-            ?: throw IllegalStateException("Properties file not found")
-
-        val properties = Properties().apply {
-            propertiesFileUrl.openStream().use { load(it) }
-        }
-
-        assertEquals("CustomValue", properties.getProperty("custom.property"))
-        assertEquals("https://api.example.com", properties.getProperty("api.url"))
-    }
+    assertEquals("CustomValue", properties.getProperty("custom.property"))
+    assertEquals("https://api.example.com", properties.getProperty("api.url"))
+  }
 }
